8f9b58208322bbf98d48a1980f2d05cc510ad086
Florijan Stamenkovic <florijan.stamenkovic@gmail.com>
2019-08-29 10:22:11 +0200

    Expose DB classes in top-level package

--- labelbox/__init__.py
+++ labelbox/__init__.py
@@ -1,4 +1,5 @@
 name = "labelbox"
 
 from labelbox.client import Client
-from labelbox.db_objects import Project
+from labelbox.db_objects import (Project, Dataset, DataRow, User, Organization,
+                                 Task)
