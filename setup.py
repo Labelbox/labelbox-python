b5e2ae3b9f0d076ad41f234b12373dc7617f870d
Florijan Stamenkovic <florijan.stamenkovic@gmail.com>
2019-09-02 13:31:22 +0200

    Update README and setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 import setuptools
 
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
     name="labelbox",
-    version="0.1",
+    version="2.0",
     author="Labelbox",
     author_email="engineering@labelbox.com",
     description="Labelbox Python API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/labelbox/labelbox-pip",
+    url="https://labelbox.com",
     packages=setuptools.find_packages(),
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords=["labelbox"],
 )
