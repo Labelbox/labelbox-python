defbd2ba0ff1d15eb4e09011ec1462d913a4386f
Florijan Stamenkovic <florijan.stamenkovic@gmail.com>
2019-09-09 12:58:10 +0200

    Add AssetMetadata

--- labelbox/__init__.py
+++ labelbox/__init__.py
@@ -2,4 +2,5 @@ name = "labelbox"
 
 from labelbox.client import Client
 from labelbox.db_objects import (
-    Project, Dataset, DataRow, Label, User, Organization, Task, LabelingFrontend)
+    Project, Dataset, DataRow, Label, User, Organization, Task, LabelingFrontend,
+    AssetMetadata)
