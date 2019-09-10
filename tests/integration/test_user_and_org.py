c225392583c696538d96ed3b5401c29d2665d669
Florijan Stamenkovic <florijan.stamenkovic@gmail.com>
2019-09-10 10:42:01 +0200

    Enable Project.created_by

--- tests/integration/test_user_and_org.py
+++ tests/integration/test_user_and_org.py
@@ -19,12 +19,7 @@ def test_user_projects(client, rand_gen):
     projects = list(user.projects())
 
     project = client.create_project(name=rand_gen(Project.name))
-    # TODO ensure that the user can be reached from the Project side
-    # this can be done by changing the relationship query not to include
-    # a "where" clause when not necessary, or by making the server-side
-    # consistent w.r.t. accepting "where" clauses
-    with pytest.raises(NetworkError):
-        project_creator = project.created_by()
+    assert project.created_by() == user
 
     assert len(list(user.projects())) == len(projects) + 1
 
