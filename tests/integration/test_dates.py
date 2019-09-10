ae8abd87e92a06899b0bb75e7c3aecd14f6475de
Florijan Stamenkovic <florijan.stamenkovic@gmail.com>
2019-09-10 10:27:10 +0200

    Fix UTC conversion test

--- tests/integration/test_dates.py
+++ tests/integration/test_dates.py
@@ -14,14 +14,20 @@ def test_dates(client):
 
 def test_utc_conversion(client):
     project = client.create_project(name="Test")
+    assert isinstance(project.created_at, datetime)
+    assert project.created_at.tzinfo == timezone.utc
 
-    # Update with a datetime without TZ info
+    # Update with a datetime without TZ info.
     project.update(setup_complete=datetime.now())
-    assert abs(project.setup_complete - datetime.utcnow()) < timedelta(minutes=1)
+    # Check that the server-side, UTC date is the same as local date
+    # converted locally to UTC.
+    diff = project.setup_complete - datetime.now().astimezone(timezone.utc)
+    assert abs(diff) < timedelta(minutes=1)
 
-    # Update wit ha datetime with TZ info
-    tz = timezone(timedelta(hours=6))
-    project.update(setup_complete=datetime.now(tz))
-    assert abs(project.setup_complete - datetime.utcnow()) < timedelta(minutes=1)
+    # Update with a datetime with TZ info
+    tz = timezone(timedelta(hours=6))   # +6 timezone
+    project.update(setup_complete=datetime.utcnow().replace(tzinfo=tz))
+    diff = datetime.utcnow() - project.setup_complete.replace(tzinfo=None)
+    assert diff > timedelta(hours=5, minutes=58)
 
     project.delete()
