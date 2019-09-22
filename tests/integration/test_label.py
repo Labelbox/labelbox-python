829b4b13a2f40573ae0f8943f9ed37c0c9d0f598
Florijan Stamenkovic <florijan.stamenkovic@gmail.com>
2019-09-22 20:16:58 +0200

    Enable label export test (failing)

--- tests/integration/test_label.py
+++ tests/integration/test_label.py
@@ -41,19 +41,18 @@ def test_labels(client, rand_gen):
 
 
 def test_label_export(client, rand_gen):
-    # TODO test
-    return
     project = client.create_project(name=rand_gen(str))
     dataset = client.create_dataset(name=rand_gen(str), projects=project)
     data_row = dataset.create_data_row(row_data=IMG_URL)
     project.create_label(data_row=data_row, label="l1", seconds_to_label=0.2)
     project.create_label(data_row=data_row, label="l2", seconds_to_label=0.3)
 
-    exported_labels_url = project.export_labels()
+    exported_labels_url = project.export_labels(5)
     assert exported_labels_url is not None
     if exported_labels_url is not None:
         exported_labels = requests.get(exported_labels_url)
         # TODO check content
+        assert False
 
     dataset.delete()
     project.delete()
