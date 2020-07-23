31caa3e8ed4ed8ec508bde302eeefa3ea8f1360b
rllin <randall@labelbox.com>
2020-07-22 17:53:10 -0700

    clean up

--- labelbox/client.py
+++ labelbox/client.py
@@ -173,10 +173,22 @@ class Client:
         return response["data"]
 
     def upload_file(self, path):
+        """Uploads given path to local file.
+
+        Also includes best guess at the content type of the file.
+
+        Args:
+            path (str): path to local file to be uploaded.
+        Returns:
+            str, the URL of uploaded data.
+        Raises:
+            labelbox.exceptions.LabelboxError: If upload failed.
+
+        """
         content_type, _ = mimetypes.guess_type(path)
+        basename = os.path.basename(path)
         with open(path, "rb") as f:
-            data = (None, f.read(), content_type)
-        return self.upload_data(data)
+            return self.upload_data(data=(basename, f.read(), content_type))
 
     def upload_data(self, data):
         """ Uploads the given data (bytes) to Labelbox.
@@ -188,7 +200,6 @@ class Client:
         Raises:
             labelbox.exceptions.LabelboxError: If upload failed.
         """
-
         request_data = {
             "operations": json.dumps({
                 "variables": {"file": None, "contentLength": len(data), "sign": False},
