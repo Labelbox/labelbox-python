bb154f260e62b1528bd60dccc1ac90d0b2e8bda4
rllin <randall@labelbox.com>
2020-07-30 18:19:03 -0700

    fix

--- tests/integration/environ.py
+++ tests/integration/environ.py
@@ -1,8 +1,9 @@
 """For integration tests to detect the environment they are being run against.
 
 """
-
 from enum import Enum
+import os
+
 
 class Environ(Enum):
     PROD = 'prod'
@@ -13,6 +14,8 @@ def get_environ() -> Environ:
     """
     Checks environment variables for LABELBOX_ENVIRON to be
     'prod' or 'staging'
+
+    Make sure to set LABELBOX_TEST_ENVIRON in .github/workflows/python-package.yaml
 
     """
     return Environ(os.environ.get['LABELBOX_TEST_ENVIRON'])
