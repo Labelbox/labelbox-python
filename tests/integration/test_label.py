dc97970c7c02bdbb1659506bc63c69914f5c13dc
rllin <randall@labelbox.com>
2020-07-31 15:28:49 -0700

    Update test_label.py

--- tests/integration/test_label.py
+++ tests/integration/test_label.py
@@ -98,7 +98,7 @@ def test_label_bulk_deletion(project, rand_gen):
     # TODO: the sdk client should really abstract all these timing issues away
     # but for now bulk deletes take enough time that this test is flaky
     # add sleep here to avoid that flake
-    time.sleep(1)
+    time.sleep(2)
 
     assert set(project.labels()) == {l2}
 
