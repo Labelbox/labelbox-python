fb12d272be8012916c3ab705e506dc71be6f0df0
rllin <randall@labelbox.com>
2020-08-01 13:36:55 -0700

    [BACKEND-829] fix content length (#30)
    * package data for upload properly to fix content length being incorrect for file uploads
    
    * basename -> filename
    
    * yapf
    
    * fix mypy
    
    * yapf
    
    * test coverage for video uploads
    
    * test video
    
    * clear parens

--- tests/integration/test_dataset.py
+++ tests/integration/test_dataset.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 
 from labelbox import Dataset
 from labelbox.exceptions import ResourceNotFoundError
@@ -78,3 +79,23 @@ def test_get_data_row_for_external_id(dataset, rand_gen):
 
     with pytest.raises(ResourceNotFoundError):
         data_row = dataset.data_row_for_external_id(external_id)
+
+
+def test_upload_video_file(dataset, sample_video: str) -> None:
+    """
+    Tests that a mp4 video can be uploaded and preserve content length
+    and content type.
+
+    """
+    dataset.create_data_row(row_data=sample_video)
+    task = dataset.create_data_rows([sample_video, sample_video])
+    task.wait_till_done()
+
+    with open(sample_video, 'rb') as video_f:
+        content_length = len(video_f.read())
+
+    for data_row in dataset.data_rows():
+        url = data_row.row_data
+        response = requests.head(url, allow_redirects=True)
+        assert int(response.headers['Content-Length']) == content_length
+        assert response.headers['Content-Type'] == 'video/mp4'
