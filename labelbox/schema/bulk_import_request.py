618e73fc5ed6d5d06a79d991d7e0ba6812fb01b9
rllin <randall@labelbox.com>
2020-08-05 23:20:08 -0700

    clean up

--- labelbox/schema/bulk_import_request.py
+++ labelbox/schema/bulk_import_request.py
@@ -12,8 +12,7 @@ import ndjson
 import requests
 
 import labelbox.exceptions
-#from labelbox import Client
-#from labelbox import Project
+from labelbox import Project
 from labelbox import User
 from labelbox.orm import query
 from labelbox.orm.db_object import DbObject
@@ -33,84 +32,16 @@ def __make_file_name(project_id: str, name: str) -> str:
 def __build_results_query_part() -> str:
     return """
         project {
-            name
-            description
-            updatedAt
-            createdAt
-            setupComplete
-            lastActivityTime
-            autoAuditNumberOfLabels
-            autoAuditPercentage
+            %s
         }
         createdBy {
             %s
         }
         %s
     """ % (
-        #query.results_query_part(Project),
+        query.results_query_part(Project),
         query.results_query_part(User),
         query.results_query_part(BulkImportRequest))
-
-
-# TODO(gszpak): move it to client.py
-def __make_request_data(project_id: str, name: str, content_length: int,
-                        file_name: str) -> dict:
-    query_str = """mutation createBulkImportRequestFromFilePyApi(
-            $projectId: ID!, $name: String!, $file: Upload!, $contentLength: Int!) {
-        createBulkImportRequest(data: {
-            projectId: $projectId,
-            name: $name,
-            filePayload: {
-                file: $file,
-                contentLength: $contentLength
-            }
-        }) {
-            %s
-        }
-    }
-    """ % __build_results_query_part()
-    variables = {
-        "projectId": project_id,
-        "name": name,
-        "file": None,
-        "contentLength": content_length
-    }
-    operations = json.dumps({"variables": variables, "query": query_str})
-
-    return {
-        "operations": operations,
-        "map": (None, json.dumps({file_name: ["variables.file"]}))
-    }
-
-
-# TODO(gszpak): move it to client.py
-def __send_create_file_command(
-        client, request_data: dict, file_name: str,
-        file_data: Tuple[str, Union[bytes, BinaryIO], str]) -> dict:
-    response = requests.post(
-        client.endpoint,
-        headers={"authorization": "Bearer %s" % client.api_key},
-        data=request_data,
-        files={file_name: file_data})
-
-    try:
-        response_json = response.json()
-    except ValueError:
-        raise labelbox.exceptions.LabelboxError(
-            "Failed to parse response as JSON: %s" % response.text)
-
-    response_data = response_json.get("data", None)
-    if response_data is None:
-        raise labelbox.exceptions.LabelboxError(
-            "Failed to upload, message: %s" % response_json.get("errors", None))
-
-    if not response_data.get("createBulkImportRequest", None):
-        raise labelbox.exceptions.LabelboxError(
-            "Failed to create BulkImportRequest, message: %s" %
-            response_json.get("errors", None) or
-            response_data.get("error", None))
-
-    return response_data
 
 
 class BulkImportRequest(DbObject):
@@ -124,8 +55,7 @@ class BulkImportRequest(DbObject):
     state = Field.Enum(BulkImportRequestState, "state")
 
     # TODO(gszpak): building query body should be handled by the client
-    @staticmethod
-    def get(client, project_id: str, name: str) -> 'BulkImportRequest':
+    def get(self, project_id: str, name: str) -> 'BulkImportRequest':
         """
         Fetches existing BulkImportRequest.
 
@@ -147,7 +77,7 @@ class BulkImportRequest(DbObject):
         }
         """ % __build_results_query_part()
         params = {"projectId": project_id, "name": name}
-        bulk_import_request_kwargs = client.execute(
+        bulk_import_request_kwargs = self.client.execute(
             query_str, params=params).get("bulkImportRequest")
         if bulk_import_request_kwargs is None:
             raise labelbox.exceptions.ResourceNotFoundError(
@@ -155,7 +85,7 @@ class BulkImportRequest(DbObject):
                     "projectId": project_id,
                     "name": name
                 })
-        return BulkImportRequest.from_result(client, bulk_import_request_kwargs)
+        return BulkImportRequest.from_result(self.client, bulk_import_request_kwargs)
 
     def refresh(self) -> None:
         """
@@ -201,16 +131,74 @@ class BulkImportRequest(DbObject):
             return self.__user
         return None
 
-    @staticmethod
-    def from_result(client, result: dict) -> 'BulkImportRequest':
+    # TODO(gszpak): move it to client.py
+    def __make_request_data(self, name: str, content_length: int,
+                            file_name: str) -> dict:
+        query_str = """mutation createBulkImportRequestFromFilePyApi(
+                $projectId: ID!, $name: String!, $file: Upload!, $contentLength: Int!) {
+            createBulkImportRequest(data: {
+                projectId: $projectId,
+                name: $name,
+                filePayload: {
+                    file: $file,
+                    contentLength: $contentLength
+                }
+            }) {
+                %s
+            }
+        }
+        """ % __build_results_query_part()
+        variables = {
+            "projectId": self.project.uid,
+            "name": name,
+            "file": None,
+            "contentLength": content_length
+        }
+        operations = json.dumps({"variables": variables, "query": query_str})
+
+        return {
+            "operations": operations,
+            "map": (None, json.dumps({file_name: ["variables.file"]}))
+        }
+
+    # TODO(gszpak): move it to client.py
+    def __send_create_file_command(
+            self, request_data: dict, file_name: str,
+            file_data: Tuple[str, Union[bytes, BinaryIO], str]) -> dict:
+        response = requests.post(
+            self.client.endpoint,
+            headers={"authorization": "Bearer %s" % self.client.api_key},
+            data=request_data,
+            files={file_name: file_data})
+
+        try:
+            response_json = response.json()
+        except ValueError:
+            raise labelbox.exceptions.LabelboxError(
+                "Failed to parse response as JSON: %s" % response.text)
+
+        response_data = response_json.get("data", None)
+        if response_data is None:
+            raise labelbox.exceptions.LabelboxError(
+                "Failed to upload, message: %s" % response_json.get("errors", None))
+
+        if not response_data.get("createBulkImportRequest", None):
+            raise labelbox.exceptions.LabelboxError(
+                "Failed to create BulkImportRequest, message: %s" %
+                response_json.get("errors", None) or
+                response_data.get("error", None))
+
+        return response_data
+
+    def from_result(result: dict) -> 'BulkImportRequest':
         project = result.pop("project")
         user = result.pop("createdBy")
-        bulk_import_request = BulkImportRequest(client, result)
+        bulk_import_request = BulkImportRequest(self.client, result)
         if project is not None:
             bulk_import_request.__project = Project(  # type: ignore
-                client, project)
+                self.client, project)
         if user is not None:
-            bulk_import_request.__user = User(client, user)  # type: ignore
+            bulk_import_request.__user = User(self.client, user)  # type: ignore
         return bulk_import_request
 
 
@@ -275,13 +263,12 @@ def create_from_objects(client, project_id: str, name: str,
     data_str = ndjson.dumps(predictions)
     data = data_str.encode('utf-8')
     file_name = __make_file_name(project_id, name)
-    request_data = __make_request_data(project_id, name, len(data_str),
-                                       file_name)
+    request_data = self.__make_request_data(name, len(data_str), file_name)
     file_data = (file_name, data, NDJSON_MIME_TYPE)
-    response_data = __send_create_file_command(client=client,
-                                               request_data=request_data,
-                                               file_name=file_name,
-                                               file_data=file_data)
+    response_data = self.__send_create_file_command(
+        request_data=request_data,
+        file_name=file_name,
+        file_data=file_data)
 
     return BulkImportRequest.from_result(
         client, response_data["createBulkImportRequest"])
@@ -307,22 +294,23 @@ def create_from_local_file(client,
     """
     file_name = __make_file_name(project_id, name)
     content_length = file.stat().st_size
-    request_data = __make_request_data(project_id, name, content_length,
-                                       file_name)
+    request_data = self.__make_request_data(name, content_length, file_name)
     if validate_file:
         with file.open('rb') as f:
             reader = ndjson.reader(f)
+            # ensure that the underlying json load call is valid
+            # https://github.com/rhgrant10/ndjson/blob/ff2f03c56b21f28f7271b27da35ca4a8bf9a05d0/ndjson/api.py#L53
+            # by iterating through the file so we only store
+            # each line in memory rather than the entire file
             try:
                 for line in reader:
-                    # ensure that the underlying json load call is valid
-                    # https://github.com/rhgrant10/ndjson/blob/ff2f03c56b21f28f7271b27da35ca4a8bf9a05d0/ndjson/api.py#L53
                     pass
             except ValueError:
                 raise ValueError(f"{file} is not a valid ndjson file")
 
     with file.open('rb') as f:
         file_data = (file.name, f, NDJSON_MIME_TYPE)
-        response_data = __send_create_file_command(client, request_data,
-                                                   file_name, file_data)
+        response_data = self.__send_create_file_command(
+            request_data, file_name, file_data)
     return BulkImportRequest.from_result(
         client, response_data["createBulkImportRequest"])
