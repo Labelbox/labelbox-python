b7df38f9194c7ab53df3e6702b2be1a8843f8ed3
rllin <randall@labelbox.com>
2020-08-11 14:47:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="labelbox",
-    version="2.4.3",
+    version="2.4.4",
     author="Labelbox",
     author_email="engineering@labelbox.com",
     description="Labelbox Python API",
