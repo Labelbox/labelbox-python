7380ebee6bce78d48072967aa00be3aa0f5d551e
rllin <randall@labelbox.com>
2020-08-20 23:41:20 -0700

    catch bad inputs

--- labelbox/schema/bulk_import_request.py
+++ labelbox/schema/bulk_import_request.py
@@ -218,6 +218,9 @@ class BulkImportRequest(DbObject):
             BulkImportRequest object
         """
         data_str = ndjson.dumps(predictions)
+        if not data_str:
+            raise ValueError('annotations cannot be empty')
+
         data = data_str.encode('utf-8')
         file_name = _make_file_name(project_id, name)
         request_data = _make_request_data(project_id, name, len(data_str),
