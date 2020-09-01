3300f774bff31b8f3758fe8dfee6aaf1a3feec45
rllin <randall@labelbox.com>
2020-08-31 23:20:31 -0700

    retry

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
         "backoff==1.10.0",
         "ndjson==0.3.1",
         "requests>=2.22.0",
+        "google-api-core>=1.22.1",
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
