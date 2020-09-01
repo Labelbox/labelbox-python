4db8fb60ce37c841689eef1dc40fb3d34d5cadc5
rllin <randall@labelbox.com>
2020-08-31 18:53:56 -0700

    loosen requirements + catch actual error for reraise

--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@ setuptools.setup(
     install_requires=[
         "backoff==1.10.0",
         "ndjson==0.3.1",
-        "requests==2.22.0",
-        "google-api-core>=1.22.1",
+        "requests>=2.22.0",
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
