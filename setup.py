bad3149d6f572bcd0937383e92345677f3ba33ea
rllin <randall@labelbox.com>
2020-09-01 01:36:07 -0700

    [BACKEND-946] [BACKEND-947] [BACKEND-948] pass thru errors, retry, catch bad annotation types (#55)
    * catch bad inputs
    
    * print response when fail
    
    * update labeling frontend test to work with any project even if it has more than the default editors
    
    * yapf
    
    * Update client.py
    
    * catch actual errors
    
    * loosen requirements + catch actual error for reraise
    
    * print 400s
    
    * catchall for other errors
    
    * retry
    
    * ignore missing improrts
    
    * update version and changelog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="labelbox",
-    version="2.4.4",
+    version="2.4.5",
     author="Labelbox",
     author_email="engineering@labelbox.com",
     description="Labelbox Python API",
@@ -13,7 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://labelbox.com",
     packages=setuptools.find_packages(),
-    install_requires=["backoff==1.10.0", "ndjson==0.3.1", "requests==2.22.0"],
+    install_requires=[
+        "backoff==1.10.0",
+        "ndjson==0.3.1",
+        "requests>=2.22.0",
+        "google-api-core>=1.22.1",
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
