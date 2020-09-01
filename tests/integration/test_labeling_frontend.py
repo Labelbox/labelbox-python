a21fa5181a03215938036944e96bb609c43ebed0
rllin <randall@labelbox.com>
2020-08-31 17:12:11 -0700

    update labeling frontend test to work with any project even if it has more than the default editors

--- tests/integration/test_labeling_frontend.py
+++ tests/integration/test_labeling_frontend.py
@@ -3,13 +3,15 @@ from labelbox import LabelingFrontend
 
 def test_get_labeling_frontends(client):
     frontends = list(client.get_labeling_frontends())
-    assert len(frontends) == 1, frontends
+    assert len(frontends) >= 1, (
+        'Projects should have at least one frontend by default.')
 
     # Test filtering
-    single = list(
-        client.get_labeling_frontends(where=LabelingFrontend.iframe_url_path ==
-                                      frontends[0].iframe_url_path))
-    assert len(single) == 1, single
+    target_frontend = frontends[0]
+    filtered_frontends = client.get_labeling_frontends(
+        where=LabelingFrontend.iframe_url_path == target_frontend.iframe_url_path)
+    for frontend in filtered_frontends:
+        assert target_frontend == frontend
 
 
 def test_labeling_frontend_connecting_to_project(project):
