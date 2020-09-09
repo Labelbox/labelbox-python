bb12aaf91f37034b82c3c22bcfb32c37126b2fb6
rllin <randall@labelbox.com>
2020-09-09 15:43:42 -0700

    update python to 3.7, 3.8

--- setup.py
+++ setup.py
@@ -23,8 +23,9 @@ setuptools.setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
+    python_requires='>=3.7',
     keywords=["labelbox"],
 )
