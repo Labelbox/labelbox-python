fa9610a66269c8f8289db3217142dddca56d5893
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2021-02-03 16:33:32 -0800

    updates to the file to have helper functions that smoothen the writeup

--- labelbox/schema/ontology_generator.py
+++ labelbox/schema/ontology_generator.py
@@ -1,5 +1,10 @@
 '''
 TODO
+1. make classification pulls recursive (go beyond just the 1st layer) -> but how to make classification->option->classification->infinitely?
+2. validate that we can pull in all sorts of project ontology classes
+3. validate we can submit basic ones back in 
+4. do the rest of the stuff below
+
 currently allows creation of a TEXT classification and options inside this file
 ↑ should not be possible, and in fact causes some unintended behavior when creating the ontology (creates options as a totally new classification)
 ↑ need to ensure that with TEXT we cannot embed options
@@ -8,15 +13,12 @@ validate prior to submission, so likely in the o.build() - like have a way to ma
 
 work on enforcing certain classifications need options (and work on other things other than text)
 
-create an example of a classification with options
-
-work on nesting classes inside tools (and properly extrapolating the options when taking it from inside a project)
-
-work on nesting classes inside other classes
-
 maybe there should be a way to check if a project has an existing ontology, and that it would overwrite it?
 
 in the future: work on adding NER capability for tool types (?)
+
+EXTRA
+    #TODO: look into static methods and differences vs. class methods
 '''
 
 from dataclasses import dataclass, field
@@ -39,15 +41,22 @@ class Option:
     @property
     def label(self):
         return self.value
+    
+    def to_dict(self) -> dict:
+        return {
+            "schemaNodeId": self.schema_id,
+            "featureSchemaId": self.feature_schema_id,
+            "label": self.label,
+            "value": self.value
+        }
 
     @classmethod
-    def option_class_to_dict(cls, option):
-        return {
-            "schemaNodeId": option.schema_id,
-            "featureSchemaId": option.feature_schema_id,
-            "label": option.value,
-            "value": option.value
-        }
+    def from_dict(cls, dictionary: dict):
+        return Option(
+            value = dictionary["value"],
+            schema_id = dictionary['schemaNodeId'],
+            feature_schema_id = dictionary['featureSchemaId']
+        )
     
 
 @dataclass
@@ -70,12 +79,33 @@ class Classification:
     def name(self):
         return self.instructions
 
+    def to_dict(self) -> dict:
+        return {
+                "type": self.class_type.value,
+                "instructions": self.instructions,
+                "name": self.name,
+                "required": self.required,
+                "options": [option.to_dict() for option in classification.options],
+                "schemaNodeId": self.schema_id,
+                "featureSchemaId": self.feature_schema_id
+        }   
+
+    @classmethod
+    def from_dict(cls, dictionary: dict): 
+        return Classification(
+            class_type = Classification.Type(dictionary["type"]),
+            instructions = dictionary["instructions"],
+            required = dictionary["required"],
+            options = [Option.from_dict(option) for option in dictionary["options"]],
+            schema_id = dictionary["schemaNodeId"],
+            feature_schema_id = dictionary["schemaNodeId"]
+        )
+
     def add_option(self, *args, **kwargs):
         new_option = Option(*args, **kwargs)
         if new_option.value in (option.value for option in self.options):
             raise InconsistentOntologyException(f"Duplicate option '{new_option.value}' for classification '{self.name}'.")
         self.options.append(new_option)
-        return new_option
 
 @dataclass
 class Tool:
@@ -95,6 +125,31 @@ class Tool:
     schema_id: Optional[str] = None
     feature_schema_id: Optional[str] = None
 
+    def to_dict(self) -> dict:
+        return {
+            "tool": self.tool.value,
+            "name": self.name,
+            "required": self.required,
+            "color": self.color,
+            "classifications": self.classifications,
+            "schemaNodeId": self.schema_id,
+            "featureSchemaId": self.feature_schema_id
+        }
+
+    @classmethod 
+    def from_dict(cls, dictionary: dict):
+        return Tool(
+            name = dictionary['name'],
+            schema_id = dictionary["schemaNodeId"],
+            feature_schema_id = dictionary["featureSchemaId"],
+            required = dictionary["required"],
+            tool = Tool.Type(dictionary["tool"]),   
+            classifications = [Classification.from_dict(classification) for classification in dictionary["classifications"]],
+            color = dictionary["color"]
+        )
+
+
+
 @dataclass
 class Ontology:
     
@@ -106,38 +161,23 @@ class Ontology:
         ontology = project.ontology().normalized
         return_ontology = Ontology()
 
-        for tool in ontology["tools"]:      
-            return_ontology.add_tool(
-                name=tool['name'],
-                schema_id=tool["schemaNodeId"],
-                feature_schema_id=tool["featureSchemaId"],
-                required=tool["required"],
-                tool=Tool.Type(tool["tool"]),                
-                classifications=tool["classifications"],
-                color=tool["color"],
-            )
+        for tool in ontology["tools"]: 
+            return_ontology.add_tool(tool)
 
         for classification in ontology["classifications"]:
-            return_ontology.add_classification(
-                schema_id=classification["schemaNodeId"], 
-                feature_schema_id=classification["schemaNodeId"],
-                required=classification["required"],
-                instructions=classification["instructions"],
-                class_type=Classification.Type(classification["type"]),
-                options = [Option(value=option["value"], schema_id=option["schemaNodeId"], feature_schema_id=option["featureSchemaId"]) for option in classification["options"] if len(classification["options"]) > 0]
-            )
+            return_ontology.add_classification(classification)
  
         return return_ontology
 
-    def add_tool(self, *args, **kwargs):
-        new_tool = Tool(*args, **kwargs)
+    def add_tool(self, tool: dict) -> Tool:
+        new_tool = Tool.from_dict(tool)
         if new_tool.name in (tool.name for tool in self.tools):
             raise InconsistentOntologyException(f"Duplicate tool name '{new_tool.name}'. ")
         self.tools.append(new_tool)
         return new_tool
 
-    def add_classification(self, *args, **kwargs):
-        new_classification = Classification(*args, **kwargs)
+    def add_classification(self, classification: dict) -> Classification:
+        new_classification = Classification.from_dict(classification)
         if new_classification.instructions in (classification.instructions for classification in self.classifications):
             raise InconsistentOntologyException(f"Duplicate classifications instructions '{new_classification.instructions}'. ")
         self.classifications.append(new_classification)
@@ -166,8 +206,7 @@ class Ontology:
                 "instructions": classification.instructions,
                 "name": classification.name,
                 "required": classification.required,
-                # "options": classification.options,
-                "options": [Option.option_class_to_dict(option) for option in classification.options],
+                "options": [option.to_dict() for option in classification.options],
                 "schemaNodeId": classification.schema_id,
                 "featureSchemaId": classification.feature_schema_id
             })
@@ -180,6 +219,17 @@ def run():
     frontend = list(client.get_labeling_frontends(where=LabelingFrontend.name == "Editor"))[0]
     project.setup(frontend, o.build())
     return project
+#also filler right here for now for testing
+def print_stuff():
+    tools = o.tools
+    classifications = o.classifications
+
+    print("tools\n")
+    for tool in tools:
+        print(tool)
+    print("\nclassifications\n")
+    for classification in classifications:
+        print(classification)
 
 if __name__ == "__main__":
     import json
@@ -189,12 +239,23 @@ if __name__ == "__main__":
     project = client.get_project("ckhchkye62xn30796uui5lu34")
 
     o = Ontology().from_project(project)
+    print_stuff()
     
-    o.add_tool(tool=Tool.Type.POLYGON, name="i am a polygon tool")
-    checklist = o.add_classification(class_type=Classification.Type.CHECKLIST, instructions="I AM A CHECKLIST")
-    checklist.add_option(value="checklist answer 1")
-    checklist.add_option(value="checklist answer 2")
+    
+    # o.add_tool(tool=Tool.Type.POLYGON, name="i am a polygon tool2")
+    # checklist = o.add_classification(class_type=Classification.Type.CHECKLIST, instructions="I AM A CHECKLIST2")
+    # checklist.add_option(value="checklist answer 1")
+    # checklist.add_option(value="checklist answer 2")
 
 
-    print(o.build())
-    run()
+    # print(o.build())
+    # print(type(o.build()))
+    # print(o.build())
+    # run()
+
+    
+
+
+
+
+
