07683e490428a20ceb2d0d2a9f67f964f3bc4343
Matt Sokoloff <msokoloff@labelbox.com>
2021-02-04 10:08:57 -0500

    format

--- labelbox/client.py
+++ labelbox/client.py
@@ -33,7 +33,6 @@ class Client:
     authentication key). Provides functions for querying and creating
     top-level data objects (Projects, Datasets).
     """
-
     def __init__(self,
                  api_key=None,
                  endpoint='https://api.labelbox.com/graphql'):
@@ -68,7 +67,7 @@ class Client:
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'Authorization': 'Bearer %s' % api_key,
-            'X-User-Agent' : f'python-sdk {SDK_VERSION}'
+            'X-User-Agent': f'python-sdk {SDK_VERSION}'
         }
 
     @retry.Retry(predicate=retry.if_exception_type(
@@ -113,10 +112,14 @@ class Client:
 
         if params is not None:
             params = {
-                key: convert_value(value) for key, value in params.items()
+                key: convert_value(value)
+                for key, value in params.items()
             }
 
-        data = json.dumps({'query': query, 'variables': params}).encode('utf-8')
+        data = json.dumps({
+            'query': query,
+            'variables': params
+        }).encode('utf-8')
 
         try:
             response = requests.post(self.endpoint,
@@ -270,18 +273,18 @@ class Client:
 
         request_data = {
             "operations":
-                json.dumps({
-                    "variables": {
-                        "file": None,
-                        "contentLength": len(content),
-                        "sign": False
-                    },
-                    "query":
-                        """mutation UploadFile($file: Upload!, $contentLength: Int!,
+            json.dumps({
+                "variables": {
+                    "file": None,
+                    "contentLength": len(content),
+                    "sign": False
+                },
+                "query":
+                """mutation UploadFile($file: Upload!, $contentLength: Int!,
                                             $sign: Boolean) {
                             uploadFile(file: $file, contentLength: $contentLength,
                                        sign: $sign) {url filename} } """,
-                }),
+            }),
             "map": (None, json.dumps({"1": ["variables.file"]})),
         }
         response = requests.post(
@@ -290,7 +293,7 @@ class Client:
             data=request_data,
             files={
                 "1": (filename, content, content_type) if
-                     (filename and content_type) else content
+                (filename and content_type) else content
             })
 
         try:
