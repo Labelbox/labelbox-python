81e96211cd4147de30725c11eefb4146aa48906f
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2021-03-09 20:12:56 -0800

    making pep8 format

--- labelbox/schema/ontology_generator.py
+++ labelbox/schema/ontology_generator.py
@@ -6,12 +6,9 @@ from typing import List, Optional, Dict, Any
 from labelbox import Client, Project, Dataset, LabelingFrontend
 
 
-class InconsistentOntologyException(Exception):
-    pass
-
 @dataclass
 class Option:
-    value: str    
+    value: str
     schema_id: Optional[str] = None
     feature_schema_id: Optional[str] = None
     options: List["Classification"] = field(default_factory=list)
@@ -19,23 +16,25 @@ class Option:
     @property
     def label(self):
         return self.value
-    
+
+    @classmethod
+    def from_dict(cls, dictionary: Dict[str, Any]):
+        return Option(value=dictionary["value"],
+                      schema_id=dictionary["schemaNodeId"],
+                      feature_schema_id=dictionary["featureSchemaId"],
+                      options=[
+                          Classification.from_dict(o)
+                          for o in dictionary.get("options", [])
+                      ])
+
     def asdict(self) -> Dict[str, Any]:
         return {
             "schemaNodeId": self.schema_id,
             "featureSchemaId": self.feature_schema_id,
             "label": self.label,
             "value": self.value,
-            "options": [o.asdict() for o in self.options]}
-
-    @classmethod
-    def from_dict(cls, dictionary: Dict[str,Any]):
-        return Option(
-            value = dictionary["value"],
-            schema_id = dictionary["schemaNodeId"],
-            feature_schema_id = dictionary["featureSchemaId"],
-            options = [Classification.from_dict(o) 
-                       for o in dictionary.get("options", [])])
+            "options": [o.asdict() for o in self.options]
+        }
 
     def add_option(self, option: 'Classification') -> 'Classification':
         if option.instructions in (o.instructions for o in self.options):
@@ -43,10 +42,10 @@ class Option:
                 f"Duplicate nested classification '{option.instructions}' "
                 f"for option '{self.label}'")
         self.options.append(option)
-    
-@dataclass
-class Classification:    
 
+
+@dataclass
+class Classification:
     class Type(Enum):
         TEXT = "text"
         CHECKLIST = "checklist"
@@ -66,9 +65,19 @@ class Classification:
     def name(self):
         return self.instructions
 
-    def asdict(self) -> Dict[str,Any]:
+    @classmethod
+    def from_dict(cls, dictionary: Dict[str, Any]):
+        return Classification(
+            class_type=Classification.Type(dictionary["type"]),
+            instructions=dictionary["instructions"],
+            required=dictionary["required"],
+            options=[Option.from_dict(o) for o in dictionary["options"]],
+            schema_id=dictionary["schemaNodeId"],
+            feature_schema_id=dictionary["schemaNodeId"])
+
+    def asdict(self) -> Dict[str, Any]:
         if self.class_type in Classification._REQUIRES_OPTIONS \
-                and len(self.options) < 1: 
+                and len(self.options) < 1:
             raise InconsistentOntologyException(
                 f"Classification '{self.instructions}' requires options.")
         return {
@@ -78,17 +87,8 @@ class Classification:
             "required": self.required,
             "options": [o.asdict() for o in self.options],
             "schemaNodeId": self.schema_id,
-            "featureSchemaId": self.feature_schema_id}   
-
-    @classmethod
-    def from_dict(cls, dictionary: Dict[str,Any]): 
-        return Classification(
-            class_type = Classification.Type(dictionary["type"]),
-            instructions = dictionary["instructions"],
-            required = dictionary["required"],
-            options = [Option.from_dict(o) for o in dictionary["options"]],
-            schema_id = dictionary["schemaNodeId"],
-            feature_schema_id = dictionary["schemaNodeId"])
+            "featureSchemaId": self.feature_schema_id
+        }
 
     def add_option(self, option: Option):
         if option.value in (o.value for o in self.options):
@@ -97,9 +97,9 @@ class Classification:
                 f"for classification '{self.name}'.")
         self.options.append(option)
 
+
 @dataclass
 class Tool:
-
     class Type(Enum):
         POLYGON = "polygon"
         SEGMENTATION = "superpixel"
@@ -108,15 +108,28 @@ class Tool:
         LINE = "line"
         NER = "named-entity"
 
-    tool: Type 
-    name: str 
+    tool: Type
+    name: str
     required: bool = False
     color: str = "#000000"
     classifications: List[Classification] = field(default_factory=list)
     schema_id: Optional[str] = None
     feature_schema_id: Optional[str] = None
 
-    def asdict(self) -> Dict[str,Any]:
+    @classmethod
+    def from_dict(cls, dictionary: Dict[str, Any]):
+        return Tool(name=dictionary['name'],
+                    schema_id=dictionary["schemaNodeId"],
+                    feature_schema_id=dictionary["featureSchemaId"],
+                    required=dictionary["required"],
+                    tool=Tool.Type(dictionary["tool"]),
+                    classifications=[
+                        Classification.from_dict(c)
+                        for c in dictionary["classifications"]
+                    ],
+                    color=dictionary["color"])
+
+    def asdict(self) -> Dict[str, Any]:
         return {
             "tool": self.tool.value,
             "name": self.name,
@@ -124,59 +137,54 @@ class Tool:
             "color": self.color,
             "classifications": [c.asdict() for c in self.classifications],
             "schemaNodeId": self.schema_id,
-            "featureSchemaId": self.feature_schema_id}
-
-    @classmethod 
-    def from_dict(cls, dictionary: Dict[str,Any]):
-        return Tool(
-            name = dictionary['name'],
-            schema_id = dictionary["schemaNodeId"],
-            feature_schema_id = dictionary["featureSchemaId"],
-            required = dictionary["required"],
-            tool = Tool.Type(dictionary["tool"]), 
-            classifications = [Classification.from_dict(c) 
-                               for c in dictionary["classifications"]],
-            color = dictionary["color"])
+            "featureSchemaId": self.feature_schema_id
+        }
 
     def add_classification(self, classification: Classification):
-        if classification.instructions in (c.instructions 
+        if classification.instructions in (c.instructions
                                            for c in self.classifications):
             raise InconsistentOntologyException(
                 f"Duplicate nested classification '{classification.instructions}' "
                 f"for tool '{self.name}'")
-        self.classifications.append(classification)      
+        self.classifications.append(classification)
+
 
 @dataclass
 class Ontology:
-    
+
     tools: List[Tool] = field(default_factory=list)
     classifications: List[Classification] = field(default_factory=list)
+
+    @classmethod
+    def from_dict(cls, dictionary: Dict[str, Any]):
+        return Ontology(tools=[Tool.from_dict(t) for t in dictionary["tools"]],
+                        classifications=[
+                            Classification.from_dict(c)
+                            for c in dictionary["classifications"]
+                        ])
+
+    def asdict(self):
+        return {
+            "tools": [t.asdict() for t in self.tools],
+            "classifications": [c.asdict() for c in self.classifications]
+        }
 
     @classmethod
     def from_project(cls, project: Project):
         ontology = project.ontology().normalized
         return Ontology.from_dict(ontology)
 
-    def add_tool(self, tool: Tool) -> Tool: 
+    def add_tool(self, tool: Tool) -> Tool:
         if tool.name in (t.name for t in self.tools):
             raise InconsistentOntologyException(
                 f"Duplicate tool name '{tool.name}'. ")
         self.tools.append(tool)
-   
-    def add_classification(self, classification: Classification) -> Classification:
-        if classification.instructions in (c.instructions 
+
+    def add_classification(self,
+                           classification: Classification) -> Classification:
+        if classification.instructions in (c.instructions
                                            for c in self.classifications):
             raise InconsistentOntologyException(
-                f"Duplicate classification instructions '{classification.instructions}'. ")
+                f"Duplicate classification instructions '{classification.instructions}'. "
+            )
         self.classifications.append(classification)
-
-    def asdict(self):
-        return {
-            "tools": [t.asdict() for t in self.tools],
-            "classifications": [c.asdict() for c in self.classifications]}
-
-    @classmethod
-    def from_dict(cls, dictionary: Dict[str,Any]):
-        return Ontology(
-            tools = [Tool.from_dict(t) for t in dictionary["tools"]],
-            classifications = [Classification.from_dict(c) for c in dictionary["classifications"]])
--- tests/integration/test_ontology.py
+++ tests/integration/test_ontology.py
@@ -6,175 +6,205 @@ from labelbox import LabelingFrontend
 from labelbox.schema.ontology_generator import Ontology, \
     Tool, Classification, Option, InconsistentOntologyException
 
-
 _SAMPLE_ONTOLOGY = {
-        "tools": [{
-            "schemaNodeId": None,
-            "featureSchemaId": None,
-            "required": False,
-            "name": "poly",
-            "color": "#FF0000",
-            "tool": "polygon",
-            "classifications": []
-        }, {
-            "schemaNodeId": None,
-            "featureSchemaId": None,
-            "required": False,
-            "name": "segment",
-            "color": "#FF0000",
-            "tool": "superpixel",
-            "classifications": []
-        }, {
-            "schemaNodeId": None,
-            "featureSchemaId": None,
-            "required": False,
-            "name": "bbox",
-            "color": "#FF0000",
-            "tool": "rectangle",
-            "classifications": [{
-                "schemaNodeId": None,
-                "featureSchemaId": None,
-                "required": True,
-                "instructions": "nested classification",
-                "name": "nested classification",
-                "type": "radio",
+    "tools": [{
+        "schemaNodeId": None,
+        "featureSchemaId": None,
+        "required": False,
+        "name": "poly",
+        "color": "#FF0000",
+        "tool": "polygon",
+        "classifications": []
+    }, {
+        "schemaNodeId": None,
+        "featureSchemaId": None,
+        "required": False,
+        "name": "segment",
+        "color": "#FF0000",
+        "tool": "superpixel",
+        "classifications": []
+    }, {
+        "schemaNodeId":
+        None,
+        "featureSchemaId":
+        None,
+        "required":
+        False,
+        "name":
+        "bbox",
+        "color":
+        "#FF0000",
+        "tool":
+        "rectangle",
+        "classifications": [{
+            "schemaNodeId":
+            None,
+            "featureSchemaId":
+            None,
+            "required":
+            True,
+            "instructions":
+            "nested classification",
+            "name":
+            "nested classification",
+            "type":
+            "radio",
+            "options": [{
+                "schemaNodeId":
+                None,
+                "featureSchemaId":
+                None,
+                "label":
+                "first",
+                "value":
+                "first",
                 "options": [{
                     "schemaNodeId": None,
                     "featureSchemaId": None,
-                    "label": "first",
-                    "value": "first",
-                    "options": [{
-                        "schemaNodeId": None,
-                        "featureSchemaId": None,
-                        "required": False,
-                        "instructions": "nested nested text",
-                        "name": "nested nested text",
-                        "type": "text",
-                        "options": []
-                    }]
-                }, {
-                    "schemaNodeId": None,
-                    "featureSchemaId": None,
-                    "label": "second",
-                    "value": "second",
+                    "required": False,
+                    "instructions": "nested nested text",
+                    "name": "nested nested text",
+                    "type": "text",
                     "options": []
                 }]
             }, {
                 "schemaNodeId": None,
                 "featureSchemaId": None,
-                "required": True,
-                "instructions": "nested text",
-                "name": "nested text",
-                "type": "text",
+                "label": "second",
+                "value": "second",
                 "options": []
             }]
         }, {
-            "schemaNodeId": None,
-            "featureSchemaId": None,
-            "required": False,
-            "name": "dot",
-            "color": "#FF0000",
-            "tool": "point",
-            "classifications": []
-        }, {
-            "schemaNodeId": None,
-            "featureSchemaId": None,
-            "required": False,
-            "name": "polyline",
-            "color": "#FF0000",
-            "tool": "line",
-            "classifications": []
-        }, {
-            "schemaNodeId": None,
-            "featureSchemaId": None,
-            "required": False,
-            "name": "ner",
-            "color": "#FF0000",
-            "tool": "named-entity",
-            "classifications": []
-        }],
-        "classifications": [{
             "schemaNodeId": None,
             "featureSchemaId": None,
             "required": True,
-            "instructions": "This is a question.",
-            "name": "This is a question.",
-            "type": "radio",
-            "options": [{
-                "schemaNodeId": None,
-                "featureSchemaId": None,
-                "label": "yes",
-                "value": "yes",
-                "options": []
-            }, {
-                "schemaNodeId": None,
-                "featureSchemaId": None,
-                "label": "no",
-                "value": "no",
-                "options": []
-            }]
+            "instructions": "nested text",
+            "name": "nested text",
+            "type": "text",
+            "options": []
         }]
-    }
+    }, {
+        "schemaNodeId": None,
+        "featureSchemaId": None,
+        "required": False,
+        "name": "dot",
+        "color": "#FF0000",
+        "tool": "point",
+        "classifications": []
+    }, {
+        "schemaNodeId": None,
+        "featureSchemaId": None,
+        "required": False,
+        "name": "polyline",
+        "color": "#FF0000",
+        "tool": "line",
+        "classifications": []
+    }, {
+        "schemaNodeId": None,
+        "featureSchemaId": None,
+        "required": False,
+        "name": "ner",
+        "color": "#FF0000",
+        "tool": "named-entity",
+        "classifications": []
+    }],
+    "classifications": [{
+        "schemaNodeId":
+        None,
+        "featureSchemaId":
+        None,
+        "required":
+        True,
+        "instructions":
+        "This is a question.",
+        "name":
+        "This is a question.",
+        "type":
+        "radio",
+        "options": [{
+            "schemaNodeId": None,
+            "featureSchemaId": None,
+            "label": "yes",
+            "value": "yes",
+            "options": []
+        }, {
+            "schemaNodeId": None,
+            "featureSchemaId": None,
+            "label": "no",
+            "value": "no",
+            "options": []
+        }]
+    }]
+}
+
 
 @pytest.mark.parametrize("tool_type", list(Tool.Type))
 def test_create_tool(tool_type) -> None:
-    t = Tool(tool = tool_type, name = "tool")
-    assert(t.tool == tool_type)
+    t = Tool(tool=tool_type, name="tool")
+    assert (t.tool == tool_type)
+
 
 @pytest.mark.parametrize("class_type", list(Classification.Type))
 def test_create_classification(class_type) -> None:
-    c = Classification(class_type = class_type, instructions = "classification")
-    assert(c.class_type == class_type)
+    c = Classification(class_type=class_type, instructions="classification")
+    assert (c.class_type == class_type)
 
-@pytest.mark.parametrize(
-    "value, expected_value, typing",[(3,3, int),("string","string", str)])
+
+@pytest.mark.parametrize("value, expected_value, typing",
+                         [(3, 3, int), ("string", "string", str)])
 def test_create_option(value, expected_value, typing) -> None:
-    o = Option(value = value)
-    assert(o.value == expected_value)
-    assert(o.value == o.label)
+    o = Option(value=value)
+    assert (o.value == expected_value)
+    assert (o.value == o.label)
+
 
 def test_create_empty_ontology() -> None:
     o = Ontology()
-    assert(o.tools == [])
-    assert(o.classifications == [])
+    assert (o.tools == [])
+    assert (o.classifications == [])
+
 
 def test_add_ontology_tool() -> None:
     o = Ontology()
-    o.add_tool(Tool(tool = Tool.Type.BBOX, name = "bounding box"))
+    o.add_tool(Tool(tool=Tool.Type.BBOX, name="bounding box"))
 
-    second_tool = Tool(tool = Tool.Type.SEGMENTATION, name = "segmentation")
+    second_tool = Tool(tool=Tool.Type.SEGMENTATION, name="segmentation")
     o.add_tool(second_tool)
     assert len(o.tools) == 2
 
     for tool in o.tools:
-        assert(type(tool) == Tool)    
+        assert (type(tool) == Tool)
 
     with pytest.raises(InconsistentOntologyException) as exc:
-        o.add_tool(Tool(tool=Tool.Type.BBOX, name = "bounding box"))
+        o.add_tool(Tool(tool=Tool.Type.BBOX, name="bounding box"))
     assert "Duplicate tool name" in str(exc.value)
+
 
 def test_add_ontology_classification() -> None:
     o = Ontology()
-    o.add_classification(Classification(
-        class_type = Classification.Type.TEXT, instructions = "text"))
+    o.add_classification(
+        Classification(class_type=Classification.Type.TEXT,
+                       instructions="text"))
 
     second_classification = Classification(
-        class_type = Classification.Type.CHECKLIST, instructions = "checklist")
+        class_type=Classification.Type.CHECKLIST, instructions="checklist")
     o.add_classification(second_classification)
     assert len(o.classifications) == 2
 
     for classification in o.classifications:
-        assert(type(classification) == Classification)
+        assert (type(classification) == Classification)
 
     with pytest.raises(InconsistentOntologyException) as exc:
-        o.add_classification(Classification(
-            class_type = Classification.Type.TEXT, instructions = "text"))
+        o.add_classification(
+            Classification(class_type=Classification.Type.TEXT,
+                           instructions="text"))
     assert "Duplicate classification instructions" in str(exc.value)
 
+
 def test_tool_add_classification() -> None:
-    t = Tool(tool = Tool.Type.SEGMENTATION, name = "segmentation")
-    c = Classification(
-        class_type = Classification.Type.TEXT, instructions = "text")
+    t = Tool(tool=Tool.Type.SEGMENTATION, name="segmentation")
+    c = Classification(class_type=Classification.Type.TEXT,
+                       instructions="text")
     t.add_classification(c)
     assert t.classifications == [c]
 
@@ -182,30 +212,34 @@ def test_tool_add_classification() -> None:
         t.add_classification(c)
     assert "Duplicate nested classification" in str(exc)
 
+
 def test_classification_add_option() -> None:
-    c = Classification(
-        class_type = Classification.Type.RADIO, instructions = "radio")
-    o = Option(value = "option")
+    c = Classification(class_type=Classification.Type.RADIO,
+                       instructions="radio")
+    o = Option(value="option")
     c.add_option(o)
     assert c.options == [o]
 
     with pytest.raises(InconsistentOntologyException) as exc:
-        c.add_option(Option(value = "option"))
+        c.add_option(Option(value="option"))
     assert "Duplicate option" in str(exc.value)
 
+
 def test_option_add_option() -> None:
-    o = Option(value = "option")
-    c = Classification(
-        class_type = Classification.Type.TEXT, instructions = "text")
+    o = Option(value="option")
+    c = Classification(class_type=Classification.Type.TEXT,
+                       instructions="text")
     o.add_option(c)
     assert o.options == [c]
 
     with pytest.raises(InconsistentOntologyException) as exc:
         o.add_option(c)
-    assert "Duplicate nested classification" in str(exc.value)    
+    assert "Duplicate nested classification" in str(exc.value)
+
 
 def test_ontology_asdict(project) -> None:
     assert Ontology.from_dict(_SAMPLE_ONTOLOGY).asdict() == _SAMPLE_ONTOLOGY
+
 
 def test_from_project_ontology(client, project) -> None:
     o = Ontology.from_project(project)
