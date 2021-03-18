a5a5a58083f1c4544e6798c004f9a8b5b8c9cb4f
Matt Sokoloff <msokoloff@labelbox.com>
2021-03-17 21:19:15 -0400

    fix function names

--- examples/model_assisted_labeling/image_mal_utils.py
+++ examples/model_assisted_labeling/image_mal_utils.py
@@ -4,7 +4,7 @@ import requests
 from io import BytesIO
 from PIL import Image
 
-def visualize_bbox_upload(image, bbox_ndjsons, color):
+def visualize_bbox_ndjsons(image, bbox_ndjsons, color):
     for bbox_ndjson in bbox_ndjsons:
         bbox = bbox_ndjson['bbox']
         start_point = (int(bbox['left']), int(bbox['top']))
@@ -12,20 +12,20 @@ def visualize_bbox_upload(image, bbox_ndjsons, color):
         image = cv2.rectangle(image, start_point, end_point, thickness = 2, color = color)
     return image
 
-def visualize_poly_upload(image, poly_ndjsons, color):
+def visualize_poly_ndjsons(image, poly_ndjsons, color):
     for poly_ndjson in poly_ndjsons:
         pts = [[poly['x'], poly['y']] for poly in poly_ndjson['polygon']]
         pts = np.array(pts).astype(np.int32)
         image = cv2.polylines(image,[pts],True,color, thickness = 2)
     return image
 
-def visualize_point_upload(image, point_ndjsons, color):
+def visualize_point_ndjsons(image, point_ndjsons, color):
     for point_ndjson in point_ndjsons:
         point = point_ndjson['point']
         image = cv2.circle(image, (point['x'],point['y']), radius=10, color=color, thickness=-1)
     return image
 
-def visualize_mask_uploads(image, mask_ndjsons):
+def visualize_mask_ndjsons(image, mask_ndjsons):
     masks = []
     for ndjson in mask_ndjsons:
         instanceURI = ndjson['mask']['instanceURI']
