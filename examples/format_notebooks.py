f27ed2128155b797c0e3ad538cd55837d3bd593d
Matt Sokoloff <msokoloff@labelbox.com>
2021-03-30 17:16:48 -0400

    formatted, added table to readme, small style changes

---
+++ examples/format_notebooks.py
@@ -0,0 +1,34 @@
+from yapf.yapflib.yapf_api import FormatCode
+import json
+import glob
+
+def format_cell(source):
+    for line in source.split('\n'):
+        if line.strip().startswith(('!', '%')):
+            return source
+    return FormatCode(source, style_config = "google")[0]
+
+def format_file(file_name):
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+
+    idx = 1
+    for cell in data['cells']:
+        if cell['cell_type'] == 'code':
+            cell['execution_count'] = idx
+            if isinstance(cell['source'], list):
+                cell['source'] = ''.join(cell['source'])
+            cell['source'] = format_cell(cell['source'])
+            idx += 1
+            if cell['source'].endswith('\n'):
+                 cell['source'] = cell['source'][:-1]
+
+    with open(file_name, 'w') as file:
+        file.write(json.dumps(data))
+    print("Formatted", file_name)
+
+
+if __name__ == '__main__':
+    for file in glob.glob("*/*.ipynb"):
+        format_file(file)
+
