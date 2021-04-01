cd80ca996e416c0defc9bccec1d623ef4e39c64c
Matt Sokoloff <msokoloff@labelbox.com>
2021-04-01 14:10:58 -0400

    yapf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,9 +52,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # Prevent the sidebar from collapsing
-html_js_files = [
-    'js/prevent_collapse.js'
-]
+html_js_files = ['js/prevent_collapse.js']
 html_theme_options = {
     "collapse_navigation": False,
 }
