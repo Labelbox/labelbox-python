02203a6b8cc4624643023f9b4d43964669a9c355
Matt Sokoloff <msokoloff@labelbox.com>
2021-04-23 10:19:39 -0400

    style

--- labelbox/orm/query.py
+++ labelbox/orm/query.py
@@ -40,12 +40,15 @@ def results_query_part(entity):
     Args:
         entity (type): The entity which needs fetching.
     """
+    # Query for fields
     fields = [field.graphql_name for field in entity.fields()]
-    for relationship in entity.relationships():
-        if relationship.cache:
-            fields.append(
-                Query(relationship.graphql_name,
-                      relationship.destination_type).format()[0])
+
+    # Query for cached relationships
+    fields.extend([
+        Query(rel.graphql_name, rel.destination_type).format()[0]
+        for rel in entity.relationships()
+        if rel.cache
+    ])
     return " ".join(fields)
 
 
