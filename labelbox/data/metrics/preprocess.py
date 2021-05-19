fa4e720e6ddf5e0cab0618c414e2dfb97f09d2b0
Matt Sokoloff <msokoloff@labelbox.com>
2021-05-19 09:37:38 -0400

    clean up

--- labelbox/data/metrics/preprocess.py
+++ labelbox/data/metrics/preprocess.py
@@ -1,32 +1,34 @@
-from typing import DefaultDict
+from typing import List, Dict, Any
+from collections import defaultdict
 from shapely.geometry import Polygon, box
 import numpy as np
 from PIL import Image
 import requests
 
 
-def bbox_to_shapely_poly(bbox):
-    return box(bbox['left'], bbox['top'], bbox['left'] + bbox['width'],
-               bbox['top'] + bbox['height'])
-
-
-def poly_to_shapely_poly(poly):
-    return Polygon([[pt['x'], pt['y']] for pt in poly])
-
-
-def to_shapely_polys(tool, keys):
-    if 'polygon' in keys:
-        return [poly_to_shapely_poly(poly['polygon']) for poly in tool]
-    else:
-        return [bbox_to_shapely_poly(bbox['bbox']) for bbox in tool]
-
-
-def lookup_by_schema(rows):
-    data = DefaultDict(list)
+def create_schema_lookup(rows: Dict[str, Any]) -> Dict[str, List[Any]]:
+    data = defaultdict(list)
     for row in rows:
         data[row['schemaId']].append(row)
     return data
 
 
-def url_to_numpy(mask_url):
+def url_to_numpy(mask_url: str) -> np.ndarray:
     return np.array(Image.open(requests.get(mask_url).raw))
+
+
+def _bbox_to_shapely_poly(bbox: Dict[str, float]) -> Polygon:
+    return box(bbox['left'], bbox['top'], bbox['left'] + bbox['width'],
+               bbox['top'] + bbox['height'])
+
+
+def _poly_to_shapely_poly(poly: List[Dict[str, float]]) -> Polygon:
+    return Polygon([[pt['x'], pt['y']] for pt in poly])
+
+
+def to_shapely_polys(tool: List[Dict[str, Any]],
+                     keys: List[str]) -> List[Polygon]:
+    if 'polygon' in keys:
+        return [_poly_to_shapely_poly(poly['polygon']) for poly in tool]
+    else:
+        return [_bbox_to_shapely_poly(bbox['bbox']) for bbox in tool]
