092b38c9f2a53df28a0a3317365148f4329e9d89
Matt Sokoloff <msokoloff@labelbox.com>
2021-05-19 09:37:38 -0400

    clean up

--- labelbox/data/metrics/iou.py
+++ labelbox/data/metrics/iou.py
@@ -1,5 +1,5 @@
-from labelbox.data.metrics.preprocess import lookup_by_schema, to_shapely_polys, url_to_numpy
-from typing import Dict, Any, List
+from labelbox.data.metrics.preprocess import create_schema_lookup, to_shapely_polys, url_to_numpy
+from typing import Dict, Any, List, Optional
 from shapely.geometry import Polygon
 from itertools import product
 import numpy as np
@@ -8,37 +8,29 @@ SUPPORTED_TYPES = {'segmentation', 'polygon', 'bbox'}
 ALL_TYPES = {'bbox', 'polygon', 'line', 'point', 'segmentation'}
 
 
-def polygon_iou(p1: Polygon, p2: Polygon) -> float:
-    if p1.intersects(p2):
-        return p1.intersection(p2).area / p1.union(p2).area
-    return 0.
-
-
-def mask_iou(tool_pred, tool_label):
+def mask_iou(predictions: List[Dict[str, Any]],
+             labels: List[Dict[str, Any]]) -> float:
     """
     Mask iou is performed at the class level and not the instance level.
     """
-    # This is kind of inefficient. The mask exists locally for them to upload it.
+    # This is inefficient. The mask exists locally for them to upload it.
     # RLE data would be convenient here.
-    assert len(tool_label) == 1
-
     # There will only ever be one mask label per class
-    label = url_to_numpy(tool_label[0]['instanceURI'])
-    pred_masks = [url_to_numpy(x['instanceURI']) for x in tool_pred]
-
-    label = np.sum(label, axis=-1) > 0
-    pred_mask = np.sum(pred_masks, axis=(0, 3)) > 0
-
-    assert label.shape == pred_mask.shape
-    return np.sum(label & pred_mask) / np.sum(label | pred_mask)
+    label_mask = _instance_urls_to_binary_mask(
+        [pred['instanceURI'] for pred in predictions])
+    pred_mask = _instance_urls_to_binary_mask(
+        [label['instanceURI'] for label in labels])
+    assert label_mask.shape == pred_mask.shape
+    return _mask_iou(label_mask, pred_mask)
 
 
-def vector_iou(tool_pred, tool_label, keys):
+def vector_iou(predictions: List[Dict[str, Any]], labels: List[Dict[str, Any]],
+               keys: Dict[str, Any]) -> float:
     agreements = [(feature_a['uuid'], feature_b['featureId'],
-                   polygon_iou(polygon_a, polygon_b))
+                   _polygon_iou(polygon_a, polygon_b))
                   for (feature_a, polygon_a), (feature_b, polygon_b) in product(
-                      zip(tool_pred, to_shapely_polys(tool_pred, keys)),
-                      zip(tool_label, to_shapely_polys(tool_label, keys)))
+                      zip(predictions, to_shapely_polys(predictions, keys)),
+                      zip(labels, to_shapely_polys(labels, keys)))
                   if polygon_a is not None and polygon_b is not None]
 
     agreements = [(feature_a, feature_b, agreement)
@@ -59,7 +51,8 @@ def vector_iou(tool_pred, tool_label, keys):
     return np.mean(solution_agreements)
 
 
-def feature_miou(predictions, labels):
+def feature_miou(predictions: List[Dict[str, Any]],
+                 labels: List[Dict[str, Any]]) -> Optional[float]:
     if len(predictions):
         keys = predictions[0]
     elif len(labels):
@@ -79,7 +72,7 @@ def feature_miou(predictions, labels):
             f"Unexpected annotation found. Must be one of {SUPPORTED_TYPES}")
 
 
-def datarow_miou(label_content: Dict[str, Any],
+def datarow_miou(label_content: List[Dict[str, Any]],
                  ndjsons: List[Dict[str, Any]]) -> float:
     """
     label_content is the bulk_export['Label']['objects']
@@ -89,8 +82,8 @@ def datarow_miou(label_content: Dict[str, Any],
 
     # We are assuming that these are all valid payloads ..
     """
-    labels = lookup_by_schema(label_content)
-    predictions = lookup_by_schema(ndjsons)
+    labels = create_schema_lookup(label_content)
+    predictions = create_schema_lookup(ndjsons)
     feature_schemas = set(predictions.keys()).union(set(labels.keys()))
     # TODO: Do we need to weight this by the number of examples in a class?
     # What does B&C do?
@@ -104,3 +97,18 @@ def datarow_miou(label_content: Dict[str, Any],
         # TODO: Figure out the behavior we want for this.
         raise ValueError("No predictions or labels found for this example....")
     return np.mean(ious)
+
+
+def _polygon_iou(poly1: Polygon, poly2: Polygon) -> float:
+    if poly1.intersects(poly2):
+        return poly1.intersection(poly2).area / poly1.union(poly2).area
+    return 0.
+
+
+def _mask_iou(mask1: np.ndarray, mask2: np.ndarray) -> float:
+    return np.sum(mask1 & mask2) / np.sum(mask1 | mask2)
+
+
+def _instance_urls_to_binary_mask(urls: List[str]) -> np.ndarray:
+    masks = [url_to_numpy(url) for url in urls]
+    return np.sum(masks, axis=(0, 3)) > 0
