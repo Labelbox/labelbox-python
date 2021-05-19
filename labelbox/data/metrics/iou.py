1b5bb99f5ed4494fdbd1055035ca53510675886b
Matt Sokoloff <msokoloff@labelbox.com>
2021-05-19 08:37:30 -0400

    add metrics

--- labelbox/data/metrics/iou.py
+++ labelbox/data/metrics/iou.py
@@ -1,14 +1,11 @@
+from labelbox.data.metrics.preprocess import lookup_by_schema, to_shapely_polys, url_to_numpy
 from typing import Dict, Any, List
-from labelbox import Label
-from shapely.geometry import Polygon, polygon
+from shapely.geometry import Polygon
 from itertools import product
-from collections import defaultdict
 import numpy as np
-from io import BytesIO
-from PIL import Image
-import requests
 
-SUPPORTED_TYPES = {'mask', 'polygon', 'bbox'}
+SUPPORTED_TYPES = {'segmentation', 'polygon', 'bbox'}
+ALL_TYPES = {'bbox', 'polygon', 'line', 'point', 'segmentation'}
 
 
 def polygon_iou(p1: Polygon, p2: Polygon) -> float:
@@ -16,93 +13,94 @@ def polygon_iou(p1: Polygon, p2: Polygon) -> float:
         return p1.intersection(p2).area / p1.union(p2).area
     return 0.
 
-def mask_iou(m1 : np.bool, m2 : np.bool) -> float:
+
+def mask_iou(tool_pred, tool_label):
     """
     Mask iou is performed at the class level and not the instance level.
-
     """
-    return np.sum(m1 & m2) / np.sum( m1 | m2 )
+    # This is kind of inefficient. The mask exists locally for them to upload it.
+    # RLE data would be convenient here.
+    assert len(tool_label) == 1
+
+    # There will only ever be one mask label per class
+    label = url_to_numpy(tool_label[0]['instanceURI'])
+    pred_masks = [url_to_numpy(x['instanceURI']) for x in tool_pred]
+
+    label = np.sum(label, axis=-1) > 0
+    pred_mask = np.sum(pred_masks, axis=(0, 3)) > 0
+
+    assert label.shape == pred_mask.shape
+    return np.sum(label & pred_mask) / np.sum(label | pred_mask)
 
 
-def datarow_miou(label_content: Dict[str, Any], ndjsons : List[Dict[str, Any]]) -> float:
+def vector_iou(tool_pred, tool_label, keys):
+    agreements = [(feature_a['uuid'], feature_b['featureId'],
+                   polygon_iou(polygon_a, polygon_b))
+                  for (feature_a, polygon_a), (feature_b, polygon_b) in product(
+                      zip(tool_pred, to_shapely_polys(tool_pred, keys)),
+                      zip(tool_label, to_shapely_polys(tool_label, keys)))
+                  if polygon_a is not None and polygon_b is not None]
+
+    agreements = [(feature_a, feature_b, agreement)
+                  for feature_a, feature_b, agreement in agreements]
+    agreements.sort(key=lambda triplet: triplet[2], reverse=True)
+    solution_agreements = []
+    solution_features = set()
+    all_features = set()
+    for a, b, agreement in agreements:
+        all_features.update({a, b})
+        if a not in solution_features and b not in solution_features:
+            solution_features.update({a, b})
+            solution_agreements.append(agreement)
+
+    # Add zeros for unmatched Features
+    solution_agreements.extend([0.0] *
+                               (len(all_features) - len(solution_features)))
+    return np.mean(solution_agreements)
+
+
+def feature_miou(predictions, labels):
+    if len(predictions):
+        keys = predictions[0]
+    elif len(labels):
+        # No existing predictions but existing labels means no matches.
+        return 0.0
+    else:
+        # Ignore examples that do not have any labels or predictions
+        return None
+
+    tool = (set(keys) & ALL_TYPES or {"segmentation"}).pop()
+    if tool == 'segmentation':
+        return mask_iou(predictions, labels)
+    elif tool in {'polygon', 'bbox'}:
+        return vector_iou(predictions, labels, keys)
+    else:
+        raise ValueError(
+            f"Unexpected annotation found. Must be one of {SUPPORTED_TYPES}")
+
+
+def datarow_miou(label_content: Dict[str, Any],
+                 ndjsons: List[Dict[str, Any]]) -> float:
     """
+    label_content is the bulk_export['Label']['objects']
+
     label: Label in the bulk export format
     ndjson: All ndjsons that have the same datarow as the label (must be a polygon, mask, or box ndjson).
 
     # We are assuming that these are all valid payloads ..
     """
-    labels = defaultdict(list)
-    predictions = defaultdict(list)
-    for annot in label_content:
-        labels[annot['schemaId']].append(annot)
-
-    for pred in ndjsons:
-        predictions[pred['featureSchema']['Id']].append(pred)
-
-    feature_schemas = set(pred.keys()).union(set(labels.keys()))
+    labels = lookup_by_schema(label_content)
+    predictions = lookup_by_schema(ndjsons)
+    feature_schemas = set(predictions.keys()).union(set(labels.keys()))
     # TODO: Do we need to weight this by the number of examples in a class?
     # What does B&C do?
-    ious = [] 
+    ious = []
+    # This weights each class equally so rare classes will have a larger impact on the iou
     for feature_schema in feature_schemas:
-        if len(predictions[feature_schema]):
-            keys = predictions[feature_schema][0]
-        elif len(labels[feature_schema]):
-            # No existing predictions but existing labels means no matches.
-            ious.append(0.0)
-        else:
-            continue # I guess we just ignore these
-
-        if 'mask' in keys:
-            # This is kind of inefficient. The mask exists locally for them to upload it.
-            assert len(labels[feature_schema]) == 1
-            label = np.array(Image.open(BytesIO(requests.get(labels[feature_schema][0]['mask']))))
-            label = np.sum(label, axis = -1) > 0
-            pred_masks = [np.array(Image.open(BytesIO(requests.get(x['mask'])))) for x in predictions[feature_schema]]
-            pred_mask = np.sum(pred_masks, axis = (0, 3)) > 0
-            assert label.shape == pred_mask.shape
-            ious.append(mask_iou(label, pred_mask))
-        elif 'polygon' in keys or 'bbox' in keys:
-            if 'polygon' in keys:
-                key = 'polygon'
-            else:
-                key = 'bbox'
-            pred_geoms = [Polygon([[pt['x'], pt['y']] for pt in x[key]]) for x in predictions[feature_schema]]
-            label_geoms = [Polygon([[pt['x'], pt['y']] for pt in x[key]]) for x in labels[feature_schema]]
-
-            agreements = [(feature_a, feature_b, polygon_iou(polygon_a, polygon_b))
-            for (feature_a, polygon_a), (feature_b, polygon_b)
-            in product(zip(predictions[feature_schema], pred_geoms),
-                                 zip(labels[feature_schema], label_geoms))
-            if polygon_a is not None and polygon_b is not None]
-            
-            agreements = [(feature_a["feature_id"], feature_b["feature_id"], agreement) for feature_a, feature_b, agreement in agreements]
-            agreements.sort(key=lambda triplet: triplet[2], reverse=True)            
-            solution_agreements = []
-            solution_features = set()
-            all_features = set()
-            for a, b, agreement in agreements:
-                all_features.update({a, b})
-                if a not in solution_features and b not in solution_features:
-                    solution_features.update({a, b})
-                    solution_agreements.append(agreement)
-
-            # Add zeros for unmatched Features
-            solution_agreements.extend(
-                [0.0] * (len(all_features) - len(solution_features)))
-            ious.append(np.mean(solution_agreements))
-        else:
-            raise ValueError(f"Unexpected annotation found. Must be one of {SUPPORTED_TYPES}")
-
-
-        
-
-
-
-
-    
-
-
-
-    
-
-
+        ious.append(
+            feature_miou(predictions[feature_schema], labels[feature_schema]))
+    ious = [iou for iou in ious if iou is not None]
+    if not ious:
+        # TODO: Figure out the behavior we want for this.
+        raise ValueError("No predictions or labels found for this example....")
+    return np.mean(ious)
