c1cc1b261210ef1fcfb944f02f2748cd17fafe2d
Matt Sokoloff <msokoloff@labelbox.com>
2021-06-03 08:33:04 -0400

    works

--- labelbox/schema/model_run.py
+++ labelbox/schema/model_run.py
@@ -6,10 +6,10 @@ class ModelRun(DbObject):
     name =  Field.String("name")
     updated_at = Field.DateTime("updated_at")
     created_at = Field.DateTime("created_at")
-    created_by_id = Field.String("created_by_id", "created_by")
+    created_by_id = Field.String("created_by_id", "createdBy")
 
 
-    def upsert_labels():
+    def upsert_labels(self):
         ...
 
     def add_predictions(self, predictions):
