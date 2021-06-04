39e517fc0ccaa4cef8e1b9a23a34bcd6964b298a
Matt Sokoloff <msokoloff@labelbox.com>
2021-06-04 07:00:41 -0400

    remove extra print, format

--- labelbox/schema/model_run.py
+++ labelbox/schema/model_run.py
@@ -3,11 +3,10 @@ from labelbox.orm.db_object import DbObject
 
 
 class ModelRun(DbObject):
-    name =  Field.String("name")
+    name = Field.String("name")
     updated_at = Field.DateTime("updated_at")
     created_at = Field.DateTime("created_at")
     created_by_id = Field.String("created_by_id", "createdBy")
-
 
     def upsert_labels(self):
         ...
