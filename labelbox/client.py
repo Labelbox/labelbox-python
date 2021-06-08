005c05b1610b7000fbe41f85465204905270e226
Matt Sokoloff <msokoloff@labelbox.com>
2021-06-08 11:41:00 -0400

    computed metrics on real data

--- labelbox/client.py
+++ labelbox/client.py
@@ -151,6 +151,7 @@ class Client:
                 request['headers'] = {
                     'Authorization': self.headers['Authorization']
                 }
+
             response = requests.post(**request)
             logger.debug("Response: %s", response.text)
         except requests.exceptions.Timeout as e:
