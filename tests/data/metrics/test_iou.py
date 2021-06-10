8ac68845bd37e8508936ed5c1957e03446de9c42
Matt Sokoloff <msokoloff@labelbox.com>
2021-06-10 10:17:03 -0400

    yapf

--- tests/data/metrics/test_iou.py
+++ tests/data/metrics/test_iou.py
@@ -9,8 +9,10 @@ from labelbox.data.metrics.iou import datarow_miou
 def check_iou(pair):
     assert datarow_miou(pair.labels, pair.predictions) == pair.expected
 
+
 def strings_to_fixtures(strings):
     return [fixture_ref(x) for x in strings]
+
 
 def test_overlapping(polygon_pair, box_pair, mask_pair):
     check_iou(polygon_pair)
@@ -23,62 +25,51 @@ def test_overlapping(polygon_pair, box_pair, mask_pair):
 
 
 @parametrize("pair",
-                         strings_to_fixtures(
-                             [
-                                 "unmatched_label",
-                                 "unmatched_prediction",
-                         ]))
+             strings_to_fixtures([
+                 "unmatched_label",
+                 "unmatched_prediction",
+             ]))
 def test_unmatched(pair):
     check_iou(pair)
 
 
 @parametrize("pair",
-                         strings_to_fixtures(
-                             [
-                                 "empty_radio_label",
-                                 "matching_radio",
-                                 "empty_radio_prediction",
-                         ]))
+             strings_to_fixtures([
+                 "empty_radio_label",
+                 "matching_radio",
+                 "empty_radio_prediction",
+             ]))
 def test_radio(pair):
     check_iou(pair)
 
+
 @parametrize("pair",
-                         strings_to_fixtures(
-                             [
-                                 "matching_checklist",
-                                 "partially_matching_checklist_1",
-                                 "partially_matching_checklist_2",
-                                 "partially_matching_checklist_3",
-                                 "empty_checklist_label",
-                                 "empty_checklist_prediction",
-                         ]))
+             strings_to_fixtures([
+                 "matching_checklist",
+                 "partially_matching_checklist_1",
+                 "partially_matching_checklist_2",
+                 "partially_matching_checklist_3",
+                 "empty_checklist_label",
+                 "empty_checklist_prediction",
+             ]))
 def test_checklist(pair):
     check_iou(pair)
 
 
-@parametrize("pair",
-                         strings_to_fixtures(
-                             [
-                                 "matching_text",
-                                 "not_matching_text"
-                         ]))
+@parametrize("pair", strings_to_fixtures(["matching_text",
+                                          "not_matching_text"]))
 def test_text(pair):
     check_iou(pair)
 
+
 @parametrize("pair",
-                         strings_to_fixtures(
-                             [
-                                 "test_box_with_wrong_subclass",
-                                 "test_box_with_subclass"
-                         ]))
+             strings_to_fixtures(
+                 ["test_box_with_wrong_subclass", "test_box_with_subclass"]))
 def test_vector_with_subclass(pair):
     check_iou(pair)
 
-@parametrize("pair",
-                         strings_to_fixtures(
-                             [
-                                 "point_pair",
-                                 "line_pair"
-                         ]))
+
+@parametrize("pair", strings_to_fixtures(["point_pair", "line_pair"]))
 def test_others(pair):
-    assert math.isclose(datarow_miou(pair.labels, pair.predictions),pair.expected)
+    assert math.isclose(datarow_miou(pair.labels, pair.predictions),
+                        pair.expected)
