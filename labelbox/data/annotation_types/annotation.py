a88c315cd69e1fa346ea94352872c92fd098b2f8
Matt Sokoloff <msokoloff@labelbox.com>
2021-07-07 10:08:34 -0400

    fixed small bugs, added subclass, started tests

--- labelbox/data/annotation_types/annotation.py
+++ labelbox/data/annotation_types/annotation.py
@@ -1,22 +1,17 @@
 from typing import List, Optional, Union
 from uuid import uuid4
 from pydantic import BaseModel
-from labelbox.data.annotation_types.reference import FeatureSchemaRef
+
 from labelbox.schema.ontology import OntologyBuilder
-from labelbox.data.annotation_types.classification.classification import Classification
-from labelbox.data.annotation_types.data.video import VideoData
+from labelbox.data.annotation_types.reference import FeatureSchemaRef
+from labelbox.data.annotation_types.classification.classification import Classification, Subclass
 from labelbox.data.annotation_types.ner import TextEntity
 from labelbox.data.annotation_types.geometry.geometry import Geometry
-from labelbox.data.annotation_types.classification.classification import (
-    Radio, CheckList, Text, Dropdown)
 
-
-class Annotation(BaseModel):
+class Annotation(FeatureSchemaRef):
     feature_id: Optional[
         str] = None  # Can be used to reference the feature in labelbox
-    feature_schema_ref: FeatureSchemaRef  # This is not super meaningful to non-labelbox users. It is more the feature_type_id
-    classifications: Optional[List[Union[Radio, CheckList, Text,
-                                         Dropdown]]] = None
+    classifications: List[Subclass] = []
     value: Union[Classification, Geometry, TextEntity]
 
     def to_mal_ndjson(self, ontology: OntologyBuilder):
@@ -41,7 +36,6 @@ class Frames(BaseModel):
 
 
 class VideoAnnotation(Annotation):
-    data: VideoData
     frames: List[Frames] = []
 
     def to_mal_ndjson(self, ontology: OntologyBuilder):
--- labelbox/data/annotation_types/label.py
+++ labelbox/data/annotation_types/label.py
@@ -4,13 +4,14 @@ from labelbox.schema.ontology import OntologyBuilder
 from labelbox.data.annotation_types.annotation import Annotation
 from labelbox.data.annotation_types.data.raster import RasterData
 from labelbox.data.annotation_types.data.text import TextData
+from labelbox.data.annotation_types.data.video import VideoData
 from labelbox.data.annotation_types.metrics import Metric
 from pydantic import BaseModel
 
 
 class Label(BaseModel):
     # TODO: This sounds too much like the other label we need to rename this...
-    data: Union[RasterData, TextData]
+    data: Union[RasterData, TextData, VideoData]
     annotations: List[Union[Annotation, Metric]] = []
 
     def to_mal_ndjson(self, ontology: OntologyBuilder):
