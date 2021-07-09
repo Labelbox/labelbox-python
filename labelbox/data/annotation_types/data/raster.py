0f592200c6ab62ae03dedef6aa234163154ecf58
Matt Sokoloff <msokoloff@labelbox.com>
2021-07-09 07:28:55 -0400

    update data types for signing

--- labelbox/data/annotation_types/data/raster.py
+++ labelbox/data/annotation_types/data/raster.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Optional
+from typing import Callable, Dict, Any, Optional
 from io import BytesIO
 
 from PIL import Image
@@ -16,25 +16,21 @@ class RasterData(DataRowRef):
     im_bytes: Optional[bytes] = None
     file_path: Optional[str] = None
     url: Optional[str] = None
-    _cache = True
+    numpy: Optional[np.ndarray] = None
 
     def bytes_to_np(self, image_bytes: bytes) -> np.ndarray:
         return np.array(Image.open(BytesIO(image_bytes)))
 
-    @classmethod
-    def from_np(cls, arr: np.array) -> "RasterData":
-        if arr.dtype != np.uint8:
-            raise TypeError("Numpy array representing segmentation mask must be np.uint8")
-        elif len(arr.shape) not in [2,3]:
-            raise ValueError(f"Numpy array must have 2 or 3 dims. Found shape {arr.shape}")
+    def np_to_bytes(self, arr: np.ndarray) -> bytes:
         im_bytes = BytesIO()
         Image.fromarray(arr).save(im_bytes, format = "PNG")
-        return RasterData(im_bytes = im_bytes.getvalue())
-
+        return im_bytes.getvalue()
 
     @property
-    def numpy(self) -> np.ndarray:
+    def data(self) -> np.ndarray:
         # This is where we raise the exception..
+        if self.numpy is not None:
+            return self.numpy
         if self.im_bytes is not None:
             return self.bytes_to_np(self.im_bytes)
         elif self.file_path is not None:
@@ -42,25 +38,50 @@ class RasterData(DataRowRef):
             # What does imread do?
             with open(self.file_path, "rb") as img:
                 im_bytes = img.read()
-            if self._cache:
-                self.im_bytes = im_bytes
+            self.im_bytes = im_bytes
             return self.bytes_to_np(im_bytes)
         elif self.url is not None:
             response = requests.get(self.url)
             response.raise_for_status()
             im_bytes = response.content
-            if self._cache:
-                self.im_bytes = im_bytes
+            self.im_bytes = im_bytes
             return self.bytes_to_np(im_bytes)
         else:
             raise ValueError("Must set either url, file_path or im_bytes")
 
+
+    def create_url(self, signer: Callable[[bytes], str]) -> None:
+        if self.url is not None:
+            return self.url
+        elif self.im_bytes is not None:
+            self.url = signer(self.im_bytes)
+        elif self.file_path is not None:
+            with open(self.file_path, 'rb') as file:
+                self.url = signer(file.read())
+        elif self.numpy is not None:
+            self.url = signer(self.np_to_bytes(self.arr))
+        else:
+            raise ValueError("One of url, im_bytes, file_path, numpy must not be None.")
+        return self.url
+
     @root_validator
-    def validate_date(cls, values):
+    def validate_args(cls, values):
         file_path = values.get("file_path")
         im_bytes = values.get("im_bytes")
         url = values.get("url")
-        if file_path == im_bytes == url == None:
+        arr = values.get("numpy")
+        if file_path == im_bytes == url == arr == None:
             raise ValidationError(
-                "One of `file_path`, `im_bytes`, or `url` required.")
+                "One of `file_path`, `im_bytes`, `url`, or `numpy` required.")
+        if arr:
+            if arr.dtype != np.uint8:
+                raise ValidationError("Numpy array representing segmentation mask must be np.uint8")
+            elif len(arr.shape) not in [2,3]:
+                raise ValidationError(f"Numpy array must have 2 or 3 dims. Found shape {arr.shape}")
         return values
+
+
+
+
+
+
