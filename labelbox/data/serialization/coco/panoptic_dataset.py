ea38642658aed38f9b60517e254f33b562422094
Matt Sokoloff <msokoloff@labelbox.com>
2021-08-02 18:16:33 -0400

    wip

--- labelbox/data/serialization/coco/panoptic_dataset.py
+++ labelbox/data/serialization/coco/panoptic_dataset.py
@@ -8,13 +8,79 @@ from labelbox.data.serialization.coco.categories import Categories
 from labelbox.data.annotation_types.collection import LabelCollection
 from labelbox.data.serialization.coco.image import CocoImage
 from labelbox.data.serialization.coco.annotation import PanopticAnnotation, SegmentInfo, get_annotation_lookup
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from pydantic import BaseModel
+import hashlib
 from tqdm import tqdm
 import os
 import numpy as np
 from PIL import Image
 from typing import Dict, Any, List
 
+
+
+
+def get_image(label, image_root, image_id):
+    path = os.path.join(image_root, f"{image_id}.jpg")
+
+    if 1: #not os.path.exists(path):
+        im = Image.fromarray(label.data.data)
+        im.save(path)
+        w, h = im.size
+    #else:
+    #    w, h = imagesize.get(path)
+
+    return CocoImage(id=image_id, width=w, height=h, file_name=path)
+
+
+
+def process_label(label, idx, image_root, seg_root):
+    annotations = get_annotation_lookup(label.annotations)
+    image_id = idx  #label.data.uid or idx
+    path = os.path.join(image_root, f"{image_id}.jpg")
+
+    if 1:  #not os.path.exists(path):
+        im = Image.fromarray(label.data.data)
+        im.save(path)
+        w, h = im.size
+    #else:
+    #w,h = imagesize.get(path)
+    image = CocoImage(id=image_id,
+                        width=w,
+                        height=h,
+                        file_name=path.split(os.sep)[-1])
+
+    canvas = np.zeros((h, w, 3))
+    segments = []
+    categories = {}
+    for class_name in annotations:
+        for idx2, annotation in enumerate(annotations[class_name]):
+                categories[annotation.name] = int(
+                    hashlib.sha256(annotation.name.encode('utf-8')).hexdigest(),
+                    16) % 10**8
+            if isinstance(annotation.value, Mask):
+                segment, canvas = (mask_to_segment_info(
+                    canvas, annotation, categories[annotation.name]))
+                segments.append(segment)
+            elif isinstance(annotation.value, (Polygon, Rectangle)):
+                segments.append(
+                    vector_to_segment_info(
+                        canvas,
+                        annotation,
+                        annotation_idx=idx2,
+                        image=image,
+                        category_id=categories[annotation.name]))
+        # TODO: Report on unconverted annotations.
+
+    mask_file = path.replace('.jpg', '.png')
+    mask_file = os.path.join(
+        seg_root,
+        path.split(os.sep)[-1].replace('.jpg', '.png'))
+    Image.fromarray(canvas.astype(np.uint8)).save(mask_file)
+
+    return PanopticAnnotation(image_id=image_id,
+                            file_name=mask_file.split(os.sep)[-1],
+                            segments_info=segments), image, categories
 
 class CocoPanopticDataset(BaseModel):
     info: Dict[str, Any] = {}
@@ -24,75 +90,38 @@ class CocoPanopticDataset(BaseModel):
 
     @classmethod
     def from_common(cls, labels: LabelCollection, image_root, seg_root):
-        coco_annotations = []
-        categories = {}
+        all_coco_annotations = []
+        coco_categories = {}
         images = []
         # TODO: use multiprocessing
         #TODO: Check if directory structure exists and prompt to overwrite..
         # Or set a flag idk.
 
-        for idx, label in tqdm(enumerate(labels)):
-            if idx > 200:
-                break
-            annotations = get_annotation_lookup(label.annotations)
-            image_id = idx  #label.data.uid or idx
-            path = os.path.join(image_root, f"{image_id}.jpg")
+        with ProcessPoolExecutor(max_workers=12) as exc:
+            futures = [
+                exc.submit(process_label, label, idx, image_root)
+                for idx, label in enumerate(labels)
+            ]
+            for future in tqdm(as_completed(futures)):
+                image, annotation, categories = future.result()
+                images.append(image)
+                all_coco_annotations.append(annotation)
+                coco_categories.update(categories)
 
-            if 1:  #not os.path.exists(path):
-                im = Image.fromarray(label.data.data)
-                im.save(path)
-                w, h = im.size
-            #else:
-            #w,h = imagesize.get(path)
-            image = CocoImage(id=image_id,
-                              width=w,
-                              height=h,
-                              file_name=path.split(os.sep)[-1])
-            images.append(image)
-            canvas = np.zeros((h, w, 3))
-            segments = []
 
-            for class_name in annotations:
-                for idx2, annotation in enumerate(annotations[class_name]):
-                    if annotation.name not in categories:
-                        categories[annotation.name] = len(categories)
-                    if isinstance(annotation.value, Mask):
-                        segment, canvas = (mask_to_segment_info(
-                            canvas, annotation, categories[annotation.name]))
-                        segments.append(segment)
-                    elif isinstance(annotation.value, (Polygon, Rectangle)):
-                        segments.append(
-                            vector_to_segment_info(
-                                canvas,
-                                annotation,
-                                annotation_idx=idx2,
-                                image=image,
-                                category_id=categories[annotation.name]))
-                # TODO: Report on unconverted annotations.
-
-            mask_file = path.replace('.jpg', '.png')
-            mask_file = os.path.join(
-                seg_root,
-                path.split(os.sep)[-1].replace('.jpg', '.png'))
-            Image.fromarray(canvas.astype(np.uint8)).save(mask_file)
-            coco_annotations.append(
-                PanopticAnnotation(image_id=image_id,
-                                   file_name=mask_file.split(os.sep)[-1],
-                                   segments_info=segments))
         return CocoPanopticDataset(info={
             'image_root': image_root,
             'seg_root': seg_root
         },
                                    images=images,
-                                   annotations=coco_annotations,
+                                   annotations=all_coco_annotations,
                                    categories=[
                                        Categories(id=idx,
                                                   name=name,
                                                   supercategory='all',
                                                   isthing=0)
-                                       for name, idx in categories.items()
+                                       for name, idx in coco_categories.items()
                                    ])
-
     def to_common(self, image_root, seg_root):
         category_lookup = {
             category.id: category for category in self.categories
