9f15de1296d4c471db4b84219ee8e66b8fd0ff89
Matt Sokoloff <msokoloff@labelbox.com>
2021-08-15 20:18:49 -0400

    wip

--- labelbox/data/serialization/coco/image.py
+++ labelbox/data/serialization/coco/image.py
@@ -1,8 +1,11 @@
+from labelbox.data.annotation_types import Label
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Tuple
 import os
 from PIL import Image
+import hashlib
 import imagesize
+
 
 class CocoImage(BaseModel):
     id: int
@@ -15,13 +18,38 @@ class CocoImage(BaseModel):
     #date_captured: datetime
 
 
-def get_image(label, image_root, image_id):
+def get_image_id(label: Label, idx: int):
+    if label.data.file_path is not None:
+        file_name = label.data.file_path.replace(".jpg", "")
+        if file_name.isdecimal():
+            return label.data.file_path.replace(".jpg", "")
+    return idx
+
+
+def get_image(label: Label, image_root, image_id):
     path = os.path.join(image_root, f"{image_id}.jpg")
     if not os.path.exists(path):
-        im = Image.fromarray(label.data.data)
+        im = Image.fromarray(label.data.value)
         im.save(path)
         w, h = im.size
     else:
         w, h = imagesize.get(path)
+    return CocoImage(id=image_id,
+                     width=w,
+                     height=h,
+                     file_name=path.split(os.sep)[-1])
 
-    return CocoImage(id=image_id, width=w, height=h, file_name=path.split(os.sep)[-1])
+
+def id_to_rgb(id: int) -> Tuple[int, int, int]:
+    blue = id // (256 * 256)
+    rem = id - (blue * 256 * 256)
+    green = rem // 256
+    red = rem - (256 * green)
+    return red, green, blue
+
+
+def rgb_to_id(red: int, green: int, blue: int) -> int:
+    id = blue * 256 * 256
+    id += (green * 256)
+    id += red
+    return id
