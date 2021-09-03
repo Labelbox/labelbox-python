4cb17805abd09e38d7a53bda4183036d0359cf57
Matt Sokoloff <msokoloff@labelbox.com>
2021-09-02 21:35:25 -0400

    resolve converter issues

--- tests/data/serialization/labelbox_v1/test_image.py
+++ tests/data/serialization/labelbox_v1/test_image.py
@@ -5,13 +5,10 @@ import pytest
 from labelbox.data.serialization.labelbox_v1.converter import LBV1Converter
 
 
-
-@pytest.mark.parametrize(
-    "file_path",
-    [
-        'tests/data/assets/labelbox_v1/highly_nested_image.json',
-        'tests/data/assets/labelbox_v1/image_export.json'
-    ])
+@pytest.mark.parametrize("file_path", [
+    'tests/data/assets/labelbox_v1/highly_nested_image.json',
+    'tests/data/assets/labelbox_v1/image_export.json'
+])
 def test_image(file_path):
     with open(file_path, 'r') as file:
         payload = json.load(file)
@@ -30,10 +27,13 @@ def test_image(file_path):
                     # We don't add a classification key to the payload if there is no classifications.
                     annotation_a.pop('classifications')
 
-                if isinstance(annotation_b.get('classifications'), list) and len(annotation_b['classifications']):
+                if isinstance(annotation_b.get('classifications'),
+                              list) and len(annotation_b['classifications']):
                     if isinstance(annotation_b['classifications'][0], list):
-                        annotation_b['classifications'] = annotation_b['classifications'][0]
+                        annotation_b['classifications'] = annotation_b[
+                            'classifications'][0]
 
                 assert annotation_a == annotation_b
+
 
 # After check the nd serializer on this shit.. It should work for almost everything (except the other horse shit..)
