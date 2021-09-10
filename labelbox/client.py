ec81414730405744a61846ce5be98e9eeca23933
Matt Sokoloff <msokoloff@labelbox.com>
2021-09-10 11:35:30 -0400

    remove extra breakpoint

--- labelbox/client.py
+++ labelbox/client.py
@@ -551,7 +551,7 @@ class Client:
                 """mutation validateDatasetPyApi($id: ID!){validateDataset(where: {id : $id}){
                     valid checks{name, success}}}
                 """, {'id': dataset.uid})
-            breakpoint()
+
             if not validation_result['validateDataset']['valid']:
                 raise labelbox.exceptions.LabelboxError(
                     f"IAMIntegration {validation_result['validateDataset']['checks']['name']} was not successfully added added to the project."
