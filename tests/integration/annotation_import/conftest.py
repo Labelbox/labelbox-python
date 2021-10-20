28ca7ac36ab2db6c44b2b14548b58505f8380402
Matt Sokoloff <msokoloff@labelbox.com>
2021-10-20 19:59:57 -0400

    format

--- tests/integration/annotation_import/conftest.py
+++ tests/integration/annotation_import/conftest.py
@@ -327,8 +327,8 @@ def model_run(rand_gen, model):
 
 @pytest.fixture
 def model_run_with_model_run_data_rows(client, configured_project,
-                                annotation_submit_fn, model_run_predictions,
-                                model_run):
+                                       annotation_submit_fn,
+                                       model_run_predictions, model_run):
     configured_project.enable_model_assisted_labeling()
 
     upload_task = MALPredictionImport.create_from_objects(
