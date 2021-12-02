369a6c20ded645fbf0ce928aa16d8753291695c7
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2021-12-01 19:12:56 -0600

    starter to the tiled image annotation type

---
+++ labelbox/data/annotation_types/tiled_image.py
@@ -0,0 +1,63 @@
+from enum import Enum
+from typing import Optional, List
+
+from pydantic import BaseModel, validator
+
+from .geometry import Point
+
+
+class EPSG(Enum):
+    """ Provides the EPSG for tiled image assets that are currently supported.
+    
+    SIMPLEPIXEL is Simple that can be used to obtain the pixel space coordinates
+
+    >>> epsg = EPSG()
+    """
+    SIMPLEPIXEL = 1
+    EPSG4326 = 2
+    EPSG3857 = 3
+    EPSG3395 = 4
+
+
+class TiledBounds(BaseModel):
+    """ Bounds for a tiled image asset related to the relevant epsg. 
+
+    Each bound is a list of Point objects.
+
+    If version of asset is 2, these should be [[lat,lng],[lat,lng]]
+    If version of asset is 1, these should be [[lng,lat]],[lng,lat]]
+
+    >>> bounds = TiledBounds(
+        epsg=EPSG.4326, 
+        bounds=[[0,0],[100,100]]
+        )
+    """
+    epsg: EPSG
+    bounds: List[List[Point]]
+
+
+class TileLayer(BaseModel):
+    """ Url that contains the tile layer. Must be in the format:
+
+    https://c.tile.openstreetmap.org/{z}/{x}/{y}.png
+
+    >>> layer = TileLayer(
+        url=https://c.tile.openstreetmap.org/{z}/{x}/{y}.png,
+        name="slippy map tile"
+        )
+    """
+    url: str
+    name: Optional[str] = "default"
+
+    def from_dict():
+        pass
+
+    def to_dict():
+        pass
+
+    @validator('url')
+    def validate_url(cls, url):
+        xyz_format = "/{z}/{x}/{y}"
+        if xyz_format not in url:
+            raise AssertionError(f"{self.url} needs to contain {xyz_format}")
+        return url
