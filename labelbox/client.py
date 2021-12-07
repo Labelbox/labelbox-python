cc0879d420d8fe2c0039f7a7707e87b25db870d2
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2021-12-07 12:22:56 -0600

    test

--- labelbox/client.py
+++ labelbox/client.py
@@ -12,7 +12,6 @@ from google.api_core import retry
 import requests
 import requests.exceptions
 
-
 import labelbox.exceptions
 from labelbox import utils
 from labelbox import __version__ as SDK_VERSION
