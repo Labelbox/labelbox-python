e0e29dd557c5f96bb39a11d4e2a226d3c9540a31
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2022-01-28 16:33:26 -0500

    resolve conflict

--- labelbox/data/annotation_types/geometry/mask.py
+++ labelbox/data/annotation_types/geometry/mask.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional, Tuple, Union, List
+from typing import Callable, Optional, Tuple, Union, Dict
 
 import numpy as np
 from pydantic.class_validators import validator
