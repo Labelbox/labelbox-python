44c95a76cf83a3c6c42026f6f7537f2b898417c7
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2022-02-28 15:55:19 -0500

    nit reverse of small file change

--- examples/integrations/gcp/src/coordinator/coordinator.py
+++ examples/integrations/gcp/src/coordinator/coordinator.py
@@ -16,14 +16,12 @@ logger = logging.getLogger("uvicorn")
 logger.setLevel(logging.DEBUG)
 app = FastAPI()
 
-# async def run(json_data: Dict[str, Any], pipeline: PipelineName):
-# try:
-#     await run_in_threadpool(pipelines[pipeline].run, json_data)
-# except Exception as e:
-#     # TODO: Notify labelbox
-#     logger.info(f"Job failed. Error: {e}")
 async def run(json_data: Dict[str, Any], pipeline: PipelineName):
+try:
     await run_in_threadpool(pipelines[pipeline].run, json_data)
+except Exception as e:
+    # TODO: Notify labelbox
+    logger.info(f"Job failed. Error: {e}")
 
 
 @app.get("/models")
