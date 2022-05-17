12aa306b9fe04169ae8980efd3a75638bb11a3f8
Kevin Kim <kkim@kevins-mbp-2.lan>
2022-05-17 16:00:55 -0700

    Randomize batch name for retestability

--- tests/integration/test_batch.py
+++ tests/integration/test_batch.py
@@ -1,4 +1,5 @@
 import pytest
+import uuid
 
 from labelbox import Dataset, Project
 
@@ -35,8 +36,9 @@ def test_create_batch(configured_project: Project, big_dataset: Dataset):
     configured_project.update(queue_mode=Project.QueueMode.Batch)
 
     data_rows = [dr.uid for dr in list(big_dataset.export_data_rows())]
-    batch = configured_project.create_batch("test-batch", data_rows, 3)
-    assert batch.name == 'test-batch'
+    batch_name = f"test-batch-{uuid.uuid4()}"
+    batch = configured_project.create_batch(batch_name, data_rows, 3)
+    assert batch.name == batch_name
     assert batch.size == len(data_rows)
 
 
