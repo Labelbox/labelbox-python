5c359c83ded2a5a9380aeb506533a4be035421a9
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2022-05-17 12:35:04 -0400

    update to files to be able to handle pulling in metadata from datarows

--- labelbox/schema/dataset.py
+++ labelbox/schema/dataset.py
@@ -410,9 +410,8 @@ class Dataset(DbObject, Updateable, Deletable):
                 response.raise_for_status()
                 reader = ndjson.reader(StringIO(response.text))
                 # TODO: Update result to parse customMetadata when resolver returns
-                return (Entity.DataRow(self.client, {
-                    **result, 'customMetadata': []
-                }) for result in reader)
+                return (Entity.DataRow(self.client, {**result})
+                        for result in reader)
             elif res["status"] == "FAILED":
                 raise LabelboxError("Data row export failed.")
 
