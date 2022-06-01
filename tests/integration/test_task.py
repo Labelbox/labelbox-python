5954537f7e5e7bb80924c911a2f319d676c1d412
Kevin Kim <kkim@kevins-mbp-2.lan>
2022-06-01 13:10:26 -0700

    Add tests

---
+++ tests/integration/test_task.py
@@ -0,0 +1,39 @@
+from labelbox import DataRow
+from labelbox.schema.data_row_metadata import DataRowMetadataField
+
+EMBEDDING_SCHEMA_ID = "ckpyije740000yxdk81pbgjdc"
+
+
+def test_task_errors(dataset, image_url):
+    client = dataset.client
+    embeddings = [0.0] * 128
+    task = dataset.create_data_rows([
+        {
+            DataRow.row_data:
+                image_url,
+            DataRow.metadata_fields: [
+                DataRowMetadataField(schema_id=EMBEDDING_SCHEMA_ID,
+                                     value=embeddings),
+                DataRowMetadataField(schema_id=EMBEDDING_SCHEMA_ID,
+                                     value=embeddings)
+            ]
+        },
+    ])
+    assert task in client.get_user().created_tasks()
+    task.wait_till_done()
+    assert task.status == "FAILED"
+    assert task.errors() is not None
+    assert 'message' in task.errors()
+
+
+def test_task_success(dataset, image_url):
+    client = dataset.client
+    task = dataset.create_data_rows([
+        {
+            DataRow.row_data: image_url,
+        },
+    ])
+    assert task in client.get_user().created_tasks()
+    task.wait_till_done()
+    assert task.status == "COMPLETE"
+    assert task.errors() is None
