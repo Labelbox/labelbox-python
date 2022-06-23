24dac656fde8a077b8abbbe185393eedf0f7faf4
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2022-06-23 10:29:22 -0400

    update to conditional arg of version for np

--- labelbox/data/annotation_types/types.py
+++ labelbox/data/annotation_types/types.py
@@ -35,13 +35,12 @@ class _TypedArray(np.ndarray, Generic[DType, DShape]):
             )
         return val
 
-
-if version.parse('1.22.0') <= version.parse(
-        np.__version__) < version.parse('1.23.0'):
-    from numpy.typing import _GenericAlias
-    TypedArray = _GenericAlias(_TypedArray, (Any, DType))
 if version.parse(np.__version__) >= version.parse('1.23.0'):
     from numpy._typing import _GenericAlias
+    TypedArray = _GenericAlias(_TypedArray, (Any, DType))    
+elif version.parse('1.22.0') <= version.parse(
+        np.__version__) < version.parse('1.23.0'):
+    from numpy.typing import _GenericAlias
     TypedArray = _GenericAlias(_TypedArray, (Any, DType))
 else:
     TypedArray = _TypedArray[Any, DType]
