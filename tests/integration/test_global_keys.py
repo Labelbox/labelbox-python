68f6c5f1b343ee0ec97f5db91f874735eb30fa20
jtsodapop <67922677+jtsodapop@users.noreply.github.com>
2022-09-09 17:04:27 -0400

    update assign test cases

--- tests/integration/test_global_keys.py
+++ tests/integration/test_global_keys.py
@@ -1,3 +1,4 @@
+from tkinter import E
 import uuid
 import pytest
 
@@ -21,13 +22,14 @@ def test_assign_global_keys_to_data_rows(client, dataset, image_url):
         "data_row_id": dr_2.uid,
         "global_key": gk_2
     }]
-    client.assign_global_keys_to_data_rows(assignment_inputs)
-
-    res = client.get_data_row_ids_for_global_keys([gk_1, gk_2])
+    res = client.assign_global_keys_to_data_rows(assignment_inputs)
+    assert res['status'] == "SUCCESS"
+    assert res['errors'] == []
 
     assert len(res['results']) == 2
-    successful_assignments = set(res['results'])
-    assert successful_assignments == row_ids
+    for r in res['results']:
+        del r['sanitized']
+    assert res['results'] == assignment_inputs
 
 
 def test_assign_global_keys_to_data_rows_validation_error(client):
@@ -48,4 +50,30 @@ def test_assign_global_keys_to_data_rows_validation_error(client):
     with pytest.raises(ValueError) as excinfo:
         client.assign_global_keys_to_data_rows(assignment_inputs)
     e = """[{'data_row_id': 'test uid', 'wrong_key': 'gk 1'}, {'wrong_key': 'test uid 3', 'global_key': 'gk 3'}, {'data_row_id': 'test uid 4'}, {'global_key': 'gk 5'}, {}]"""
-    assert e in str(excinfo.value)
+    assert e
+
+
+def test_assign_same_global_keys_to_data_rows(client, dataset, image_url):
+    dr_1 = dataset.create_data_row(row_data=image_url, external_id="hello")
+    dr_2 = dataset.create_data_row(row_data=image_url, external_id="world")
+
+    gk_1 = str(uuid.uuid4())
+
+    assignment_inputs = [{
+        "data_row_id": dr_1.uid,
+        "global_key": gk_1
+    }, {
+        "data_row_id": dr_2.uid,
+        "global_key": gk_1
+    }]
+    res = client.assign_global_keys_to_data_rows(assignment_inputs)
+
+    assert res['status'] == "PARTIAL SUCCESS"
+    assert len(res['results']) == 1
+    assert res['results'][0]['data_row_id'] == dr_1.uid
+    assert res['results'][0]['global_key'] == gk_1
+
+    assert len(res['errors']) == 1
+    assert res['errors'][0]['data_row_id'] == dr_2.uid
+    assert res['errors'][0]['global_key'] == gk_1
+    assert res['errors'][0]['error'] == "Invalid global key"
