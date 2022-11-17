aa99894b6d621f5d4d751a41ef6a1ab60adbd430
Sergey Dubinin <sdubinin@labelbox.com>
2022-11-17 22:46:11 +0500

    AL-3578: Fixed attr access

--- labelbox/schema/confidence_presence_checker.py
+++ labelbox/schema/confidence_presence_checker.py
@@ -34,12 +34,15 @@ class LabelsConfidencePresenceChecker:
                                                  ScalarMetric,
                                                  ConfusionMatrixMetric]):
 
-        confidence: Union[float, None] = getattr(annotation, 'confidence')
+        confidence: Union[float,
+                          None] = getattr(annotation, 'confidence') if hasattr(
+                              annotation, 'confidence') else None
         if confidence is not None:
             return True
 
-        classifications: Union[List[ClassificationAnnotation],
-                               None] = getattr(annotation, 'classifications')
+        classifications: Union[List[ClassificationAnnotation], None] = getattr(
+            annotation, 'classifications') if hasattr(
+                annotation, 'classifications') else None
         if classifications:
             return any([cls._check_classification(x) for x in classifications])
         return False
