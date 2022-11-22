f675f33dd3dace3789677acbc28f010b26aa6476
Ramy <ramy1951@live.com>
2022-11-22 10:30:46 -0800

    reformat code

--- labelbox/schema/organization.py
+++ labelbox/schema/organization.py
@@ -97,7 +97,8 @@ class Organization(DbObject):
             raise LabelboxError(f"Unable to send invite for email {email}")
         return Entity.Invite(self.client, invite_response)
 
-    def bulk_invite_users(self, invites=List["Invite"]) -> Dict[str, List["Invite"]]:
+    def bulk_invite_users(self,
+                          invites=List["Invite"]) -> Dict[str, List["Invite"]]:
         """
         Invite up to 1000 new members to the org. This will send each new user an email invite
 
