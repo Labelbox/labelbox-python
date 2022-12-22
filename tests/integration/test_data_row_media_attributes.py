0f31c6219aee2ce1b42da58e124731a0b917a0b7
Kevin Kim <kkim@kevins-mbp-2.lan>
2022-12-22 12:09:56 -0800

    Fix error msg and use list()

--- tests/integration/test_data_row_media_attributes.py
+++ tests/integration/test_data_row_media_attributes.py
@@ -5,8 +5,8 @@ def test_export_empty_media_attributes(configured_project_with_label):
     project, _, _, _ = configured_project_with_label
     # Wait for exporter to retrieve latest labels
     sleep(10)
-    labels = project.label_generator().as_list()
+    labels = list(project.label_generator())
     assert len(
         labels
-    ) == 1, "Labels could not be fetched via `project.label_generator()`"
+    ) == 1, "Label export job unexpectedly returned an empty result set`"
     assert labels[0].data.media_attributes == {}
