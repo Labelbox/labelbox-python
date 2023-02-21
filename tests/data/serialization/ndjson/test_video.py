c802abb67cea0b06f498a7328899eb59dace01e6
Val Brodsky <vbrodsky@labelbox.com>
2023-02-20 16:30:24 -0800

    Remove more as_list()

--- tests/data/serialization/ndjson/test_video.py
+++ tests/data/serialization/ndjson/test_video.py
@@ -7,7 +7,7 @@ def test_video():
     with open('tests/data/assets/ndjson/video_import.json', 'r') as file:
         data = json.load(file)
 
-    res = NDJsonConverter.deserialize(data).as_list()
+    res = list(NDJsonConverter.deserialize(data))
     res = list(NDJsonConverter.serialize(res))
     assert res == [data[2], data[0], data[1], data[3], data[4], data[5]]
 
@@ -17,6 +17,6 @@ def test_video_name_only():
               'r') as file:
         data = json.load(file)
 
-    res = NDJsonConverter.deserialize(data).as_list()
+    res = list(NDJsonConverter.deserialize(data))
     res = list(NDJsonConverter.serialize(res))
     assert res == [data[2], data[0], data[1], data[3], data[4], data[5]]
