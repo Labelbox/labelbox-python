c1f8af0b686644e6cdd5c41a2cf507e0449c8c67
mnoszczak <mnoszczak@labelbox.com>
2023-03-09 16:47:20 +0100

    Add docs to filters

--- labelbox/schema/export_filters.py
+++ labelbox/schema/export_filters.py
@@ -11,4 +11,18 @@ from typing import Tuple
 
 class ProjectExportFilters(TypedDict):
     label_created_at: Optional[Tuple[str, str]]
+    """ Date range for labels created at
+    Formatted "YYYY-MM-DD" or "YYYY-MM-DD hh:mm:ss"
+    Examples: 
+    >>>   ["2000-01-01 00:00:00", "2050-01-01 00:00:00"]
+    >>>   [None, "2050-01-01 00:00:00"]
+    >>>   ["2000-01-01 00:00:00", None]
+    """
     last_activity_at: Optional[Tuple[str, str]]
+    """ Date range for last activity at
+    Formatted "YYYY-MM-DD" or "YYYY-MM-DD hh:mm:ss"
+    Examples: 
+    >>>   ["2000-01-01 00:00:00", "2050-01-01 00:00:00"]
+    >>>   [None, "2050-01-01 00:00:00"]
+    >>>   ["2000-01-01 00:00:00", None]
+    """
