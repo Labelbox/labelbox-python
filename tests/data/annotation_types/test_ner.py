4e35ffc9df1d6855c68ec042bf419d861f650a49
Val Brodsky <vbrodsky@labelbox.com>
2023-03-14 14:22:38 -0700

    Add tests

--- tests/data/annotation_types/test_ner.py
+++ tests/data/annotation_types/test_ner.py
@@ -26,14 +26,8 @@ def test_document_entity():
 
 
 def test_conversation_entity():
-    document_entity = ConversationEntity(name="tool_name",
-                                         message_id=1,
-                                         start=0,
-                                         end=1,
-                                         confidence=0.5)
+    conversation_entity = ConversationEntity(message_id=1, start=0, end=1)
 
-    assert document_entity.name == "tool_name"
-    assert document_entity.message_id == "1"
-    assert document_entity.start == 0
-    assert document_entity.end == 1
-    assert document_entity.confidence == 0.5
+    assert conversation_entity.message_id == "1"
+    assert conversation_entity.start == 0
+    assert conversation_entity.end == 1
