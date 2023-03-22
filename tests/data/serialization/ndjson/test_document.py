5638aa5c483b575a15da7f96087ded5971123a24
Ibrahim Muhammad <imuhammad@labelbox.com>
2023-03-22 15:40:45 -0700

    [AL-5253] Bbox annotation types for PDF

--- tests/data/serialization/ndjson/test_document.py
+++ tests/data/serialization/ndjson/test_document.py
@@ -1,6 +1,33 @@
 import json
-
+import labelbox.types as lb_types
 from labelbox.data.serialization.ndjson.converter import NDJsonConverter
+
+bbox_annotation = lb_types.ObjectAnnotation(
+    name="bounding_box",  # must match your ontology feature's name
+    value=lb_types.DocumentRectangle(
+        start=lb_types.Point(x=42.799, y=86.498),  # Top left
+        end=lb_types.Point(x=141.911, y=303.195),  # Bottom right
+        page=1,
+        unit=lb_types.RectangleUnit.POINTS))
+bbox_labels = [
+    lb_types.Label(data=lb_types.DocumentData(global_key='test-global-key'),
+                   annotations=[bbox_annotation])
+]
+bbox_ndjson = [{
+    'bbox': {
+        'height': 216.697,
+        'left': 42.799,
+        'top': 86.498,
+        'width': 99.112,
+    },
+    'classifications': [],
+    'dataRow': {
+        'globalKey': 'test-global-key'
+    },
+    'name': 'bounding_box',
+    'page': 1,
+    'unit': 'POINTS'
+}]
 
 
 def round_dict(data):
@@ -38,3 +65,15 @@ def test_pdf_with_name_only():
     res = list(NDJsonConverter.serialize(res))
     assert [round_dict(x) for x in res] == [round_dict(x) for x in data]
     f.close()
+
+
+def test_pdf_bbox_serialize():
+    serialized = list(NDJsonConverter.serialize(bbox_labels))
+    serialized[0].pop('uuid')
+    assert serialized == bbox_ndjson
+
+
+def test_pdf_bbox_deserialize():
+    deserialized = list(NDJsonConverter.deserialize(bbox_ndjson))
+    deserialized[0].annotations[0].extra = {}
+    assert deserialized[0].annotations == bbox_labels[0].annotations
