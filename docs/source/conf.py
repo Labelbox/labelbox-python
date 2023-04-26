d4d14e61457c722aafcd5a0a559c34695b537aa2
Kevin Kim <kkim@kevins-mbp-2.lan>
2023-04-26 13:52:50 -0700

    Prep 3.44.0 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ project = 'Python SDK reference'
 copyright = '2021, Labelbox'
 author = 'Labelbox'
 
-release = '3.43.0'
+release = '3.44.0'
 
 # -- General configuration ---------------------------------------------------
 
