142e3820d0593588986921d110a45bd9ab261613
Val Brodsky <vbrodsky@labelbox.com>
2023-05-11 14:25:41 -0700

    Add custom Decoder to be format json and delegate the rest to standard Python json decoder
    To be able to handle loading byte, bytearray etc. all other types as support by the json library itself

--- labelbox/data/serialization/ndjson/parser.py
+++ labelbox/data/serialization/ndjson/parser.py
@@ -1,20 +1,32 @@
+from collections.abc import Callable
 from io import FileIO, StringIO
 import json
-from typing import Iterable, Union
+from typing import Any, Iterable, Union
 
 
-def loads(ndjson_string: str, **kwargs) -> list:
-    # NOTE: the consequence of this line would be conversion of 'literal' line breaks to commas
-    lines = ','.join(ndjson_string.splitlines())
-    text = f"[{lines}]"  # NOTE: this is a hack to make json.loads work for ndjson
-    return json.loads(text, **kwargs)
+class NdjsonDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.parse_array = self._parse_array
+
+    # def _parse_array(self, *args, **kwargs):
+    #     return list(self.scan_once(*args, **kwargs))
+    
+    def decode(self, s: str, *args, **kwargs) -> Any:
+        lines = ','.join(s.splitlines())
+        text = f"[{lines}]"  # NOTE: this is a hack to make json.loads work for ndjson
+        return super().decode(text, *args, **kwargs)
 
 
-def dumps(obj: list, **kwargs) -> str:
+def loads(ndjson_string, **kwargs) -> list:
+    kwargs.setdefault('cls', NdjsonDecoder)
+    return json.loads(ndjson_string, **kwargs)
+
+def dumps(obj, **kwargs) -> str:
     lines = map(lambda obj: json.dumps(obj, **kwargs), obj)
     return '\n'.join(lines)
 
 
-def reader(io_handle: Union[StringIO, FileIO, Iterable], **kwargs):
+def reader(io_handle, **kwargs):
     for line in io_handle:
         yield json.loads(line, **kwargs)
