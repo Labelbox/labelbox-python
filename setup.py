973016b2f497e1d65573d685bb39ff5c2c16e4ff
Val Brodsky <vbrodsky@labelbox.com>
2023-05-29 18:18:57 -0700

    Remove backports

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "backoff==1.10.0", "requests>=2.22.0", "google-api-core>=1.22.1",
-        "pydantic>=1.8,<2.0", "tqdm", "backports-datetime-fromisoformat~=2.0"
+        "pydantic>=1.8,<2.0", "tqdm", "python-dateutil>=2.8.2,<2.9.0"
     ],
     extras_require={
         'data': [
@@ -31,7 +31,6 @@ setuptools.setup(
         ],
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
