541d7c4d9565a8fb15d83fe840af834daae1a8b7
Val Brodsky <vbrodsky@labelbox.com>
2023-06-05 09:45:35 -0700

    PR changes
    Make comments read better

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -8,10 +8,10 @@ from labelbox.utils import format_iso_datetime, format_iso_from_string
         ('2011-11-04T00:05:23Z', '2011-11-04T00:05:23Z'),
         ('2011-11-04T00:05:23+00:00', '2011-11-04T00:05:23Z'),
         ('2011-11-04T00:05:23+05:00', '2011-11-04T00:05:23Z'
-        ),  #NOTE not converting with timezone... this is compatible with out current implementation
+        ),  # NOTE: The current implementation is not converting from timezone other then UTC. This how it has been working prior to my change.
         ('2011-11-04T00:05:23', '2011-11-04T00:05:23Z')
     ])
 def test_datetime_parsing(datetime_str, expected_datetime_str):
-    # NOTE I would normally not tested expected using another function from sdk code, but in this case this is exactly the usage in _validate_parse_datetime
+    # NOTE I would normally not take 'expected' using another function from sdk code, but in this case this is exactly the usage in _validate_parse_datetime
     assert format_iso_datetime(
         format_iso_from_string(datetime_str)) == expected_datetime_str
