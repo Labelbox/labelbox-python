cb697333969eadfc80d89f64f282453d954998a8
Nitin Pasari <npasari@labelbox.com>
2023-07-25 20:07:03 -0700

    use wait_for_data_row_processing to get updates on data row

--- tests/integration/test_data_rows.py
+++ tests/integration/test_data_rows.py
@@ -526,7 +526,7 @@ def test_create_data_rows_with_metadata_wrong_type(dataset, image_url):
         ])
 
 
-def test_data_row_update(dataset, rand_gen, image_url):
+def test_data_row_update(client, dataset, rand_gen, image_url, wait_for_data_row_processing):
     external_id = rand_gen(str)
     data_row = dataset.create_data_row(row_data=image_url,
                                        external_id=external_id)
@@ -541,6 +541,7 @@ def test_data_row_update(dataset, rand_gen, image_url):
     assert data_row.row_data == in_line_content
 
     data_row.update(row_data=image_url)
+    data_row = wait_for_data_row_processing(client, data_row)
     assert data_row.row_data == image_url
 
     # tileLayer becomes a media attribute
