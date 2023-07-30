a75ec1cc58398a6988615ff96c7391c1cd6c8ba6
mnoszczak <99751601+mnoszczak@users.noreply.github.com>
2023-07-30 22:29:45 +0200

    Add clear global keys test

--- tests/integration/test_global_keys.py
+++ tests/integration/test_global_keys.py
@@ -9,7 +9,6 @@ def test_assign_global_keys_to_data_rows(client, dataset, image_url):
 
     dr_1 = dataset.create_data_row(row_data=image_url, external_id="hello")
     dr_2 = dataset.create_data_row(row_data=image_url, external_id="world")
-    row_ids = set([dr_1.uid, dr_2.uid])
 
     gk_1 = str(uuid.uuid4())
     gk_2 = str(uuid.uuid4())
@@ -29,6 +28,11 @@ def test_assign_global_keys_to_data_rows(client, dataset, image_url):
     for r in res['results']:
         del r['sanitized']
     assert res['results'] == assignment_inputs
+
+    # Test clear global keys functionality as part of this test to reduce test time
+    res = client.clear_global_keys([gk_1, gk_2])
+    assert res['status'] == "SUCCESS"
+    assert res['results'] == [gk_1, gk_2]
 
 
 def test_assign_global_keys_to_data_rows_validation_error(client):
