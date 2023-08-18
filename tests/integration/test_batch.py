b439780d1a8a164e3b1c143c3cd86e910bfd95dd
Attila Papai <97034214+attila-papai@users.noreply.github.com>
2023-08-18 17:37:57 +0200

    Remove sleep between creating and exporting a batch (#1213)

--- tests/integration/test_batch.py
+++ tests/integration/test_batch.py
@@ -211,8 +211,6 @@ def test_export_data_rows(project: Project, dataset: Dataset):
 
     data_rows = [dr.uid for dr in list(dataset.export_data_rows())]
     batch = project.create_batch("batch test", data_rows)
-    # allow time for catapult to sync changes to ES
-    time.sleep(5)
     result = list(batch.export_data_rows())
     exported_data_rows = [dr.uid for dr in result]
 
