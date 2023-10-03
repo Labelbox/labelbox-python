810c79015a897619ed627b335002c6dde0a11749
Richard Sun <rsun@labelbox.com>
2023-10-03 15:53:09 -0700

    [QQC-2590] Create method in SDK to modify LPO priorities in bulk

--- tests/integration/test_labeling_parameter_overrides.py
+++ tests/integration/test_labeling_parameter_overrides.py
@@ -45,10 +45,42 @@ def test_labeling_parameter_overrides(consensus_project, initial_dataset,
         data = [(data_rows[2], "a_string", 3)]
         project.set_labeling_parameter_overrides(data)
     assert str(exc_info.value) == \
-        f"Priority must be an int. Found <class 'str'> for data_row {data_rows[2]}. Index: 0"
+           f"Priority must be an int. Found <class 'str'> for data_row {data_rows[2]}. Index: 0"
 
     with pytest.raises(TypeError) as exc_info:
         data = [(data_rows[2].uid, 1)]
         project.set_labeling_parameter_overrides(data)
     assert str(exc_info.value) == \
-        "data_row should be be of type DataRow. Found <class 'str'>. Index: 0"
+           "data_row should be be of type DataRow. Found <class 'str'>. Index: 0"
+
+
+def test_set_labeling_priority(consensus_project, initial_dataset, rand_gen,
+                               image_url):
+    project = consensus_project
+    dataset = initial_dataset
+
+    task = dataset.create_data_rows([{DataRow.row_data: image_url}] * 3)
+    task.wait_till_done()
+    assert task.status == "COMPLETE"
+
+    data_rows = list(dataset.data_rows())
+    assert len(data_rows) == 3
+
+    project.create_batch(
+        rand_gen(str),
+        data_rows,  # sample of data row objects
+        5  # priority between 1(Highest) - 5(lowest)
+    )
+
+    init_labeling_parameter_overrides = list(
+        project.labeling_parameter_overrides())
+    assert len(init_labeling_parameter_overrides) == 3
+    assert {o.priority for o in init_labeling_parameter_overrides} == {5, 5, 5}
+
+    data = [data_row.uid for data_row in data_rows]
+    success = project.set_data_row_labeling_priority(data, 1)
+    assert success
+
+    updated_overrides = list(project.labeling_parameter_overrides())
+    assert len(updated_overrides) == 3
+    assert {o.priority for o in updated_overrides} == {1, 1, 1}
