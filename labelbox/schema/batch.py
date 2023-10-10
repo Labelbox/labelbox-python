ca037204c09f37ca43afc17b9aa3c508ea4de4d5
Yamini Kancharana <ykancharana@labelbox.com>
2023-10-10 15:18:32 -0700

    update the copy for exports v1 deprecation warning

--- labelbox/schema/batch.py
+++ labelbox/schema/batch.py
@@ -105,8 +105,9 @@ class Batch(DbObject):
             LabelboxError: if the export fails or is unable to download within the specified time.
         """
         warnings.warn(
-            "You are currently utilizing exports v1 for this action, which will be deprecated after December 31st, 2023. We recommend transitioning to exports v2. Find out more at this link: https://docs.labelbox.com/reference/label-export."
-        )
+            "You are currently utilizing exports v1 for this action, which will be deprecated after December 31st, 2023. We recommend transitioning to exports v2. To view export v2 details, visit our docs: https://docs.labelbox.com/reference/label-export",
+            DeprecationWarning)
+
         id_param = "batchId"
         metadata_param = "includeMetadataInput"
         query_str = """mutation GetBatchDataRowsExportUrlPyApi($%s: ID!, $%s: Boolean!)
--- labelbox/schema/dataset.py
+++ labelbox/schema/dataset.py
@@ -567,8 +567,8 @@ class Dataset(DbObject, Updateable, Deletable):
             LabelboxError: if the export fails or is unable to download within the specified time.
         """
         warnings.warn(
-            "You are currently utilizing exports v1 for this action, which will be deprecated after December 31st, 2023. We recommend transitioning to exports v2. Find out more at this link: https://docs.labelbox.com/reference/label-export."
-        )
+            "You are currently utilizing exports v1 for this action, which will be deprecated after December 31st, 2023. We recommend transitioning to exports v2. To view export v2 details, visit our docs: https://docs.labelbox.com/reference/label-export",
+            DeprecationWarning)
         id_param = "datasetId"
         metadata_param = "includeMetadataInput"
         query_str = """mutation GetDatasetDataRowsExportUrlPyApi($%s: ID!, $%s: Boolean!)
