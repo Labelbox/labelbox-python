6aee81f79f25dffde07582620d46a7d2c71e6567
Val Brodsky <vbrodsky@labelbox.com>
2023-11-16 20:03:13 -0800

    Update to support identifiable as type in graphql and other identifiable improvements

--- tests/unit/test_unit_identifiable.py
+++ tests/unit/test_unit_identifiable.py
@@ -1,30 +1,46 @@
-from labelbox.schema.identifiable import GlobalKeys, UniqueIds, strings_to_identifiable
+from labelbox.schema.identifiable import GlobalKeys, UniqueIds
 
 
 def test_unique_ids():
     ids = ["a", "b", "c"]
     identifiable = UniqueIds(ids)
     assert identifiable.keys == ids
-    assert identifiable.id_type == "DATA_ROW_ID"
+    assert identifiable._id_type == "UID"
 
 
 def test_global_keys():
     ids = ["a", "b", "c"]
     identifiable = GlobalKeys(ids)
     assert identifiable.keys == ids
-    assert identifiable.id_type == "GLOBAL_KEY"
+    assert identifiable._id_type == "GLOBAL_KEY"
 
 
-def test_strings_to_identifiable():
+def test_strings_to_identifiable_unique_id():
     ids = ["a", "b", "c"]
-    identifiable = strings_to_identifiable(ids)
+    identifiable = UniqueIds.strings_to_identifiable(ids)
+    assert type(identifiable) == UniqueIds
     assert identifiable.keys == ids
-    assert identifiable.id_type == "DATA_ROW_ID"
+    assert identifiable._id_type == "UID"
 
     ids = "a"
-    identifiable = strings_to_identifiable(ids)
+    identifiable = UniqueIds.strings_to_identifiable(ids)
+    assert type(identifiable) == UniqueIds
     assert identifiable.keys == [ids]
-    assert identifiable.id_type == "DATA_ROW_ID"
+    assert identifiable._id_type == "UID"
+
+
+def test_strings_to_identifiable_global_key():
+    ids = ["a", "b", "c"]
+    identifiable = GlobalKeys.strings_to_identifiable(ids)
+    assert type(identifiable) == GlobalKeys
+    assert identifiable.keys == ids
+    assert identifiable._id_type == "GLOBAL_KEY"
+
+    ids = "a"
+    identifiable = GlobalKeys.strings_to_identifiable(ids)
+    assert type(identifiable) == GlobalKeys
+    assert identifiable.keys == [ids]
+    assert identifiable._id_type == "GLOBAL_KEY"
 
 
 def test__str__():
