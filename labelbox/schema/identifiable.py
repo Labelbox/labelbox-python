b51d3e2ddd4435461893bb97061eeca192167065
Val Brodsky <vbrodsky@labelbox.com>
2023-11-21 10:12:10 -0800

    Add Identifiables as iterator

--- labelbox/schema/identifiable.py
+++ labelbox/schema/identifiable.py
@@ -1,65 +1,29 @@
 from abc import ABC, abstractmethod
-from enum import Enum
 from typing import List, Union
-
-
-class IdType(str, Enum):
-    DataRowId = "UID"
-    GlobalKey = "GLOBAL_KEY"
 
 
 class Identifiable(ABC):
 
-    def __init__(self, keys: Union[str, List[str]], id_type: IdType):
-        self._keys = keys
-        if isinstance(keys, str):
-            self._keys = [keys]
-        self._id_type = id_type
+    def __init__(self, key: str):
+        self._key = key
 
     @property
-    def keys(self):
-        return self._keys
-
-    @keys.setter
-    def keys(self, keys):
-        self._keys = keys
-        if isinstance(keys, str):
-            self._keys = [keys]
-
-    @classmethod
-    @abstractmethod
-    def strings_to_identifiable(cls, keys: Union[str, List[str]]):
-        pass
+    def key(self):
+        return self.key
 
     def __eq__(self, other):
-        return other.keys == self.keys
+        return other.key == self.key
 
     def __hash__(self):
-        hash(self.keys)
+        hash(self.key)
 
     def __str__(self):
-        return self.keys.__str__()
+        return self.key.__str__()
 
 
-class UniqueIds(Identifiable):
-
-    @classmethod
-    def strings_to_identifiable(cls, keys: Union[str, List[str]]):
-        return cls(keys)
-
-    def __init__(self, keys: Union[str, List[str]]):
-        super().__init__(keys, IdType.DataRowId)
+class UniqueId(Identifiable):
+    pass
 
 
-class GlobalKeys(Identifiable):
-
-    @classmethod
-    def strings_to_identifiable(cls, keys: Union[str, List[str]]):
-        return cls(keys)
-
-    def __init__(self, keys: Union[str, List[str]]):
-        super().__init__(keys, IdType.GlobalKey)
-
-
-DefaultIdentifiable = UniqueIds
-DataRowIdentifiers = Union[UniqueIds, GlobalKeys]
+class GlobalKey(Identifiable):
+    pass
