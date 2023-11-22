6f7d7985634993b60784633d96feb8b4f0f71674
Yesu Aggarwal <yesuagg@gmail.com>
2023-11-22 11:10:25 -0800

    [QQC-2816] validate project names

--- tests/integration/test_legacy_project.py
+++ tests/integration/test_legacy_project.py
@@ -34,3 +34,15 @@ def test_project_auto_audit_parameters(client, rand_gen):
             "quality_mode must be set instead of auto_audit_percentage or auto_audit_number_of_labels."
     ):
         client.create_project(name=rand_gen(str), auto_audit_number_of_labels=2)
+
+
+def test_project_name_parameter(client, rand_gen):
+    with pytest.raises(ValueError,
+                       match="project name must be a valid string."
+    ):
+        client.create_project()
+
+    with pytest.raises(ValueError,
+                       match="project name must be a valid string."
+    ):
+        client.create_project(name="     ")
