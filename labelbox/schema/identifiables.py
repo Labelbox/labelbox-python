34fe8f3610e3b07c5fceaab62d9cafe1d7c16002
Val Brodsky <vbrodsky@labelbox.com>
2023-11-27 13:35:25 -0800

    Add doc strings to Identifiable / Identifiables

--- labelbox/schema/identifiables.py
+++ labelbox/schema/identifiables.py
@@ -3,6 +3,12 @@ from typing import List, Union
 
 
 class IdType(str, Enum):
+    """
+    The type of id used to identify a data row.
+    Currently supported types are:
+        - DataRowId: The id assigned to a data row by Labelbox.
+        - GlobalKey: The id assigned to a data row by the user.
+    """
     DataRowId = "UID"
     GlobalKey = "GLOBAL_KEY"
 
@@ -10,6 +16,11 @@ class IdType(str, Enum):
 class Identifiables:
 
     def __init__(self, iterable, id_type: IdType):
+        """
+        Args:
+            iterable: Iterable of ids (unique or global keys)
+            id_type: The type of id used to identify a data row.
+        """
         self._iterable = iterable
         self._index = 0
         self._id_type = id_type
@@ -19,12 +30,18 @@ class Identifiables:
 
 
 class UniqueIds(Identifiables):
+    """
+    Represents a collection of unique, internally generated ids.
+    """
 
     def __init__(self, iterable: List[str]):
         super().__init__(iterable, IdType.DataRowId)
 
 
 class GlobalKeys(Identifiables):
+    """
+    Represents a collection of user generated ids.
+    """
 
     def __init__(self, iterable: List[str]):
         super().__init__(iterable, IdType.GlobalKey)
