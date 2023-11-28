d61b9739381d328d5c42861ef1433fdb0d6c7284
Val Brodsky <vbrodsky@labelbox.com>
2023-11-28 14:50:46 -0800

    Rename IdType(s) to match Adv

--- tests/unit/test_unit_identifiables.py
+++ tests/unit/test_unit_identifiables.py
@@ -5,11 +5,11 @@ def test_unique_ids():
     ids = ["a", "b", "c"]
     identifiables = UniqueIds(ids)
     assert [i for i in identifiables] == ids
-    assert identifiables._id_type == "UID"
+    assert identifiables._id_type == "ID"
 
 
 def test_global_keys():
     ids = ["a", "b", "c"]
     identifiables = GlobalKeys(ids)
     assert [i for i in identifiables] == ids
-    assert identifiables._id_type == "GLOBAL_KEY"
+    assert identifiables._id_type == "GKEY"
