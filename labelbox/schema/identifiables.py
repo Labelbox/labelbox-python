78e3c69ade55e5dd224ad0bb0fb7c4467f48b8ae
Yamini Kancharana <ykancharana@labelbox.com>
2023-12-11 15:28:29 -0800

    fix indentation error

--- labelbox/schema/identifiables.py
+++ labelbox/schema/identifiables.py
@@ -5,6 +5,7 @@ from typing import List, Union
 class IdType(str, Enum):
     """
     The type of id used to identify a data row.
+    
     Currently supported types are:
         - DataRowId: The id assigned to a data row by Labelbox.
         - GlobalKey: The id assigned to a data row by the user.
