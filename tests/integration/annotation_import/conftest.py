52f55496035d0ffc889040e71f24a0e0fb6fd67a
kozikkamil <kkozik@labelbox.com>
2024-02-05 16:04:17 +0100

    Fix lint

--- tests/integration/annotation_import/conftest.py
+++ tests/integration/annotation_import/conftest.py
@@ -761,13 +761,6 @@ def polygon_inference(prediction_id_mapping):
         }, {
             "x": 28.308,
             "y": 169.846
-        }],
-        "customMetrics": [{
-            "name": "customMetric1",
-            "value": 0.4
-        }, {
-            "name": "customMetric2",
-            "value": 0.6
         }]
     })
     del polygon['tool']
@@ -793,10 +786,6 @@ def rectangle_inference(prediction_id_mapping):
             "height": 65,
             "width": 12
         },
-        "customMetrics": [{
-            "name": "customMetric1",
-            "value": 0.4
-        }],
         'classifications': [{
             "schemaId":
                 rectangle['tool']['classifications'][0]['featureSchemaId'],
