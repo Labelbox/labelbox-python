cd26ee39188b36b6013be3b8cf95d756cfa0a1fc
Adrian Chang <achang@labelbox.com>
2024-03-11 21:36:28 -0700

    remove python 3.7

--- setup.py
+++ setup.py
@@ -33,9 +33,11 @@ setuptools.setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     python_requires='>=3.7',
     keywords=["labelbox"],
