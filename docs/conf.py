75eaea6ffa87468cfd29154300d286d341c4ef70
Adrian Chang <achang@labelbox.com>
2024-03-25 17:22:44 -0700

    make it work for develop

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,11 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
+
 
 # -- Project information -----------------------------------------------------
 
@@ -24,11 +28,8 @@ release = '3.66.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'multiproject', 
-    'sphinx.ext.autodoc', 
-    'sphinx.ext.viewcode', 
-    'sphinx.ext.napoleon', 
-    'sphinx_rtd_theme'
+    'multiproject', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon', 'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -39,11 +40,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-multiproject_projects = {
-    "labelbox": {
-        "path": "labelbox"
-    }
-}
+multiproject_projects = { "labelbox": { "path": "labelbox" } }
 
 # -- Options for HTML output -------------------------------------------------
 
