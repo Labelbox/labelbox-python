ae4f0491fbe95f48dec83b597620e8f83fca409d
Val Brodsky <vbrodsky@labelbox.com>
2024-04-16 11:12:36 -0700

    Update CHANGELOG for the new release

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = 'Python SDK reference'
 copyright = '2024, Labelbox'
 author = 'Labelbox'
-release = '3.67.0'
+release = '3.68.0'
 
 # -- General configuration ---------------------------------------------------
 
