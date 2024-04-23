b291934df633b8e2da8b3f37e36c04ed56f0e367
Attila Papai <apapai@labelbox.com>
2024-04-23 18:32:37 +0200

    use create_data_rows

--- libs/labelbox/tests/data/export/streamable/test_export_embeddings_streamable.py
+++ libs/labelbox/tests/data/export/streamable/test_export_embeddings_streamable.py
@@ -39,13 +39,15 @@ class TestExportEmbeddings:
     def test_export_embeddings_custom(self, client, dataset, image_url,
                                       embedding):
         vector = [random.uniform(1.0, 2.0) for _ in range(embedding.dims)]
-        dataset.create_data_row(
-            row_data=image_url,
-            embeddings=[{
+        import_task = dataset.create_data_rows([{
+            "row_data": image_url,
+            "embeddings": [{
                 "embedding_id": embedding.id,
                 "vector": vector,
             }],
-        )
+        }])
+        import_task.wait_till_done()
+        assert import_task.status == "COMPLETE"
 
         export_task = dataset.export(params={"embeddings": True})
         export_task.wait_till_done()
@@ -64,10 +66,10 @@ class TestExportEmbeddings:
         assert "embeddings" in results[0]
         assert (len(results[0]["embeddings"])
                 >= 1)  # should at least contain the custom embedding
-        for embedding in results[0]["embeddings"]:
-            if embedding["id"] == embedding.id:
-                assert embedding["name"] == embedding.name
-                assert embedding["dimensions"] == embedding.dims
-                assert embedding["is_custom"] == True
-                assert len(embedding["values"]) == 1
-                assert embedding["values"][0]["value"] == vector
+        for emb in results[0]["embeddings"]:
+            if emb["id"] == embedding.id:
+                assert emb["name"] == embedding.name
+                assert emb["dimensions"] == embedding.dims
+                assert emb["is_custom"] == True
+                assert len(emb["values"]) == 1
+                assert emb["values"][0]["value"] == vector
