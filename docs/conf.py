f0e2fb0b14894df60d88168a0c13bde9586c2cf3
Adrian Chang <achang@labelbox.com>
2024-04-25 11:26:02 -0700

    prep 3.69.0

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = 'Python SDK reference'
 copyright = '2024, Labelbox'
 author = 'Labelbox'
-release = '3.68.0'
+release = '3.69.0'
 
 # -- General configuration ---------------------------------------------------
 
