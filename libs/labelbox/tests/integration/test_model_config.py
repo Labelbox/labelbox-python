d1dce8b25ef6f37bc21f7799a902e0d637585241
Samuel Fendell <sfendell@labelbox.com>
2024-05-28 10:19:35 -0700

    Throw ResourceNotFound error when deleting nonexistant model config

--- libs/labelbox/tests/integration/test_model_config.py
+++ libs/labelbox/tests/integration/test_model_config.py
@@ -1,4 +1,5 @@
 import pytest
+from labelbox.exceptions import ResourceNotFoundError
 
 def test_create_model_config(client, valid_model_id):
     model_config = client.create_model_config("model_config", valid_model_id, {"param": "value"})
@@ -10,3 +11,7 @@ def test_create_model_config(client, valid_model_id):
 def test_delete_model_config(client, valid_model_id):
     model_config_id = client.create_model_config("model_config", valid_model_id, {"param": "value"})
     assert(client.delete_model_config(model_config_id.uid))
+
+def test_delete_nonexistant_model_config(client):
+    with pytest.raises(ResourceNotFoundError):
+        client.delete_model_config("invalid_model_id")
