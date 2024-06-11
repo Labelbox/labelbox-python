a6459398f926912327e98a77c6c3be0696a44105
Val Brodsky <vbrodsky@labelbox.com>
2024-06-11 16:57:21 -0700

    Revert "[PLT-1107] Add validation for empty row_data for Dataset create_data_rows (#1667)"
    This reverts commit e23c3010ff1f51dfdefc995e719661b1ea957f6b, reversing
    changes made to 75383027e2ff9fda0a1e122c647171562d1b6785.

--- libs/labelbox/tests/unit/test_data_row_upsert_data.py
+++ libs/labelbox/tests/unit/test_data_row_upsert_data.py
@@ -1,13 +1,7 @@
-from unittest.mock import MagicMock, patch
-
 import pytest
-from labelbox.schema.internal.data_row_upsert_item import (DataRowUpsertItem,
-                                                           DataRowCreateItem)
+from labelbox.schema.internal.data_row_upsert_item import (DataRowUpsertItem)
 from labelbox.schema.identifiable import UniqueId, GlobalKey
 from labelbox.schema.asset_attachment import AttachmentType
-from labelbox.schema.dataset import Dataset
-from labelbox.schema.internal.descriptor_file_creator import DescriptorFileCreator
-from labelbox.schema.data_row import DataRow
 
 
 @pytest.fixture
@@ -33,17 +27,6 @@ def data_row_create_items():
 
 
 @pytest.fixture
-def data_row_create_items_row_data_none():
-    dataset_id = 'test_dataset'
-    items = [
-        {
-            "row_data": None,
-        },
-    ]
-    return dataset_id, items
-
-
-@pytest.fixture
 def data_row_update_items():
     dataset_id = 'test_dataset'
     items = [
@@ -63,7 +46,7 @@ def test_data_row_upsert_items(data_row_create_items, data_row_update_items):
     dataset_id, create_items = data_row_create_items
     dataset_id, update_items = data_row_update_items
     items = create_items + update_items
-    result = DataRowUpsertItem.build(dataset_id, items)
+    result = DataRowUpsertItem.build(dataset_id, items, (UniqueId, GlobalKey))
     assert len(result) == len(items)
     for item, res in zip(items, result):
         assert res.payload == item
@@ -71,7 +54,7 @@ def test_data_row_upsert_items(data_row_create_items, data_row_update_items):
 
 def test_data_row_create_items(data_row_create_items):
     dataset_id, items = data_row_create_items
-    result = DataRowCreateItem.build(dataset_id, items)
+    result = DataRowUpsertItem.build(dataset_id, items)
     assert len(result) == len(items)
     for item, res in zip(items, result):
         assert res.payload == item
@@ -80,75 +63,4 @@ def test_data_row_create_items(data_row_create_items):
 def test_data_row_create_items_not_updateable(data_row_update_items):
     dataset_id, items = data_row_update_items
     with pytest.raises(ValueError):
-        DataRowCreateItem.build(dataset_id, items)
-
-
-def test_upsert_is_empty():
-    item = DataRowUpsertItem(id={
-        "id": UniqueId,
-        "value": UniqueId("123")
-    },
-                             payload={})
-    assert item.is_empty()
-
-    item = DataRowUpsertItem(id={
-        "id": UniqueId,
-        "value": UniqueId("123")
-    },
-                             payload={"dataset_id": "test_dataset"})
-    assert item.is_empty()
-
-    item = DataRowUpsertItem(
-        id={}, payload={"row_data": "http://my_site.com/photos/img_01.jpg"})
-    assert not item.is_empty()
-
-
-def test_create_is_empty():
-    item = DataRowCreateItem(id={}, payload={})
-    assert item.is_empty()
-
-    item = DataRowCreateItem(id={}, payload={"dataset_id": "test_dataset"})
-    assert item.is_empty()
-
-    item = DataRowCreateItem(id={}, payload={"row_data": None})
-    assert item.is_empty()
-
-    item = DataRowCreateItem(id={}, payload={"row_data": ""})
-    assert item.is_empty()
-
-    item = DataRowCreateItem(
-        id={}, payload={"row_data": "http://my_site.com/photos/img_01.jpg"})
-    assert not item.is_empty()
-
-    item = DataRowCreateItem(
-        id={},
-        payload={DataRow.row_data: "http://my_site.com/photos/img_01.jpg"})
-    assert not item.is_empty()
-
-
-def test_create_row_data_none():
-    items = [
-        {
-            "row_data": None,
-            "global_key": "global_key1",
-        },
-    ]
-    client = MagicMock()
-    dataset = Dataset(
-        client, {
-            "id": 'test_dataset',
-            "name": 'test_dataset',
-            "createdAt": "2021-06-01T00:00:00.000Z",
-            "description": "test_dataset",
-            "updatedAt": "2021-06-01T00:00:00.000Z",
-            "rowCount": 0,
-        })
-
-    with patch.object(DescriptorFileCreator,
-                      'create',
-                      return_value=["http://bar.com/chunk_uri"]):
-        with pytest.raises(ValueError,
-                           match="Some items have an empty payload"):
-            dataset.create_data_rows(items)
-
-    client.execute.assert_not_called()
+        DataRowUpsertItem.build(dataset_id, items, ())
