744f207447552df85443532d1a510eb16d7e8043
Adrian Chang <achang@labelbox.com>
2024-07-18 00:58:17 -0700

    pass linting

--- libs/lbox-cli/src/lbox/cli/main.py
+++ libs/lbox-cli/src/lbox/cli/main.py
@@ -2,17 +2,23 @@ import click
 import tinydb
 from pathlib import Path
 
+
 @click.group()
 def cli():
-    db = tinydb.TinyDB(Path.home() / '~/.lbox/db.json', create_dirs=True, access_mode='r+')
+    db = tinydb.TinyDB(
+        Path.home() / "~/.lbox/db.json", create_dirs=True, access_mode="r+"
+    )
+
 
 @cli.group()
 def key():
     pass
 
+
 @key.command()
 def select():
-    print('select') 
+    print("select")
+
 
 def init():
-    return 'init'
+    return "init"
