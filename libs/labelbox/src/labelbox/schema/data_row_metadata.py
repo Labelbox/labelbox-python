a937770184bca18d39c8d4cbeed8fc5214aa56db
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-07-22 23:31:29 -0500

    fixed final bad test

--- libs/labelbox/src/labelbox/schema/data_row_metadata.py
+++ libs/labelbox/src/labelbox/schema/data_row_metadata.py
@@ -120,7 +120,7 @@ _BatchFunction = Callable[[_BatchInputs], List[DataRowMetadataBatchResponse]]
 
 
 class _UpsertCustomMetadataSchemaEnumOptionInput(_CamelCaseMixin):
-    id: Optional[SchemaId]
+    id: Optional[SchemaId] = None
     name: Annotated[str, StringConstraints(strip_whitespace=True,
                                         min_length=1,
                                         max_length=100)]
