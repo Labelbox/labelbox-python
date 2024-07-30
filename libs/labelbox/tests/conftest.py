69a38172d02524e60d1e6d5163670b5a011b2dbf
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-07-30 11:22:29 -0500

    set up embeddings to be reusable

--- libs/labelbox/tests/conftest.py
+++ libs/labelbox/tests/conftest.py
@@ -1063,7 +1063,7 @@ def configured_project_with_complex_ontology(client, initial_dataset, rand_gen,
     project.delete()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def embedding(client: Client):
     uuid_str = uuid.uuid4().hex
     embedding = client.create_embedding(f"sdk-int-{uuid_str}", 8)
