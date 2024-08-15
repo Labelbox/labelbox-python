db43e0fc93bde50828712a8ac0d30f886d8d16f1
Val Brodsky <vbrodsky@labelbox.com>
2024-08-15 10:22:40 -0700

    Add safe handling to LabelingServiceStatus

--- libs/labelbox/src/labelbox/schema/labeling_service_status.py
+++ libs/labelbox/src/labelbox/schema/labeling_service_status.py
@@ -8,3 +8,23 @@ class LabelingServiceStatus(Enum):
     Production = 'PRODUCTION'
     Requested = 'REQUESTED'
     SetUp = 'SET_UP'
+    Missing = None
+
+    @classmethod
+    def is_supported(cls, value):
+        return isinstance(value, cls)
+
+    @classmethod
+    def _missing_(cls, value) -> 'LabelingServiceStatus':
+        """Handle missing null new task types
+            Handle upper case names for compatibility with
+            the GraphQL"""
+
+        if value is None:
+            return cls.Missing
+
+        for name, member in cls.__members__.items():
+            if value == name.upper():
+                return member
+
+        return cls.Missing
