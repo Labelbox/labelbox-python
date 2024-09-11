c74ea18b73979d27ee621db7b66f5e25baa45fda
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-09-11 11:13:31 -0500

    started unit test

--- libs/labelbox/tests/data/serialization/ndjson/test_conversation.py
+++ libs/labelbox/tests/data/serialization/ndjson/test_conversation.py
@@ -3,131 +3,183 @@ import json
 import pytest
 import labelbox.types as lb_types
 from labelbox.data.serialization.ndjson.converter import NDJsonConverter
+from labelbox.data.mixins import CustomMetric
 
-radio_ndjson = [{
-    'dataRow': {
-        'globalKey': 'my_global_key'
-    },
-    'name': 'radio',
-    'answer': {
-        'name': 'first_radio_answer'
-    },
-    'messageId': '0'
-}]
+radio_ndjson = [
+    {
+        "dataRow": {"globalKey": "my_global_key"},
+        "name": "radio",
+        "answer": {"name": "first_radio_answer"},
+        "messageId": "0",
+    }
+]
 
 radio_label = [
     lb_types.Label(
-        data=lb_types.ConversationData(global_key='my_global_key'),
+        data=lb_types.ConversationData(global_key="my_global_key"),
         annotations=[
             lb_types.ClassificationAnnotation(
-                name='radio',
-                value=lb_types.Radio(answer=lb_types.ClassificationAnswer(
-                    name="first_radio_answer")),
-                message_id="0")
-        ])
+                name="radio",
+                value=lb_types.Radio(
+                    answer=lb_types.ClassificationAnswer(name="first_radio_answer")
+                ),
+                message_id="0",
+            )
+        ],
+    )
 ]
 
-checklist_ndjson = [{
-    'dataRow': {
-        'globalKey': 'my_global_key'
-    },
-    'name': 'checklist',
-    'answer': [
-        {
-            'name': 'first_checklist_answer'
-        },
-        {
-            'name': 'second_checklist_answer'
-        },
-    ],
-    'messageId': '2'
-}]
+checklist_ndjson = [
+    {
+        "dataRow": {"globalKey": "my_global_key"},
+        "name": "checklist",
+        "answer": [
+            {"name": "first_checklist_answer"},
+            {"name": "second_checklist_answer"},
+        ],
+        "messageId": "2",
+    }
+]
 
 checklist_label = [
-    lb_types.Label(data=lb_types.ConversationData(global_key='my_global_key'),
-                   annotations=[
-                       lb_types.ClassificationAnnotation(
-                           name='checklist',
-                           message_id="2",
-                           value=lb_types.Checklist(answer=[
-                               lb_types.ClassificationAnswer(
-                                   name="first_checklist_answer"),
-                               lb_types.ClassificationAnswer(
-                                   name="second_checklist_answer")
-                           ]))
-                   ])
+    lb_types.Label(
+        data=lb_types.ConversationData(global_key="my_global_key"),
+        annotations=[
+            lb_types.ClassificationAnnotation(
+                name="checklist",
+                message_id="2",
+                value=lb_types.Checklist(
+                    answer=[
+                        lb_types.ClassificationAnswer(name="first_checklist_answer"),
+                        lb_types.ClassificationAnswer(name="second_checklist_answer"),
+                    ]
+                ),
+            )
+        ],
+    )
 ]
 
-free_text_ndjson = [{
-    'dataRow': {
-        'globalKey': 'my_global_key'
-    },
-    'name': 'free_text',
-    'answer': 'sample text',
-    'messageId': '0'
-}]
+free_text_ndjson = [
+    {
+        "dataRow": {"globalKey": "my_global_key"},
+        "name": "free_text",
+        "answer": "sample text",
+        "messageId": "0",
+    }
+]
 free_text_label = [
-    lb_types.Label(data=lb_types.ConversationData(global_key='my_global_key'),
-                   annotations=[
-                       lb_types.ClassificationAnnotation(
-                           name='free_text',
-                           message_id="0",
-                           value=lb_types.Text(answer="sample text"))
-                   ])
+    lb_types.Label(
+        data=lb_types.ConversationData(global_key="my_global_key"),
+        annotations=[
+            lb_types.ClassificationAnnotation(
+                name="free_text",
+                message_id="0",
+                value=lb_types.Text(answer="sample text"),
+            )
+        ],
+    )
 ]
 
 
 @pytest.mark.parametrize(
     "label, ndjson",
-    [[radio_label, radio_ndjson], [checklist_label, checklist_ndjson],
-     [free_text_label, free_text_ndjson]])
+    [
+        [radio_label, radio_ndjson],
+        [checklist_label, checklist_ndjson],
+        [free_text_label, free_text_ndjson],
+    ],
+)
 def test_message_based_radio_classification(label, ndjson):
     serialized_label = list(NDJsonConverter().serialize(label))
-    serialized_label[0].pop('uuid')
+    serialized_label[0].pop("uuid")
     assert serialized_label == ndjson
 
-    deserialized_label = list(NDJsonConverter().deserialize(ndjson))
-    deserialized_label[0].annotations[0].extra.pop('uuid')
-    assert deserialized_label[0].model_dump(exclude_none=True) == label[0].model_dump(exclude_none=True)
 
-
-@pytest.mark.parametrize("filename", [
-    "tests/data/assets/ndjson/conversation_entity_import.json",
-    "tests/data/assets/ndjson/conversation_entity_without_confidence_import.json"
-])
-def test_conversation_entity_import(filename: str):
-    with open(filename, 'r') as file:
+def test_conversation_entity_import():
+    with open("tests/data/assets/ndjson/conversation_entity_import.json", "r") as file:
         data = json.load(file)
-    res = list(NDJsonConverter.deserialize(data))
-    res = list(NDJsonConverter.serialize(res))
+
+    label = lb_types.Label(
+        data=lb_types.GenericDataRowData(
+            uid="cl6xnv9h61fv0085yhtoq06ht",
+        ),
+        annotations=[
+            lb_types.ObjectAnnotation(
+                custom_metrics=[
+                    CustomMetric(name="customMetric1", value=0.5),
+                    CustomMetric(name="customMetric2", value=0.3),
+                ],
+                confidence=0.53,
+                name="some-text-entity",
+                feature_schema_id="cl6xnuwt95lqq07330tbb3mfd",
+                extra={"uuid": "5ad9c52f-058d-49c8-a749-3f20b84f8cd4"},
+                value=lb_types.ConversationEntity(
+                    start=67, end=128, message_id="some-message-id"
+                ),
+            )
+        ],
+    )
+
+    res = list(NDJsonConverter.serialize([label]))
+    assert res == data
+
+
+def test_conversation_entity_import_without_confidence():
+    with open(
+        "tests/data/assets/ndjson/conversation_entity_without_confidence_import.json",
+        "r",
+    ) as file:
+        data = json.load(file)
+    label = lb_types.Label(
+        uid=None,
+        data=lb_types.GenericDataRowData(
+            uid="cl6xnv9h61fv0085yhtoq06ht",
+        ),
+        annotations=[
+            lb_types.ObjectAnnotation(
+                name="some-text-entity",
+                feature_schema_id="cl6xnuwt95lqq07330tbb3mfd",
+                extra={"uuid": "5ad9c52f-058d-49c8-a749-3f20b84f8cd4"},
+                value=lb_types.ConversationEntity(
+                    start=67, end=128, extra={}, message_id="some-message-id"
+                ),
+            )
+        ],
+    )
+
+    res = list(NDJsonConverter.serialize([label]))
     assert res == data
 
 
 def test_benchmark_reference_label_flag_enabled():
-    label = lb_types.Label(data=lb_types.ConversationData(global_key='my_global_key'),
-                   annotations=[
-                       lb_types.ClassificationAnnotation(
-                           name='free_text',
-                           message_id="0",
-                           value=lb_types.Text(answer="sample text"))
-                   ],
-                   is_benchmark_reference=True
-                   )
+    label = lb_types.Label(
+        data=lb_types.ConversationData(global_key="my_global_key"),
+        annotations=[
+            lb_types.ClassificationAnnotation(
+                name="free_text",
+                message_id="0",
+                value=lb_types.Text(answer="sample text"),
+            )
+        ],
+        is_benchmark_reference=True,
+    )
 
     res = list(NDJsonConverter.serialize([label]))
     assert res[0]["isBenchmarkReferenceLabel"]
 
 
 def test_benchmark_reference_label_flag_disabled():
-    label = lb_types.Label(data=lb_types.ConversationData(global_key='my_global_key'),
-                   annotations=[
-                       lb_types.ClassificationAnnotation(
-                           name='free_text',
-                           message_id="0",
-                           value=lb_types.Text(answer="sample text"))
-                   ],
-                   is_benchmark_reference=False
-                   )
+    label = lb_types.Label(
+        data=lb_types.ConversationData(global_key="my_global_key"),
+        annotations=[
+            lb_types.ClassificationAnnotation(
+                name="free_text",
+                message_id="0",
+                value=lb_types.Text(answer="sample text"),
+            )
+        ],
+        is_benchmark_reference=False,
+    )
 
     res = list(NDJsonConverter.serialize([label]))
     assert not res[0].get("isBenchmarkReferenceLabel")
--- libs/labelbox/tests/data/serialization/ndjson/test_global_key.py
+++ libs/labelbox/tests/data/serialization/ndjson/test_global_key.py
@@ -1,68 +1,72 @@
-import json
-import pytest
-
-from labelbox.data.serialization.ndjson.classification import NDRadio
-
 from labelbox.data.serialization.ndjson.converter import NDJsonConverter
-from labelbox.data.serialization.ndjson.objects import NDLine
+from labelbox.types import (
+    GenericDataRowData,
+    Label,
+    ClassificationAnnotation,
+    Radio,
+    ClassificationAnswer,
+)
 
 
-def round_dict(data):
-    if isinstance(data, dict):
-        for key in data:
-            if isinstance(data[key], float):
-                data[key] = int(data[key])
-            elif isinstance(data[key], dict):
-                data[key] = round_dict(data[key])
-            elif isinstance(data[key], (list, tuple)):
-                data[key] = [round_dict(r) for r in data[key]]
+def test_generic_data_row_global_key_included():
+    expected = [
+        {
+            "answer": {"schemaId": "ckrb1sfl8099g0y91cxbd5ftb"},
+            "dataRow": {"globalKey": "ckrb1sf1i1g7i0ybcdc6oc8ct"},
+            "schemaId": "ckrb1sfjx099a0y914hl319ie",
+            "uuid": "f6879f59-d2b5-49c2-aceb-d9e8dc478673",
+        }
+    ]
 
-    return data
+    label = Label(
+        data=GenericDataRowData(
+            global_key="ckrb1sf1i1g7i0ybcdc6oc8ct",
+        ),
+        annotations=[
+            ClassificationAnnotation(
+                feature_schema_id="ckrb1sfjx099a0y914hl319ie",
+                extra={"uuid": "f6879f59-d2b5-49c2-aceb-d9e8dc478673"},
+                value=Radio(
+                    answer=ClassificationAnswer(
+                        feature_schema_id="ckrb1sfl8099g0y91cxbd5ftb",
+                    ),
+                ),
+            )
+        ],
+    )
 
+    res = list(NDJsonConverter.serialize([label]))
 
-@pytest.mark.parametrize('filename', [
-    'tests/data/assets/ndjson/classification_import_global_key.json',
-    'tests/data/assets/ndjson/metric_import_global_key.json',
-    'tests/data/assets/ndjson/polyline_import_global_key.json',
-    'tests/data/assets/ndjson/text_entity_import_global_key.json',
-    'tests/data/assets/ndjson/conversation_entity_import_global_key.json',
-])
-def test_many_types(filename: str):
-    with open(filename, 'r') as f:
-        data = json.load(f)
-    res = list(NDJsonConverter.deserialize(data))
-    res = list(NDJsonConverter.serialize(res))
-    assert res == data
-    f.close()
-
-
-def test_image():
-    with open('tests/data/assets/ndjson/image_import_global_key.json',
-              'r') as f:
-        data = json.load(f)
-    res = list(NDJsonConverter.deserialize(data))
-    res = list(NDJsonConverter.serialize(res))
-    for r in res:
-        r.pop('classifications', None)
-    assert [round_dict(x) for x in res] == [round_dict(x) for x in data]
-    f.close()
+    assert res == expected
 
 
-def test_pdf():
-    with open('tests/data/assets/ndjson/pdf_import_global_key.json', 'r') as f:
-        data = json.load(f)
-    res = list(NDJsonConverter.deserialize(data))
-    res = list(NDJsonConverter.serialize(res))
-    assert [round_dict(x) for x in res] == [round_dict(x) for x in data]
-    f.close()
+def test_dict_data_row_global_key_included():
+    expected = [
+        {
+            "answer": {"schemaId": "ckrb1sfl8099g0y91cxbd5ftb"},
+            "dataRow": {"globalKey": "ckrb1sf1i1g7i0ybcdc6oc8ct"},
+            "schemaId": "ckrb1sfjx099a0y914hl319ie",
+            "uuid": "f6879f59-d2b5-49c2-aceb-d9e8dc478673",
+        }
+    ]
 
+    label = Label(
+        data={
+            "global_key": "ckrb1sf1i1g7i0ybcdc6oc8ct",
+        },
+        annotations=[
+            ClassificationAnnotation(
+                feature_schema_id="ckrb1sfjx099a0y914hl319ie",
+                extra={"uuid": "f6879f59-d2b5-49c2-aceb-d9e8dc478673"},
+                value=Radio(
+                    answer=ClassificationAnswer(
+                        feature_schema_id="ckrb1sfl8099g0y91cxbd5ftb",
+                    ),
+                ),
+            )
+        ],
+    )
 
-def test_video():
-    with open('tests/data/assets/ndjson/video_import_global_key.json',
-              'r') as f:
-        data = json.load(f)
+    res = list(NDJsonConverter.serialize([label]))
 
-    res = list(NDJsonConverter.deserialize(data))
-    res = list(NDJsonConverter.serialize(res))
-    assert res == [data[2], data[0], data[1], data[3], data[4], data[5]]
-    f.close()
+    assert res == expected
