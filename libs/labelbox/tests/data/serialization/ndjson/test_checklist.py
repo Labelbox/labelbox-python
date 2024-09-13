a55046651333bdcf8757c8230297e6502b527148
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-09-12 20:36:15 -0500

    formatted

--- libs/labelbox/tests/data/serialization/ndjson/test_checklist.py
+++ libs/labelbox/tests/data/serialization/ndjson/test_checklist.py
@@ -1,5 +1,9 @@
 from labelbox.data.annotation_types.annotation import ClassificationAnnotation
-from labelbox.data.annotation_types.classification.classification import Checklist, ClassificationAnswer, Radio
+from labelbox.data.annotation_types.classification.classification import (
+    Checklist,
+    ClassificationAnswer,
+    Radio,
+)
 from labelbox.data.annotation_types.data.text import TextData
 from labelbox.data.annotation_types.label import Label
 
@@ -16,19 +20,15 @@ def test_serialization_min():
         annotations=[
             ClassificationAnnotation(
                 name="checkbox_question_geo",
-                value=Checklist(
-                    answer=[ClassificationAnswer(name="first_answer")]),
+                value=Checklist(answer=[ClassificationAnswer(name="first_answer")]),
             )
-        ])
+        ],
+    )
 
     expected = {
-        'name': 'checkbox_question_geo',
-        'dataRow': {
-            'id': 'bkj7z2q0b0000jx6x0q2q7q0d'
-        },
-        'answer': [{
-            'name': 'first_answer'
-        }]
+        "name": "checkbox_question_geo",
+        "dataRow": {"id": "bkj7z2q0b0000jx6x0q2q7q0d"},
+        "answer": [{"name": "first_answer"}],
     }
     serialized = NDJsonConverter.serialize([label])
     res = next(serialized)
@@ -47,61 +47,76 @@ def test_serialization_with_classification():
             ClassificationAnnotation(
                 name="checkbox_question_geo",
                 confidence=0.5,
-                value=Checklist(answer=[
-                    ClassificationAnswer(
-                        name="first_answer",
-                        confidence=0.1,
-                        classifications=[
-                            ClassificationAnnotation(
-                                name="sub_radio_question",
-                                value=Radio(answer=ClassificationAnswer(
-                                    name="first_sub_radio_answer",
-                                    confidence=0.31))),
-                            ClassificationAnnotation(
-                                name="sub_chck_question",
-                                value=Checklist(answer=[
-                                    ClassificationAnswer(
-                                        name="second_subchk_answer",
-                                        confidence=0.41),
-                                    ClassificationAnswer(
-                                        name="third_subchk_answer",
-                                        confidence=0.42),
-                                ],))
-                        ]),
-                ]))
-        ])
+                value=Checklist(
+                    answer=[
+                        ClassificationAnswer(
+                            name="first_answer",
+                            confidence=0.1,
+                            classifications=[
+                                ClassificationAnnotation(
+                                    name="sub_radio_question",
+                                    value=Radio(
+                                        answer=ClassificationAnswer(
+                                            name="first_sub_radio_answer",
+                                            confidence=0.31,
+                                        )
+                                    ),
+                                ),
+                                ClassificationAnnotation(
+                                    name="sub_chck_question",
+                                    value=Checklist(
+                                        answer=[
+                                            ClassificationAnswer(
+                                                name="second_subchk_answer",
+                                                confidence=0.41,
+                                            ),
+                                            ClassificationAnswer(
+                                                name="third_subchk_answer",
+                                                confidence=0.42,
+                                            ),
+                                        ],
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ]
+                ),
+            )
+        ],
+    )
 
     expected = {
-        'confidence':
-            0.5,
-        'name':
-            'checkbox_question_geo',
-        'dataRow': {
-            'id': 'bkj7z2q0b0000jx6x0q2q7q0d'
-        },
-        'answer': [{
-            'confidence':
-                0.1,
-            'name':
-                'first_answer',
-            'classifications': [{
-                'name': 'sub_radio_question',
-                'answer': {
-                    'confidence': 0.31,
-                    'name': 'first_sub_radio_answer',
-                }
-            }, {
-                'name':
-                    'sub_chck_question',
-                'answer': [{
-                    'confidence': 0.41,
-                    'name': 'second_subchk_answer',
-                }, {
-                    'confidence': 0.42,
-                    'name': 'third_subchk_answer',
-                }]
-            }]
-        }]
+        "confidence": 0.5,
+        "name": "checkbox_question_geo",
+        "dataRow": {"id": "bkj7z2q0b0000jx6x0q2q7q0d"},
+        "answer": [
+            {
+                "confidence": 0.1,
+                "name": "first_answer",
+                "classifications": [
+                    {
+                        "name": "sub_radio_question",
+                        "answer": {
+                            "confidence": 0.31,
+                            "name": "first_sub_radio_answer",
+                        },
+                    },
+                    {
+                        "name": "sub_chck_question",
+                        "answer": [
+                            {
+                                "confidence": 0.41,
+                                "name": "second_subchk_answer",
+                            },
+                            {
+                                "confidence": 0.42,
+                                "name": "third_subchk_answer",
+                            },
+                        ],
+                    },
+                ],
+            }
+        ],
     }
 
     serialized = NDJsonConverter.serialize([label])
@@ -122,66 +137,80 @@ def test_serialization_with_classification_double_nested():
             ClassificationAnnotation(
                 name="checkbox_question_geo",
                 confidence=0.5,
-                value=Checklist(answer=[
-                    ClassificationAnswer(
-                        name="first_answer",
-                        confidence=0.1,
-                        classifications=[
-                            ClassificationAnnotation(
-                                name="sub_radio_question",
-                                value=Radio(answer=ClassificationAnswer(
-                                    name="first_sub_radio_answer",
-                                    confidence=0.31,
-                                    classifications=[
-                                        ClassificationAnnotation(
-                                            name="sub_chck_question",
-                                            value=Checklist(answer=[
-                                                ClassificationAnswer(
-                                                    name="second_subchk_answer",
-                                                    confidence=0.41),
-                                                ClassificationAnswer(
-                                                    name="third_subchk_answer",
-                                                    confidence=0.42),
-                                            ],))
-                                    ]))),
-                        ]),
-                ]))
-        ])
+                value=Checklist(
+                    answer=[
+                        ClassificationAnswer(
+                            name="first_answer",
+                            confidence=0.1,
+                            classifications=[
+                                ClassificationAnnotation(
+                                    name="sub_radio_question",
+                                    value=Radio(
+                                        answer=ClassificationAnswer(
+                                            name="first_sub_radio_answer",
+                                            confidence=0.31,
+                                            classifications=[
+                                                ClassificationAnnotation(
+                                                    name="sub_chck_question",
+                                                    value=Checklist(
+                                                        answer=[
+                                                            ClassificationAnswer(
+                                                                name="second_subchk_answer",
+                                                                confidence=0.41,
+                                                            ),
+                                                            ClassificationAnswer(
+                                                                name="third_subchk_answer",
+                                                                confidence=0.42,
+                                                            ),
+                                                        ],
+                                                    ),
+                                                )
+                                            ],
+                                        )
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ]
+                ),
+            )
+        ],
+    )
 
     expected = {
-        'confidence':
-            0.5,
-        'name':
-            'checkbox_question_geo',
-        'dataRow': {
-            'id': 'bkj7z2q0b0000jx6x0q2q7q0d'
-        },
-        'answer': [{
-            'confidence':
-                0.1,
-            'name':
-                'first_answer',
-            'classifications': [{
-                'name': 'sub_radio_question',
-                'answer': {
-                    'confidence':
-                        0.31,
-                    'name':
-                        'first_sub_radio_answer',
-                    'classifications': [{
-                        'name':
-                            'sub_chck_question',
-                        'answer': [{
-                            'confidence': 0.41,
-                            'name': 'second_subchk_answer',
-                        }, {
-                            'confidence': 0.42,
-                            'name': 'third_subchk_answer',
-                        }]
-                    }]
-                }
-            }]
-        }]
+        "confidence": 0.5,
+        "name": "checkbox_question_geo",
+        "dataRow": {"id": "bkj7z2q0b0000jx6x0q2q7q0d"},
+        "answer": [
+            {
+                "confidence": 0.1,
+                "name": "first_answer",
+                "classifications": [
+                    {
+                        "name": "sub_radio_question",
+                        "answer": {
+                            "confidence": 0.31,
+                            "name": "first_sub_radio_answer",
+                            "classifications": [
+                                {
+                                    "name": "sub_chck_question",
+                                    "answer": [
+                                        {
+                                            "confidence": 0.41,
+                                            "name": "second_subchk_answer",
+                                        },
+                                        {
+                                            "confidence": 0.42,
+                                            "name": "third_subchk_answer",
+                                        },
+                                    ],
+                                }
+                            ],
+                        },
+                    }
+                ],
+            }
+        ],
     }
     serialized = NDJsonConverter.serialize([label])
     res = next(serialized)
@@ -200,62 +229,78 @@ def test_serialization_with_classification_double_nested_2():
         annotations=[
             ClassificationAnnotation(
                 name="sub_radio_question",
-                value=Radio(answer=ClassificationAnswer(
-                    name="first_sub_radio_answer",
-                    confidence=0.31,
-                    classifications=[
-                        ClassificationAnnotation(
-                            name="sub_chck_question",
-                            value=Checklist(answer=[
-                                ClassificationAnswer(
-                                    name="second_subchk_answer",
-                                    confidence=0.41,
-                                    classifications=[
-                                        ClassificationAnnotation(
-                                            name="checkbox_question_geo",
-                                            value=Checklist(answer=[
-                                                ClassificationAnswer(
-                                                    name="first_answer",
-                                                    confidence=0.1),
-                                            ]))
-                                    ]),
-                                ClassificationAnswer(name="third_subchk_answer",
-                                                     confidence=0.42),
-                            ]))
-                    ]))),
-        ])
+                value=Radio(
+                    answer=ClassificationAnswer(
+                        name="first_sub_radio_answer",
+                        confidence=0.31,
+                        classifications=[
+                            ClassificationAnnotation(
+                                name="sub_chck_question",
+                                value=Checklist(
+                                    answer=[
+                                        ClassificationAnswer(
+                                            name="second_subchk_answer",
+                                            confidence=0.41,
+                                            classifications=[
+                                                ClassificationAnnotation(
+                                                    name="checkbox_question_geo",
+                                                    value=Checklist(
+                                                        answer=[
+                                                            ClassificationAnswer(
+                                                                name="first_answer",
+                                                                confidence=0.1,
+                                                            ),
+                                                        ]
+                                                    ),
+                                                )
+                                            ],
+                                        ),
+                                        ClassificationAnswer(
+                                            name="third_subchk_answer", confidence=0.42
+                                        ),
+                                    ]
+                                ),
+                            )
+                        ],
+                    )
+                ),
+            ),
+        ],
+    )
 
     expected = {
-        'name': 'sub_radio_question',
-        'answer': {
-            'confidence':
-                0.31,
-            'name':
-                'first_sub_radio_answer',
-            'classifications': [{
-                'name':
-                    'sub_chck_question',
-                'answer': [{
-                    'confidence':
-                        0.41,
-                    'name':
-                        'second_subchk_answer',
-                    'classifications': [{
-                        'name': 'checkbox_question_geo',
-                        'answer': [{
-                            'confidence': 0.1,
-                            'name': 'first_answer',
-                        }]
-                    }]
-                }, {
-                    'confidence': 0.42,
-                    'name': 'third_subchk_answer',
-                }]
-            }]
+        "name": "sub_radio_question",
+        "answer": {
+            "confidence": 0.31,
+            "name": "first_sub_radio_answer",
+            "classifications": [
+                {
+                    "name": "sub_chck_question",
+                    "answer": [
+                        {
+                            "confidence": 0.41,
+                            "name": "second_subchk_answer",
+                            "classifications": [
+                                {
+                                    "name": "checkbox_question_geo",
+                                    "answer": [
+                                        {
+                                            "confidence": 0.1,
+                                            "name": "first_answer",
+                                        }
+                                    ],
+                                }
+                            ],
+                        },
+                        {
+                            "confidence": 0.42,
+                            "name": "third_subchk_answer",
+                        },
+                    ],
+                }
+            ],
         },
-        'dataRow': {
-            'id': 'bkj7z2q0b0000jx6x0q2q7q0d'
-        }
+        "dataRow": {"id": "bkj7z2q0b0000jx6x0q2q7q0d"},
     }
 
     serialized = NDJsonConverter.serialize([label])
