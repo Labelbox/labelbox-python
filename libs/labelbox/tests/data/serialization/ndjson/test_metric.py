624c7ed1b48dbc6c9fc51f81fb3bfcab5a5eb919
Gabe <33893811+Gabefire@users.noreply.github.com>
2024-09-16 13:08:20 -0500

    [PLT-1463] Removed ND deserialize from some unit test part 2 (#1815)

--- libs/labelbox/tests/data/serialization/ndjson/test_metric.py
+++ libs/labelbox/tests/data/serialization/ndjson/test_metric.py
@@ -1,38 +1,166 @@
 import json
 
+from labelbox.data.annotation_types.data.generic_data_row_data import (
+    GenericDataRowData,
+)
+from labelbox.data.annotation_types.metrics.confusion_matrix import (
+    ConfusionMatrixMetric,
+)
 from labelbox.data.serialization.ndjson.converter import NDJsonConverter
+from labelbox.types import (
+    Label,
+    ScalarMetric,
+    ScalarMetricAggregation,
+    ConfusionMatrixAggregation,
+)
 
 
 def test_metric():
     with open("tests/data/assets/ndjson/metric_import.json", "r") as file:
         data = json.load(file)
 
-    label_list = list(NDJsonConverter.deserialize(data))
-    reserialized = list(NDJsonConverter.serialize(label_list))
-    assert reserialized == data
+    labels = [
+        Label(
+            data=GenericDataRowData(
+                uid="ckrmdnqj4000007msh9p2a27r",
+            ),
+            annotations=[
+                ScalarMetric(
+                    value=0.1,
+                    extra={"uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7672"},
+                    aggregation=ScalarMetricAggregation.ARITHMETIC_MEAN,
+                )
+            ],
+        )
+    ]
+
+    res = list(NDJsonConverter.serialize(labels))
+    assert res == data
 
 
 def test_custom_scalar_metric():
-    with open(
-        "tests/data/assets/ndjson/custom_scalar_import.json", "r"
-    ) as file:
-        data = json.load(file)
+    data = [
+        {
+            "uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7672",
+            "dataRow": {"id": "ckrmdnqj4000007msh9p2a27r"},
+            "metricValue": 0.1,
+            "metricName": "custom_iou",
+            "featureName": "sample_class",
+            "subclassName": "sample_subclass",
+            "aggregation": "SUM",
+        },
+        {
+            "uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7673",
+            "dataRow": {"id": "ckrmdnqj4000007msh9p2a27r"},
+            "metricValue": 0.1,
+            "metricName": "custom_iou",
+            "featureName": "sample_class",
+            "aggregation": "SUM",
+        },
+        {
+            "uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7674",
+            "dataRow": {"id": "ckrmdnqj4000007msh9p2a27r"},
+            "metricValue": {0.1: 0.1, 0.2: 0.5},
+            "metricName": "custom_iou",
+            "aggregation": "SUM",
+        },
+    ]
 
-    label_list = list(NDJsonConverter.deserialize(data))
-    reserialized = list(NDJsonConverter.serialize(label_list))
-    assert json.dumps(reserialized, sort_keys=True) == json.dumps(
-        data, sort_keys=True
-    )
+    labels = [
+        Label(
+            data=GenericDataRowData(
+                uid="ckrmdnqj4000007msh9p2a27r",
+            ),
+            annotations=[
+                ScalarMetric(
+                    value=0.1,
+                    feature_name="sample_class",
+                    subclass_name="sample_subclass",
+                    extra={"uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7672"},
+                    metric_name="custom_iou",
+                    aggregation=ScalarMetricAggregation.SUM,
+                ),
+                ScalarMetric(
+                    value=0.1,
+                    feature_name="sample_class",
+                    extra={"uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7673"},
+                    metric_name="custom_iou",
+                    aggregation=ScalarMetricAggregation.SUM,
+                ),
+                ScalarMetric(
+                    value={"0.1": 0.1, "0.2": 0.5},
+                    extra={"uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7674"},
+                    metric_name="custom_iou",
+                    aggregation=ScalarMetricAggregation.SUM,
+                ),
+            ],
+        )
+    ]
+
+    res = list(NDJsonConverter.serialize(labels))
+
+    assert res == data
 
 
 def test_custom_confusion_matrix_metric():
-    with open(
-        "tests/data/assets/ndjson/custom_confusion_matrix_import.json", "r"
-    ) as file:
-        data = json.load(file)
+    data = [
+        {
+            "uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7672",
+            "dataRow": {"id": "ckrmdnqj4000007msh9p2a27r"},
+            "metricValue": (1, 1, 2, 3),
+            "metricName": "50%_iou",
+            "featureName": "sample_class",
+            "subclassName": "sample_subclass",
+            "aggregation": "CONFUSION_MATRIX",
+        },
+        {
+            "uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7673",
+            "dataRow": {"id": "ckrmdnqj4000007msh9p2a27r"},
+            "metricValue": (0, 1, 2, 5),
+            "metricName": "50%_iou",
+            "featureName": "sample_class",
+            "aggregation": "CONFUSION_MATRIX",
+        },
+        {
+            "uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7674",
+            "dataRow": {"id": "ckrmdnqj4000007msh9p2a27r"},
+            "metricValue": {0.1: (0, 1, 2, 3), 0.2: (5, 3, 4, 3)},
+            "metricName": "50%_iou",
+            "aggregation": "CONFUSION_MATRIX",
+        },
+    ]
 
-    label_list = list(NDJsonConverter.deserialize(data))
-    reserialized = list(NDJsonConverter.serialize(label_list))
-    assert json.dumps(reserialized, sort_keys=True) == json.dumps(
-        data, sort_keys=True
-    )
+    labels = [
+        Label(
+            data=GenericDataRowData(
+                uid="ckrmdnqj4000007msh9p2a27r",
+            ),
+            annotations=[
+                ConfusionMatrixMetric(
+                    value=(1, 1, 2, 3),
+                    feature_name="sample_class",
+                    subclass_name="sample_subclass",
+                    extra={"uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7672"},
+                    metric_name="50%_iou",
+                    aggregation=ConfusionMatrixAggregation.CONFUSION_MATRIX,
+                ),
+                ConfusionMatrixMetric(
+                    value=(0, 1, 2, 5),
+                    feature_name="sample_class",
+                    extra={"uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7673"},
+                    metric_name="50%_iou",
+                    aggregation=ConfusionMatrixAggregation.CONFUSION_MATRIX,
+                ),
+                ConfusionMatrixMetric(
+                    value={0.1: (0, 1, 2, 3), 0.2: (5, 3, 4, 3)},
+                    extra={"uuid": "a22bbf6e-b2da-4abe-9a11-df84759f7674"},
+                    metric_name="50%_iou",
+                    aggregation=ConfusionMatrixAggregation.CONFUSION_MATRIX,
+                ),
+            ],
+        )
+    ]
+
+    res = list(NDJsonConverter.serialize(labels))
+
+    assert data == res
--- libs/labelbox/tests/data/serialization/ndjson/test_nested.py
+++ libs/labelbox/tests/data/serialization/ndjson/test_nested.py
@@ -1,13 +1,135 @@
 import json
 
+from labelbox.data.annotation_types.data.generic_data_row_data import (
+    GenericDataRowData,
+)
+from labelbox.data.mixins import CustomMetric
 from labelbox.data.serialization.ndjson.converter import NDJsonConverter
+from labelbox.types import (
+    Label,
+    ObjectAnnotation,
+    Rectangle,
+    Point,
+    ClassificationAnnotation,
+    Radio,
+    ClassificationAnswer,
+    Text,
+    Checklist,
+)
 
 
 def test_nested():
     with open("tests/data/assets/ndjson/nested_import.json", "r") as file:
         data = json.load(file)
-    res = list(NDJsonConverter.deserialize(data))
-    res = list(NDJsonConverter.serialize(res))
+    labels = [
+        Label(
+            data=GenericDataRowData(
+                uid="ckrb1sf1i1g7i0ybcdc6oc8ct",
+            ),
+            annotations=[
+                ObjectAnnotation(
+                    feature_schema_id="ckrb1sfjx099a0y914hl319ie",
+                    extra={
+                        "uuid": "f6879f59-d2b5-49c2-aceb-d9e8dc478673",
+                    },
+                    value=Rectangle(
+                        start=Point(x=2275.0, y=1352.0),
+                        end=Point(x=2414.0, y=1702.0),
+                    ),
+                    classifications=[
+                        ClassificationAnnotation(
+                            feature_schema_id="ckrb1sfkn099c0y910wbo0p1a",
+                            value=Radio(
+                                answer=ClassificationAnswer(
+                                    custom_metrics=[
+                                        CustomMetric(
+                                            name="customMetric1", value=0.5
+                                        ),
+                                        CustomMetric(
+                                            name="customMetric2", value=0.3
+                                        ),
+                                    ],
+                                    confidence=0.34,
+                                    feature_schema_id="ckrb1sfl8099g0y91cxbd5ftb",
+                                ),
+                            ),
+                        )
+                    ],
+                ),
+                ObjectAnnotation(
+                    feature_schema_id="ckrb1sfjx099a0y914hl319ie",
+                    extra={
+                        "uuid": "d009925d-91a3-4f67-abd9-753453f5a584",
+                    },
+                    value=Rectangle(
+                        start=Point(x=2089.0, y=1251.0),
+                        end=Point(x=2247.0, y=1679.0),
+                    ),
+                    classifications=[
+                        ClassificationAnnotation(
+                            feature_schema_id="ckrb1sfkn099c0y910wbo0p1a",
+                            value=Radio(
+                                answer=ClassificationAnswer(
+                                    feature_schema_id="ckrb1sfl8099e0y919v260awv",
+                                ),
+                            ),
+                        )
+                    ],
+                ),
+                ObjectAnnotation(
+                    feature_schema_id="ckrb1sfjx099a0y914hl319ie",
+                    extra={
+                        "uuid": "5d03213e-4408-456c-9eca-cf0723202961",
+                    },
+                    value=Rectangle(
+                        start=Point(x=2089.0, y=1251.0),
+                        end=Point(x=2247.0, y=1679.0),
+                    ),
+                    classifications=[
+                        ClassificationAnnotation(
+                            feature_schema_id="ckrb1sfkn099c0y910wbo0p1a",
+                            value=Checklist(
+                                answer=[
+                                    ClassificationAnswer(
+                                        custom_metrics=[
+                                            CustomMetric(
+                                                name="customMetric1", value=0.5
+                                            ),
+                                            CustomMetric(
+                                                name="customMetric2", value=0.3
+                                            ),
+                                        ],
+                                        confidence=0.894,
+                                        feature_schema_id="ckrb1sfl8099e0y919v260awv",
+                                    )
+                                ],
+                            ),
+                        )
+                    ],
+                ),
+                ObjectAnnotation(
+                    feature_schema_id="ckrb1sfjx099a0y914hl319ie",
+                    extra={
+                        "uuid": "d50812f6-34eb-4f12-b3cb-bbde51a31d83",
+                    },
+                    value=Rectangle(
+                        start=Point(x=2089.0, y=1251.0),
+                        end=Point(x=2247.0, y=1679.0),
+                    ),
+                    classifications=[
+                        ClassificationAnnotation(
+                            feature_schema_id="ckrb1sfkn099c0y910wbo0p1a",
+                            extra={},
+                            value=Text(
+                                answer="a string",
+                            ),
+                        )
+                    ],
+                ),
+            ],
+        )
+    ]
+    res = list(NDJsonConverter.serialize(labels))
     assert res == data
 
 
@@ -16,6 +138,112 @@ def test_nested_name_only():
         "tests/data/assets/ndjson/nested_import_name_only.json", "r"
     ) as file:
         data = json.load(file)
-    res = list(NDJsonConverter.deserialize(data))
-    res = list(NDJsonConverter.serialize(res))
+    labels = [
+        Label(
+            data=GenericDataRowData(
+                uid="ckrb1sf1i1g7i0ybcdc6oc8ct",
+            ),
+            annotations=[
+                ObjectAnnotation(
+                    name="box a",
+                    extra={
+                        "uuid": "f6879f59-d2b5-49c2-aceb-d9e8dc478673",
+                    },
+                    value=Rectangle(
+                        start=Point(x=2275.0, y=1352.0),
+                        end=Point(x=2414.0, y=1702.0),
+                    ),
+                    classifications=[
+                        ClassificationAnnotation(
+                            name="classification a",
+                            value=Radio(
+                                answer=ClassificationAnswer(
+                                    custom_metrics=[
+                                        CustomMetric(
+                                            name="customMetric1", value=0.5
+                                        ),
+                                        CustomMetric(
+                                            name="customMetric2", value=0.3
+                                        ),
+                                    ],
+                                    confidence=0.811,
+                                    name="first answer",
+                                ),
+                            ),
+                        )
+                    ],
+                ),
+                ObjectAnnotation(
+                    name="box b",
+                    extra={
+                        "uuid": "d009925d-91a3-4f67-abd9-753453f5a584",
+                    },
+                    value=Rectangle(
+                        start=Point(x=2089.0, y=1251.0),
+                        end=Point(x=2247.0, y=1679.0),
+                    ),
+                    classifications=[
+                        ClassificationAnnotation(
+                            name="classification b",
+                            value=Radio(
+                                answer=ClassificationAnswer(
+                                    custom_metrics=[
+                                        CustomMetric(
+                                            name="customMetric1", value=0.5
+                                        ),
+                                        CustomMetric(
+                                            name="customMetric2", value=0.3
+                                        ),
+                                    ],
+                                    confidence=0.815,
+                                    name="second answer",
+                                ),
+                            ),
+                        )
+                    ],
+                ),
+                ObjectAnnotation(
+                    name="box c",
+                    extra={
+                        "uuid": "8a2b2c43-f0a1-4763-ba96-e322d986ced6",
+                    },
+                    value=Rectangle(
+                        start=Point(x=2089.0, y=1251.0),
+                        end=Point(x=2247.0, y=1679.0),
+                    ),
+                    classifications=[
+                        ClassificationAnnotation(
+                            name="classification c",
+                            value=Checklist(
+                                answer=[
+                                    ClassificationAnswer(
+                                        name="third answer",
+                                    )
+                                ],
+                            ),
+                        )
+                    ],
+                ),
+                ObjectAnnotation(
+                    name="box c",
+                    extra={
+                        "uuid": "456dd2c6-9fa0-42f9-9809-acc27b9886a7",
+                    },
+                    value=Rectangle(
+                        start=Point(x=2089.0, y=1251.0),
+                        end=Point(x=2247.0, y=1679.0),
+                    ),
+                    classifications=[
+                        ClassificationAnnotation(
+                            name="a string",
+                            value=Text(
+                                answer="a string",
+                            ),
+                        )
+                    ],
+                ),
+            ],
+        )
+    ]
+    res = list(NDJsonConverter.serialize(labels))
     assert res == data
