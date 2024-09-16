da7aeaa8cf27120e0e15335b3e9b786b894f9bc7
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-09-16 14:36:23 -0500

    Removed Coco

--- libs/labelbox/src/labelbox/data/serialization/coco/path.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from pydantic import BaseModel, model_serializer
-
-
-class PathSerializerMixin(BaseModel):
-    @model_serializer(mode="wrap")
-    def serialize_model(self, handler):
-        res = handler(self)
-        return {k: str(v) if isinstance(v, Path) else v for k, v in res.items()}
