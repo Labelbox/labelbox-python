379171a7a50c9b472962718414ccf1b6b69a4a33
Val Brodsky <vbrodsky@users.noreply.github.com>
2024-09-17 12:10:48 -0700

    [PLT-1274] Vb/deprecate bulkimportrequest plt 1274 (#1821)

--- libs/labelbox/src/labelbox/schema/enums.py
+++ libs/labelbox/src/labelbox/schema/enums.py
@@ -1,31 +1,6 @@
 from enum import Enum
 
 
-class BulkImportRequestState(Enum):
-    """State of the import job when importing annotations (RUNNING, FAILED, or FINISHED).
-
-       If you are not usinig MEA continue using BulkImportRequest.
-       AnnotationImports are in beta and will change soon.
-
-    .. list-table::
-       :widths: 15 150
-       :header-rows: 1
-
-       * - State
-         - Description
-       * - RUNNING
-         - Indicates that the import job is not done yet.
-       * - FAILED
-         - Indicates the import job failed. Check `BulkImportRequest.errors` for more information
-       * - FINISHED
-         - Indicates the import job is no longer running. Check `BulkImportRequest.statuses` for more information
-    """
-
-    RUNNING = "RUNNING"
-    FAILED = "FAILED"
-    FINISHED = "FINISHED"
-
-
 class AnnotationImportState(Enum):
     """State of the import job when importing annotations (RUNNING, FAILED, or FINISHED).
 
