321cdafcab6cfc414a198ce96ad01d1db93c663f
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-09-23 18:23:10 -0500

    Remove test_labeling_service changes

--- libs/labelbox/tests/integration/test_labeling_service.py
+++ libs/labelbox/tests/integration/test_labeling_service.py
@@ -1,10 +1,6 @@
 import pytest
 from lbox.exceptions import LabelboxError, ResourceNotFoundError
 
-from labelbox.exceptions import (
-    MalformedQueryException,
-    ResourceNotFoundError,
-)
 from labelbox.schema.labeling_service import LabelingServiceStatus
 
 
@@ -55,7 +51,7 @@ def test_request_labeling_service_moe_project(
 
     labeling_service = project.get_labeling_service()
     with pytest.raises(
-        MalformedQueryException,
+        LabelboxError,
         match='[{"errorType":"PROJECT_MODEL_CONFIG","errorMessage":"Project model config is not completed"}]',
     ):
         labeling_service.request()
@@ -77,5 +73,5 @@ def test_request_labeling_service_incomplete_requirements(ontology, project):
     ):  # No labeling service by default
         labeling_service.request()
     project.connect_ontology(ontology)
-    with pytest.raises(MalformedQueryException):
+    with pytest.raises(LabelboxError):
         labeling_service.request()
