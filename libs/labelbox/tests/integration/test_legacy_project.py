6c16f432e2a7aca8915d0600ad335c80ef189675
Val Brodsky <vbrodsky@labelbox.com>
2024-09-24 17:57:14 -0700

    Remove auto_audit inputs

--- libs/labelbox/tests/integration/test_legacy_project.py
+++ libs/labelbox/tests/integration/test_legacy_project.py
@@ -3,30 +3,7 @@ from os import name
 import pytest
 from pydantic import ValidationError
 
-from labelbox.schema import media_type
 from labelbox.schema.media_type import MediaType
-
-
-def test_project_auto_audit_parameters(client, rand_gen):
-    with pytest.raises(
-        ValueError,
-        match="quality_modes must be set instead of auto_audit_percentage or auto_audit_number_of_labels.",
-    ):
-        client.create_project(
-            name=rand_gen(str),
-            media_type=MediaType.Image,
-            auto_audit_percentage=0.5,
-        )
-
-    with pytest.raises(
-        ValidationError,
-        match="quality_modes must be set instead of auto_audit_percentage or auto_audit_number_of_labels.",
-    ):
-        client.create_project(
-            name=rand_gen(str),
-            media_type=MediaType.Image,
-            auto_audit_number_of_labels=2,
-        )
 
 
 def test_project_name_parameter(client, rand_gen):
