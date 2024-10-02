23d7e59c8828baea770e24492584eabcd9575d60
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-10-01 22:49:20 -0500

    removed rest of deprecated

--- libs/labelbox/src/labelbox/schema/queue_mode.py
+++ libs/labelbox/src/labelbox/schema/queue_mode.py
@@ -4,9 +4,3 @@ from enum import Enum
 class QueueMode(str, Enum):
     Batch = "BATCH"
     Dataset = "DATA_SET"
-
-    @classmethod
-    def _missing_(cls, value):
-        # Parses the deprecated "CATALOG" value back to QueueMode.Batch.
-        if value == "CATALOG":
-            return QueueMode.Batch
