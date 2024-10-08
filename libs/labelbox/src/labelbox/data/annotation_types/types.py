8989231d3061936378aaeb20bab54a74a9c6b25f
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-10-08 16:53:56 -0500

    Cleaned up typed array

--- libs/labelbox/src/labelbox/data/annotation_types/types.py
+++ libs/labelbox/src/labelbox/data/annotation_types/types.py
@@ -1,16 +1,12 @@
-import sys
-from typing import Annotated, Any, Generic, TypeVar
-
+from typing import Generic, TypeVar
 import numpy as np
-from packaging import version
-from pydantic import ConfigDict, Field, StringConstraints
 from pydantic_core import core_schema
 
 DType = TypeVar("DType")
 DShape = TypeVar("DShape")
 
 
-class _TypedArray(np.ndarray, Generic[DType, DShape]):
+class TypedArray(np.ndarray, Generic[DType, DShape]):
     @classmethod
     def __get_pydantic_core_schema__(
         cls, _source_type: type, _model: type
@@ -22,23 +18,3 @@ class _TypedArray(np.ndarray, Generic[DType, DShape]):
         if not isinstance(val, np.ndarray):
             raise TypeError(f"Expected numpy array. Found {type(val)}")
         return val
-
-
-if version.parse(np.__version__) >= version.parse("1.25.0"):
-    from typing import GenericAlias
-
-    TypedArray = GenericAlias(_TypedArray, (Any, DType))
-elif version.parse(np.__version__) >= version.parse("1.23.0"):
-    from numpy._typing import _GenericAlias
-
-    TypedArray = _GenericAlias(_TypedArray, (Any, DType))
-elif (
-    version.parse("1.22.0")
-    <= version.parse(np.__version__)
-    < version.parse("1.23.0")
-):
-    from numpy.typing import _GenericAlias
-
-    TypedArray = _GenericAlias(_TypedArray, (Any, DType))
-else:
-    TypedArray = _TypedArray[Any, DType]
