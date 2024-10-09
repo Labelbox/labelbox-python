8276fc682253aee6286b45ab803160f2399d6c72
Val Brodsky <vbrodsky@users.noreply.github.com>
2024-10-09 10:14:27 -0700

    Release v.5.2.0 prep (#1860)

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = 'Python SDK reference'
 copyright = '2024, Labelbox'
 author = 'Labelbox'
-release = '5.1.0'
+release = '5.2.0'
 
 # -- General configuration ---------------------------------------------------
 
