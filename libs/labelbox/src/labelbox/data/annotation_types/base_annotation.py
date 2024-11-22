305ed67b389b05d0c80bbcb3a8a3641f96813e46
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-11-22 16:17:03 -0600

    fixed

--- libs/labelbox/src/labelbox/data/annotation_types/base_annotation.py
+++ libs/labelbox/src/labelbox/data/annotation_types/base_annotation.py
@@ -16,5 +16,4 @@ class BaseAnnotation(FeatureSchema, abc.ABC):
 
     def __init__(self, **data):
         super().__init__(**data)
-        extra_uuid = data.get("extra", {}).get("uuid")
-        self._uuid = data.get("_uuid") or extra_uuid or uuid4()
+        self._uuid = data.get("_uuid") or uuid4()
