37c563b1ecdeb7168f45fa78d92511cc01bcf18e
Gabefire <33893811+Gabefire@users.noreply.github.com>
2025-01-09 07:09:34 -0800

    revert random change

--- libs/labelbox/src/labelbox/schema/project.py
+++ libs/labelbox/src/labelbox/schema/project.py
@@ -1403,9 +1403,7 @@ class Project(DbObject, Updateable, Deletable):
         ]
 
     def move_data_rows_to_task_queue(
-        self,
-        data_row_ids: DataRowIdentifiers,
-        task_queue_id: Optional[str] = None,
+        self, data_row_ids: DataRowIdentifiers, task_queue_id: str
     ):
         """
 
@@ -1414,7 +1412,7 @@ class Project(DbObject, Updateable, Deletable):
         Args:
             data_row_ids: a list of data row ids to be moved. This should be a DataRowIdentifiers object
                 DataRowIdentifier objects are lists of ids or global keys. A DataIdentifier object can be a UniqueIds or GlobalKeys class.
-            task_queue_id: the task queue id to be moved to, or None to specify the "Done" queue. Defaults to None.
+            task_queue_id: the task queue id to be moved to, or None to specify the "Done" queue.
 
         Returns:
             None if successful, or a raised error on failure
