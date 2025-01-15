84417b5fd3e0d5d05f59d81ef322cb47cae72119
Val Brodsky <vbrodsky@labelbox.com>
2025-01-15 13:38:30 -0800

    Bumped versions

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = 'Python SDK reference'
 copyright = '2024, Labelbox'
 author = 'Labelbox'
-release = '6.5.0'
+release = '6.6.0'
 
 # -- General configuration ---------------------------------------------------
 
