3fcfce9094dd95202c33e92edd295e2752012b66
Karol Jamrozy <karol.j.jamrozy@gmail.com>
2025-07-02 20:30:39 +0200

    Address CR comment

--- libs/labelbox/src/labelbox/schema/tool_building/tool_type_mapping.py
+++ libs/labelbox/src/labelbox/schema/tool_building/tool_type_mapping.py
@@ -6,9 +6,15 @@ from labelbox.schema.tool_building.step_reasoning_tool import (
     StepReasoningTool,
 )
 from labelbox.schema.tool_building.tool_type import ToolType
+from labelbox.schema.ontology import Tool
+from labelbox.schema.tool_building.relationship_tool import RelationshipTool
 
 
 def map_tool_type_to_tool_cls(tool_type_str: str):
+    # Relationship tool uses a legacy tool type, so we need to handle it separately.
+    if tool_type_str.lower() == Tool.Type.RELATIONSHIP:
+        return RelationshipTool()
+
     if not ToolType.valid(tool_type_str):
         return None
 
