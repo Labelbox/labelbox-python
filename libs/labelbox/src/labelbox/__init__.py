b1839c47c59493f2b5a67dffe7a0352176fceca5
Karol Jamrozy <karol.j.jamrozy@gmail.com>
2025-07-08 10:54:04 +0200

    [CCV-4649] Move RelationshipTool under common labelbox import

--- libs/labelbox/src/labelbox/__init__.py
+++ libs/labelbox/src/labelbox/__init__.py
@@ -54,6 +54,7 @@ from labelbox.schema.ontology import (
 from labelbox.schema.tool_building.fact_checking_tool import FactCheckingTool
 from labelbox.schema.tool_building.step_reasoning_tool import StepReasoningTool
 from labelbox.schema.tool_building.prompt_issue_tool import PromptIssueTool
+from labelbox.schema.tool_building.relationship_tool import RelationshipTool
 from labelbox.schema.role import Role, ProjectRole
 from labelbox.schema.invite import Invite, InviteLimit
 from labelbox.schema.data_row_metadata import (
