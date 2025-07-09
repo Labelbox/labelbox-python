1f72bd1e03481b1ebcfef8e60c14a69f7b8cc327
paulnoirel <87332996+paulnoirel@users.noreply.github.com>
2025-07-09 20:20:15 +0200

    Prep 7.1.1 doc

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = 'Python SDK reference'
 copyright = '2025, Labelbox'
 author = 'Labelbox'
-release = '7.1.0'
+release = '7.1.1'
 
 # -- General configuration ---------------------------------------------------
 
