ce50697f56ace0cf093d156be249c8cc159bf3af
Łukasz Głuszek <lukaszmateuszgluszek@gmail.com>
2026-01-21 19:08:37 +0100

    [PTDT-3784] Model config response count support (#2014)

--- libs/labelbox/src/labelbox/schema/project.py
+++ libs/labelbox/src/labelbox/schema/project.py
@@ -1183,18 +1183,21 @@ class Project(DbObject, Updateable, Deletable):
         res = self.client.execute(query_str, {"projectId": self.uid})
         return res["project"]["labelCount"]
 
-    def add_model_config(self, model_config_id: str) -> str:
+    def add_model_config(
+        self, model_config_id: str, response_count: Optional[int] = None
+    ) -> str:
         """Adds a model config to this project.
 
         Args:
             model_config_id (str): ID of a model config to add to this project.
+            response_count (Optional[int]): Number of responses to generate. If not provided, uses the default.
 
         Returns:
             str, ID of the project model config association. This is needed for updating and deleting associations.
         """
 
-        query = """mutation CreateProjectModelConfigPyApi($projectId: ID!, $modelConfigId: ID!)  {
-                    createProjectModelConfig(input: {projectId: $projectId, modelConfigId: $modelConfigId}) {
+        query = """mutation CreateProjectModelConfigPyApi($projectId: ID!, $modelConfigId: ID!, $responseCount: Int)  {
+                    createProjectModelConfig(input: {projectId: $projectId, modelConfigId: $modelConfigId, responseCount: $responseCount}) {
                         projectModelConfigId
                     }
                 }"""
@@ -1202,6 +1205,7 @@ class Project(DbObject, Updateable, Deletable):
         params = {
             "projectId": self.uid,
             "modelConfigId": model_config_id,
+            "responseCount": response_count,
         }
         try:
             result = self.client.execute(query, params)
