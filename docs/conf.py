649784478a896ef8e22b8348c91bf1c1a682ff4f
Dmytro Apollonin <dapollonin@labelbox.com>
2026-06-11 16:45:19 -0600

    Prep 7.8.0

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = 'Python SDK reference'
 copyright = '2025, Labelbox'
 author = 'Labelbox'
-release = '7.7.0'
+release = '7.8.0'
 
 # -- General configuration ---------------------------------------------------
 
